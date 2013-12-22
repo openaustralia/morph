91bc2dcf348296c3a0d530e63f5f0567d1f878a0
Matthew Landauer <matthew@openaustralia.org>
2013-12-22 12:47:49 +1100

    rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ScrapingPlatform::Application.config.secret_key_base = '6a65bc4ea813a775a3549313070078f95ace11a9695510a63989e7e6770abd355a9852f739d5e2d4a1f4f33a86a64f9d6475a3b7d31d7e1933c99efb14cb357c'
