5a50eeb1440adc784cde42b03f53f13d0d89a5da
Matthew Landauer <matthew@openaustralia.org>
2013-12-22 16:38:21 +1100

    Add basic signin/out ui

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 ScrapingPlatform::Application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
+  devise_scope :user do
+    get 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
