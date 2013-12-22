90a0ada466ce21c9483709c4a3d0fe617a04102a
Matthew Landauer <matthew@openaustralia.org>
2013-12-22 19:21:39 +1100

    Consistent spelling of github

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -3,7 +3,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
 
     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-    set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+    set_flash_message(:notice, :success, :kind => "GitHub") if is_navigational_format?
   end
 
   private
