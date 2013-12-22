46076c1a8591857cac9d027abf26f360f2868966
Matthew Landauer <matthew@openaustralia.org>
2013-12-22 12:53:04 +1100

    Put in a basic holding page

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ ScrapingPlatform::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'static#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
