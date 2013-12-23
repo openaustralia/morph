444525a089fb798c5bbe3c1384960e523b0841c6
Matthew Landauer <matthew@openaustralia.org>
2013-12-23 14:36:57 +1100

    List all repositories

--- app/controllers/scrapers_controller.rb
+++ app/controllers/scrapers_controller.rb
@@ -2,5 +2,9 @@ class ScrapersController < ApplicationController
   before_filter :authenticate_user!
   
   def new
+    # Get the list of repositories
+    client = Octokit::Client.new :access_token => current_user.access_token
+    @repos = client.repositories
+    puts @repos.first.to_yaml
   end
 end
