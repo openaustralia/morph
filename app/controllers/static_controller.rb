b733b89399a367af61cd690e270fd077c5fad780
Matthew Landauer <matthew@openaustralia.org>
2013-12-26 12:34:17 +1100

    Extract example values in api documentation

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -3,5 +3,8 @@ class StaticController < ApplicationController
   end
 
   def api
+    # Example scraper
+    @scraper = Scraper.find_by(full_name: "mlandauer/scraper-blue-mountains")
+    @query = "select * from swdata limit 10"
   end
 end
