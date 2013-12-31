7ff29a24f50b6bf9815914fb04726f4dd682afd6
Matthew Landauer <matthew@openaustralia.org>
2013-12-31 17:07:16 +1100

    Restart passenger after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ namespace :deploy do
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
