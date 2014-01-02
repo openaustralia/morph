5e37f98c45ba7685e6901c4338b7b41e58af0c20
Matthew Landauer <matthew@openaustralia.org>
2014-01-03 07:55:07 +1100

    Remove trailing mdashes and add links to github repos

--- app/helpers/scrapers_helper.rb
+++ app/helpers/scrapers_helper.rb
@@ -1,2 +1,7 @@
 module ScrapersHelper
+  def radio_description(repo)
+    a = link_to(content_tag(:strong, repo.name), repo.rels[:html].href)
+    a += " &mdash; #{repo.description}".html_safe unless repo.description.blank?
+    a
+  end
 end
