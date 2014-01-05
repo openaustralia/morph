9764dcb98247dc41f1d4f49c9882afd1d82100a9
Matthew Landauer <matthew@openaustralia.org>
2014-01-05 14:49:15 +1100

    Some defaults for testing

--- app/controllers/scraperwiki_forks_controller.rb
+++ app/controllers/scraperwiki_forks_controller.rb
@@ -1,5 +1,6 @@
 class ScraperwikiForksController < ApplicationController
   def new
-    @scraper = Scraper.new
+    @scraper = Scraper.new(name: "scraper-test",
+      scraperwiki_url: "https://classic.scraperwiki.com/scrapers/city_of_sydney_development_applications/")
   end
 end
