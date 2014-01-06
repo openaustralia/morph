96bb254ffbf0970fba842c1e6a777705d8c6cab8
Matthew Landauer <matthew@openaustralia.org>
2014-01-07 09:57:20 +1100

    Adds a second commit with a require to forked scraperwiki scraper

--- app/controllers/scraperwiki_forks_controller.rb
+++ app/controllers/scraperwiki_forks_controller.rb
@@ -45,10 +45,23 @@ class ScraperwikiForksController < ApplicationController
         :type => "blob",
         :content => readme_text
       },
-    ], :base_tree => "")
+    ])
     commit_message = "Fork of code from ScraperWiki at #{@scraper.scraperwiki_url}"
     commit = client.create_commit(repo["full_name"], commit_message, tree.sha)
     client.update_ref(repo["full_name"],"heads/master", commit.sha)
+
+    # Now add an extra commit that adds "require 'scraperwiki'" to the top of the scraper code
+    # TODO Only do this if necessary
+    tree2 = client.create_tree(repo["full_name"], [
+      {
+        :path => "scraper.rb",
+        :mode => "100644",
+        :type => "blob",
+        :content => "require 'scraperwiki'\n" + code
+      },
+    ], :base_tree => tree.sha)
+    commit2 = client.create_commit(repo["full_name"], "Add require 'scraperwiki'", tree2.sha, commit.sha)
+    client.update_ref(repo["full_name"],"heads/master", commit2.sha)
 
     @scraper.save!
     #flash[:notice] = "Forking in action..."
@@ -58,11 +71,12 @@ class ScraperwikiForksController < ApplicationController
     # TODO Make each background step idempotent so that failures can be retried
     # TODO Run all this in the background
 
+    # TODO Check that local scraper with that name doesn't already exist
     # TODO Add repo link
     # TODO Copy across run interval from scraperwiki
-    # TODO Add require scraperwiki to code (if required)
     # TODO Handle name on github already taken
     # TODO Check that it's a ruby scraper
     # TODO Add support for non-ruby scrapers
+    # TODO Add .gitignore for scraperwiki.sqlite
   end
 end
