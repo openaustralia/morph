fc4985ce7e5b9ab0eeac6298978f507253e86b94
Matthew Landauer <matthew@openaustralia.org>
2014-01-07 07:39:21 +1100

    Tidy up

--- app/controllers/scraperwiki_forks_controller.rb
+++ app/controllers/scraperwiki_forks_controller.rb
@@ -13,32 +13,23 @@ class ScraperwikiForksController < ApplicationController
     client = Octokit::Client.new :access_token => current_user.access_token
     # We need to set auto_init so that we can create a commit later. The API doesn't support
     # adding a commit to an empty repository
-    #repo = client.create_repository(@scraper.name, auto_init: true)
-    repo = client.repository("#{current_user.to_param}/#{@scraper.name}")
-    # TODO Copy across source code (with comment for commit with link back to scraperwiki)
-    puts "scraperwiki shortname: #{@scraper.scraperwiki_shortname}" 
+    repo = client.create_repository(@scraper.name, auto_init: true)
+    #repo = client.repository("#{current_user.to_param}/#{@scraper.name}")
     url = "https://api.scraperwiki.com/api/1.0/scraper/getinfo?format=jsondict&name=#{@scraper.scraperwiki_shortname}&version=-1&quietfields=runevents%7Chistory%7Cdatasummary%7Cuserroles"
     response = Faraday.get url
     code = (JSON.parse(response.body).first)["code"]
-    puts code
-    # Create blob
-    puts "Creating blob..."
-    blob_sha = client.create_blob(repo["full_name"], code)
 
+    # Commit the code
     tree = client.create_tree(repo["full_name"], [ {
       :path => "scraper.rb",
       :mode => "100644",
       :type => "blob",
-      # TODO Could use :content instead of :sha - more concise
-      :sha => blob_sha
+      :content => code
     } ], :base_tree => "")
-
-    puts "Return from create_tree:"
-    p tree
-    commit = client.create_commit(repo["full_name"], "Fork from Scraperwiki", tree.sha)
-    p commit
-    # Now set update the master branch
+    commit_message = "Fork of code from ScraperWiki at #{@scraper.scraperwiki_url}"
+    commit = client.create_commit(repo["full_name"], commit_message, tree.sha)
     client.update_ref(repo["full_name"],"heads/master", commit.sha)
+
     flash[:notice] = "Forking in action..."
     redirect_to new_scraperwiki_fork_url
     # TODO Check that it's a ruby scraper
