97e3a2d8c0bc606c1a8851264496b8335ba270b7
Matthew Landauer <matthew@openaustralia.org>
2014-01-09 17:22:02 +1100

    Hacky way to force showing api.morph.io for api links. Fixes #91

--- app/helpers/static_helper.rb
+++ app/helpers/static_helper.rb
@@ -1,2 +1,18 @@
 module StaticHelper
+  # TODO Move this bit of configuration somewhere sensible
+  def api_host
+    if Rails.env.development?
+      request.host
+    else
+      "api.morph.io"
+    end
+  end
+
+  def api_root
+    if Rails.env.development?
+      root_url
+    else
+      root_url(host: api_host)
+    end
+  end
 end
