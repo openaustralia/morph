96b9b6e9d5163d6df186f831a44b6367b096abf7
Matthew Landauer <matthew@openaustralia.org>
2014-01-11 17:14:12 +1100

    Generate the command for capturing the metrics

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -41,4 +41,8 @@ class Metric < ActiveRecord::Base
   # nivcsw (since Linux 2.6)
   # Involuntary context switches: 65
   # The number of times a context switch resulted due to a higher priority process becoming runnable or because the current process exceeded its time slice.
+
+  def self.command(other)
+    "time -v -o time.output #{other}"
+  end
 end
--- spec/models/metric_spec.rb
+++ spec/models/metric_spec.rb
@@ -1,5 +1,13 @@
 require 'spec_helper'
 
 describe Metric do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".command" do
+    it "should return the command needed to capture the metric" do
+      Metric.command("ls").should == "time -v -o time.output ls"
+    end
+
+    it "should do the right thing with a different command" do
+      Metric.command("ruby ./scraper.rb").should == "time -v -o time.output ruby ./scraper.rb"
+    end
+  end
 end
