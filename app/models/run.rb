87128661e926119b7d1ad4ab358d439a0879a6e6
Matthew Landauer <matthew@openaustralia.org>
2014-01-13 14:45:42 +1100

    Move methods

--- app/models/run.rb
+++ app/models/run.rb
@@ -3,7 +3,7 @@ class Run < ActiveRecord::Base
   has_many :log_lines
   belongs_to :metric
 
-  delegate :data_path, :owner, :name, to: :scraper
+  delegate :data_path, :repo_path, :owner, :name, :git_url, to: :scraper
 
   def finished?
     !!finished_at
@@ -57,8 +57,8 @@ class Run < ActiveRecord::Base
 
     begin
       c.start("Binds" => [
-        "#{local_root_path}/#{scraper.repo_path}:/repo:ro",
-        "#{local_root_path}/#{scraper.data_path}:/data"
+        "#{local_root_path}/#{repo_path}:/repo:ro",
+        "#{local_root_path}/#{data_path}:/data"
       ])
       puts "Running docker container..."
       log_line_number = 0
@@ -79,24 +79,34 @@ class Run < ActiveRecord::Base
     metric = Metric.read_from_file(time_output_path)
 
     update_attributes(status_code: status_code, metric_id: metric.id, finished_at: Time.now)
-    scraper.tidy_data_path
+    tidy_data_path
   end
 
   def synchronise_repo
     # Set git timeout to 1 minute
     # TODO Move this to a configuration
     Grit::Git.git_timeout = 60
-    gritty = Grit::Git.new(scraper.repo_path)
+    gritty = Grit::Git.new(repo_path)
     if gritty.exist?
-      puts "Pulling git repo #{scraper.repo_path}..."
+      puts "Pulling git repo #{repo_path}..."
       # TODO Fix this. Using grit seems to do a pull but not update the working directory
       # So falling back to shelling out to the git command
       #gritty = Grit::Repo.new(repo_path).git
       #puts gritty.pull({:raise => true}, "origin", "master")
-      system("cd #{scraper.repo_path}; git pull")
+      system("cd #{repo_path}; git pull")
     else
-      puts "Cloning git repo #{scraper.git_url}..."
-      puts gritty.clone({:verbose => true, :progress => true, :raise => true}, scraper.git_url, scraper.repo_path)
+      puts "Cloning git repo #{git_url}..."
+      puts gritty.clone({:verbose => true, :progress => true, :raise => true}, git_url, repo_path)
     end
+  end
+
+  # Remove any files or directories in the data_path that are not the actual database
+  def tidy_data_path
+    # First get all the files in the data directory
+    filenames = Dir.entries(data_path)
+    filenames.delete(".")
+    filenames.delete("..")
+    filenames.delete(scraper.sqlite_db_filename)
+    FileUtils.rm_rf filenames.map{|f| File.join(data_path, f)}
   end
 end
