9701a8eb7da63f7acf852b34f429b7d9d25ae103
Matthew Landauer <matthew@openaustralia.org>
2014-01-14 17:10:13 +1100

    Change table name in sql statement

--- lib/code_translate.rb
+++ lib/code_translate.rb
@@ -1,7 +1,7 @@
 class CodeTranslate
   # Translate Ruby code on ScraperWiki to something that will run on Morph
   def self.ruby(code)
-    switch_to_scraperwiki_morph(add_require(code))
+    change_table_in_sqliteexecute_and_select(switch_to_scraperwiki_morph(add_require(code)))
   end
 
   # If necessary adds "require 'scraperwiki'" to the top of the scraper code
@@ -15,5 +15,9 @@ class CodeTranslate
 
   def self.switch_to_scraperwiki_morph(code)
     code.gsub(/ScraperWiki\./, "ScraperWikiMorph.")
+  end
+
+  def self.change_table_in_sqliteexecute_and_select(code)
+    code.gsub(/(ScraperWiki\.(sqliteexecute|select)\(['"].*)swdata/, '\1data')
   end
 end
--- spec/lib/code_translate_spec.rb
+++ spec/lib/code_translate_spec.rb
@@ -3,10 +3,11 @@ require 'spec_helper'
 describe CodeTranslate do
   describe ".ruby" do
     it "should do a series of translations and return the final result" do
-      input, output1, output2 = double, double, double
+      input, output1, output2, output3 = double, double, double, double
       CodeTranslate.should_receive(:add_require).with(input).and_return(output1)
       CodeTranslate.should_receive(:switch_to_scraperwiki_morph).with(output1).and_return(output2)
-      CodeTranslate.ruby(input).should == output2
+      CodeTranslate.should_receive(:change_table_in_sqliteexecute_and_select).with(output2).and_return(output3)
+      CodeTranslate.ruby(input).should == output3
     end
   end
 
@@ -31,6 +32,13 @@ describe CodeTranslate do
     it "should replace all uses of ScraperWiki with ScraperWikiMorph" do
       CodeTranslate.switch_to_scraperwiki_morph("if foo\n  ScraperWiki.select(twiddle, bob)\nend\n").should ==
         "if foo\n  ScraperWikiMorph.select(twiddle, bob)\nend\n"
+    end
+  end
+
+  describe ".change_table_in_sqliteexecute_and_select" do
+    it "should replace the table name" do
+      CodeTranslate.change_table_in_sqliteexecute_and_select("ScraperWikiMorph.save_sqlite(swdata)\nScraperWiki.sqliteexecute('select * from swdata', foo, bar)\n").should ==
+        "ScraperWikiMorph.save_sqlite(swdata)\nScraperWiki.sqliteexecute('select * from data', foo, bar)\n"
     end
   end
 end
