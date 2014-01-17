f1101c18f04bcc191b823482d3b0833aa2ec7b66
Matthew Landauer <matthew@openaustralia.org>
2014-01-17 13:24:56 +1100

    Move ruby translation code into its own module

--- lib/code_translate.rb
+++ lib/code_translate.rb
@@ -1,9 +1,9 @@
-class CodeTranslate
+module CodeTranslate
   # Translate Ruby code on ScraperWiki to something that will run on Morph
   def self.translate(language, code)
     case language
     when :ruby
-      ruby(code)
+      Ruby::translate(code)
     when :php, :python
       code
     else
@@ -11,28 +11,30 @@ class CodeTranslate
     end
   end
 
-  def self.ruby(code)
-    switch_to_scraperwiki_morph(change_table_in_sqliteexecute_and_select(add_require(code)))
-  end
-
-  # If necessary adds "require 'scraperwiki'" to the top of the scraper code
-  def self.add_require(code)
-    if code =~ /require ['"]scraperwiki['"]/
-      code.gsub(/require ['"]scraperwiki['"]/, "require 'scraperwiki-morph'")
-    else
-      code = "require 'scraperwiki-morph'\n" + code
+  module Ruby
+    def self.translate(code)
+      switch_to_scraperwiki_morph(change_table_in_sqliteexecute_and_select(add_require(code)))
     end
-  end
 
-  def self.switch_to_scraperwiki_morph(code)
-    code.gsub(/ScraperWiki\./, "ScraperWikiMorph.")
-  end
+    # If necessary adds "require 'scraperwiki'" to the top of the scraper code
+    def self.add_require(code)
+      if code =~ /require ['"]scraperwiki['"]/
+        code.gsub(/require ['"]scraperwiki['"]/, "require 'scraperwiki-morph'")
+      else
+        code = "require 'scraperwiki-morph'\n" + code
+      end
+    end
 
-  def self.change_table_in_sqliteexecute_and_select(code)
-    code.gsub(/ScraperWiki.(sqliteexecute|select)\((['"])(.*)(['"])(.*)\)/) do |s|
-      method, bracket1, sql, bracket2, rest = $1, $2, $3, $4, $5
-      sql = sql.gsub('swdata', 'data')
-      "ScraperWiki.#{method}(#{bracket1}#{sql}#{bracket2}#{rest})"
+    def self.switch_to_scraperwiki_morph(code)
+      code.gsub(/ScraperWiki\./, "ScraperWikiMorph.")
+    end
+
+    def self.change_table_in_sqliteexecute_and_select(code)
+      code.gsub(/ScraperWiki.(sqliteexecute|select)\((['"])(.*)(['"])(.*)\)/) do |s|
+        method, bracket1, sql, bracket2, rest = $1, $2, $3, $4, $5
+        sql = sql.gsub('swdata', 'data')
+        "ScraperWiki.#{method}(#{bracket1}#{sql}#{bracket2}#{rest})"
+      end
     end
   end
 end
--- spec/lib/code_translate_spec.rb
+++ spec/lib/code_translate_spec.rb
@@ -5,7 +5,7 @@ describe CodeTranslate do
     let(:code) { double }
 
     it "should translate ruby" do
-      CodeTranslate.should_receive(:ruby).with(code)
+      CodeTranslate::Ruby.should_receive(:translate).with(code)
       CodeTranslate.translate(:ruby, code)
     end
 
@@ -18,51 +18,53 @@ describe CodeTranslate do
     end
   end
 
-  describe ".ruby" do
-    it "should do a series of translations and return the final result" do
-      input, output1, output2, output3 = double, double, double, double
-      CodeTranslate.should_receive(:add_require).with(input).and_return(output1)
-      CodeTranslate.should_receive(:change_table_in_sqliteexecute_and_select).with(output1).and_return(output2)
-      CodeTranslate.should_receive(:switch_to_scraperwiki_morph).with(output2).and_return(output3)
-      CodeTranslate.ruby(input).should == output3
-    end
-  end
-
-  describe ".add_require" do
-    it "should replace scraperwiki with scraperwiki-morph (with single quotes)" do
-      CodeTranslate.add_require("require 'scraperwiki'\nsome other code\n").should ==
-        "require 'scraperwiki-morph'\nsome other code\n"
+  describe "Ruby" do
+    describe ".translate" do
+      it "should do a series of translations and return the final result" do
+        input, output1, output2, output3 = double, double, double, double
+        CodeTranslate::Ruby.should_receive(:add_require).with(input).and_return(output1)
+        CodeTranslate::Ruby.should_receive(:change_table_in_sqliteexecute_and_select).with(output1).and_return(output2)
+        CodeTranslate::Ruby.should_receive(:switch_to_scraperwiki_morph).with(output2).and_return(output3)
+        CodeTranslate::Ruby.translate(input).should == output3
+      end
     end
 
-    it "should replace scraperwiki with scraperwiki-morph (with double quotes)" do
-      CodeTranslate.add_require("require \"scraperwiki\"\nsome other code\n").should ==
-        "require 'scraperwiki-morph'\nsome other code\n"
-    end
+    describe ".add_require" do
+      it "should replace scraperwiki with scraperwiki-morph (with single quotes)" do
+        CodeTranslate::Ruby.add_require("require 'scraperwiki'\nsome other code\n").should ==
+          "require 'scraperwiki-morph'\nsome other code\n"
+      end
 
-    it "should add the require if it's not there" do
-      CodeTranslate.add_require("some code\n").should ==
-        "require 'scraperwiki-morph'\nsome code\n"
-    end
-  end
+      it "should replace scraperwiki with scraperwiki-morph (with double quotes)" do
+        CodeTranslate::Ruby.add_require("require \"scraperwiki\"\nsome other code\n").should ==
+          "require 'scraperwiki-morph'\nsome other code\n"
+      end
 
-  describe ".switch_to_scraperwiki_morph" do
-    it "should replace all uses of ScraperWiki with ScraperWikiMorph" do
-      CodeTranslate.switch_to_scraperwiki_morph("if foo\n  ScraperWiki.select(twiddle, bob)\nend\n").should ==
-        "if foo\n  ScraperWikiMorph.select(twiddle, bob)\nend\n"
-    end
-  end
+      it "should add the require if it's not there" do
+        CodeTranslate::Ruby.add_require("some code\n").should ==
+          "require 'scraperwiki-morph'\nsome code\n"
+      end
 
-  describe ".change_table_in_sqliteexecute_and_select" do
-    it "should replace the table name" do
-      CodeTranslate.change_table_in_sqliteexecute_and_select( \
-        "ScraperWiki.save_sqlite(swdata)\nScraperWiki.sqliteexecute('select * from swdata', foo, bar)\nScraperWiki.select('select * from swdata; select * from swdata', foo, bar)\n").should ==
-        "ScraperWiki.save_sqlite(swdata)\nScraperWiki.sqliteexecute('select * from data', foo, bar)\nScraperWiki.select('select * from data; select * from data', foo, bar)\n"
-    end
+      describe ".switch_to_scraperwiki_morph" do
+        it "should replace all uses of ScraperWiki with ScraperWikiMorph" do
+          CodeTranslate::Ruby.switch_to_scraperwiki_morph("if foo\n  ScraperWiki.select(twiddle, bob)\nend\n").should ==
+            "if foo\n  ScraperWikiMorph.select(twiddle, bob)\nend\n"
+        end
+      end
 
-    it "another example" do
-      CodeTranslate.change_table_in_sqliteexecute_and_select( \
-        "if (ScraperWiki.select(\"* from swdata where `council_reference`='\#{record['council_reference']}'\").empty? rescue true)").should ==
-        "if (ScraperWiki.select(\"* from data where `council_reference`='\#{record['council_reference']}'\").empty? rescue true)"
+      describe ".change_table_in_sqliteexecute_and_select" do
+        it "should replace the table name" do
+          CodeTranslate::Ruby.change_table_in_sqliteexecute_and_select( \
+            "ScraperWiki.save_sqlite(swdata)\nScraperWiki.sqliteexecute('select * from swdata', foo, bar)\nScraperWiki.select('select * from swdata; select * from swdata', foo, bar)\n").should ==
+            "ScraperWiki.save_sqlite(swdata)\nScraperWiki.sqliteexecute('select * from data', foo, bar)\nScraperWiki.select('select * from data; select * from data', foo, bar)\n"
+        end
+
+        it "another example" do
+          CodeTranslate::Ruby.change_table_in_sqliteexecute_and_select( \
+            "if (ScraperWiki.select(\"* from swdata where `council_reference`='\#{record['council_reference']}'\").empty? rescue true)").should ==
+            "if (ScraperWiki.select(\"* from data where `council_reference`='\#{record['council_reference']}'\").empty? rescue true)"
+        end
+      end
     end
   end
 end
