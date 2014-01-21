1e22cd5f577637258ba6e9916797e811b391da51
Matthew Landauer <matthew@openaustralia.org>
2014-01-21 16:46:12 +1100

    Automatically update submodules too. #143

--- app/models/run.rb
+++ app/models/run.rb
@@ -111,6 +111,9 @@ class Run < ActiveRecord::Base
     else
       puts "Cloning git repo #{git_url}..."
       puts gritty.clone({:verbose => true, :progress => true, :raise => true}, git_url, repo_path)
+      # Handle submodules
+      system("cd #{repo_path}; git submodule init")
     end
+    system("cd #{repo_path}; git submodule update")
   end
 end
