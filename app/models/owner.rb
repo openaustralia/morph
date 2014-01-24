ea3ab5fac703f3c02c574373b74f428c745270be
Matthew Landauer <matthew@openaustralia.org>
2014-01-25 05:26:36 +1100

    Get rid of another n+1 issue

--- app/models/owner.rb
+++ app/models/owner.rb
@@ -3,6 +3,7 @@ class Owner < ActiveRecord::Base
   friendly_id :nickname, use: :finders
 
   has_many :scrapers
+  has_many :runs, through: :scrapers
 
   def github_url
     "https://github.com/#{nickname}"
