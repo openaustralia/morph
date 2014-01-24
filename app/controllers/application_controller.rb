2bf7847896cfdcde354a1e739089ab0460360edf
Matthew Landauer <matthew@openaustralia.org>
2014-01-25 06:26:55 +1100

    Revert "Show profiler info to mlandauer in production"
    This reverts commit 59900c62bffccafc458c12fac37ac9ca3be5026b.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,4 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_filter :authorize_miniprofiler
-
-  def authorize_miniprofiler
-    # Only show Miniprofiler stats in production to mlandauer
-    if current_user && current_user.nickname == "mlandauer"
-      Rack::MiniProfiler.authorize_request
-    end
-  end
 end
