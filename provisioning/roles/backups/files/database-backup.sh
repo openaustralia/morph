282deb52da283503b1d909c182634e00e060605b
Matthew Landauer <matthew@openaustralia.org>
2014-02-06 17:11:02 +1100

    New script to do really basic rolling backup of database

---
+++ provisioning/roles/backups/files/database-backup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+# Backup the database to /backups/mysql and keep the last seven days around
+# This script should be run from a cron job once per days
+
+BACKUP_DIR="/backups/mysql"
+mkdir -p $BACKUP_DIR
+# Created files should just readable (and writeable) by root
+umask 077 
+
+/usr/bin/innobackupex $BACKUP_DIR
+
+no_backups=`ls -1 $BACKUP_DIR | wc -l`
+while [ $no_backups -gt 7 ]
+do
+  oldest=`ls -1rt $BACKUP_DIR | head -n 1`
+  rm -rf $BACKUP_DIR/$oldest
+  no_backups=`ls -1 $BACKUP_DIR | wc -l`
+done
