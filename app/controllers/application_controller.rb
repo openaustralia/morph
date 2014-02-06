c878464db652973a92c9d15a1b54e584f7c91689
Matthew Landauer <matthew@openaustralia.org>
2014-02-07 07:51:42 +1100

    Stay on the same page when signing out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,9 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     user_path(current_user)
   end
+
+  # Stays on the same page after signing out
+  def after_sign_out_path_for(resource_or_scope)
+    request.referrer
+  end
 end
