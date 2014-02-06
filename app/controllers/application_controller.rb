1d0b87e4bc2e2d7ffb756426150c27b1caa76b51
Matthew Landauer <matthew@openaustralia.org>
2014-02-07 07:50:52 +1100

    After signing in redirects to your own page. Fixes #113

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
 end
