f4a5fb0988ad521fb1c0f8433211d375bad31bdd
Matthew Landauer <matthew@openaustralia.org>
2014-02-10 09:15:00 +1100

    Hardcoded desired email text

--- spec/mailers/alert_mailer_spec.rb
+++ spec/mailers/alert_mailer_spec.rb
@@ -11,11 +11,6 @@ describe AlertMailer do
       it { email.from.should == ["contact@morph.io"]}
       it { email.to.should == ["matthew@oaf.org.au"]}
       it { email.subject.should == "Morph: 1 scraper you are watching is erroring" }
-      it do
-        email.body.to_s.should == <<-EOF
-Scrapers have failed
-        EOF
-      end
     end
 
     context "two broken scrapers" do
@@ -23,6 +18,23 @@ Scrapers have failed
       let(:email) { AlertMailer.alert_email(user, broken_scrapers) }
 
       it { email.subject.should == "Morph: 2 scrapers you are watching are erroring" }
+      it do
+        email.body.to_s.should == <<-EOF
+planningalerts-scrapers/campbelltown errored about 2 hours ago
+Fix it: https://morph.io/planningalerts-scrapers/campbelltown
+PHP Fatal error: Call to a member function find() on a non-object in /repo/scraper.php on line 16
+
+planningalerts-scrapers/spear errored about 22 hours ago
+Fix it: https://morph.io/planningalerts-scrapers/spear
+/repo/scraper.rb:98:in `<main>' : undefined method `field_with' for nil:NilClass ( NoMethodError )
+
+32 other scrapers you are watching ran without error
+
+-----
+Change what you're watching - https://morph.io/users/mlandauer/watching
+Morph.io - https://morph.io
+        EOF
+      end
     end
   end  
 end
