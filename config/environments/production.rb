74abe7485456f422d8350d506405aac54415e5db
Matthew Landauer <matthew@openaustralia.org>
2014-02-10 15:08:04 +1100

    Make email links work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,6 @@ Morph::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  config.action_mailer.default_url_options = { :host => 'morph.io', protocol: "https" }
 end
