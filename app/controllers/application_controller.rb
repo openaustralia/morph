1b76b1a17169c31f45f142b45f02e98eae95eaa6
Matthew Landauer <matthew@openaustralia.org>
2014-02-14 06:35:31 +1100

    Now will correctly return to authenticated page after logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def after_sign_in_path_for(resource)
-    user_path(current_user)
-  end
-
   # Stays on the same page after signing out
   def after_sign_out_path_for(resource_or_scope)
     request.referrer
+  end
+
+  # With this when logging in on an unauthenticated page it will redirect you to your own user page
+  # When forced to log in because we were sent to an authenticated page it will redirect back to the
+  # page after logging in
+  def user_root_path
+    user_path(current_user) if current_user
   end
 end
