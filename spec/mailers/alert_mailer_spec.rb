3bdc17f4fdcb6e06c13cba6ba74c99ec981a1e64
Matthew Landauer <matthew@openaustralia.org>
2014-02-13 17:44:27 +1100

    Stop the <pre> block from making everything too wide

--- spec/mailers/alert_mailer_spec.rb
+++ spec/mailers/alert_mailer_spec.rb
@@ -54,13 +54,13 @@ Morph.io - http://dev.morph.io/
 errored about 2 hours ago
 </h2>
 <p><a href="http://dev.morph.io/planningalerts-scrapers/campbelltown">Fix it</a></p>
-<pre>PHP Fatal error: Call to a member function find() on a non-object in /repo/scraper.php on line 16</pre>
+<pre style='word-break: break-word; max-width: 600px;'>PHP Fatal error: Call to a member function find() on a non-object in /repo/scraper.php on line 16</pre>
 <h2>
 <a href="http://dev.morph.io/planningalerts-scrapers/spear">planningalerts-scrapers/spear</a>
 errored about 22 hours ago
 </h2>
 <p><a href="http://dev.morph.io/planningalerts-scrapers/spear">Fix it</a></p>
-<pre>/repo/scraper.rb:98:in `&lt;main&gt;' : undefined method `field_with' for nil:NilClass ( NoMethodError )</pre>
+<pre style='word-break: break-word; max-width: 600px;'>/repo/scraper.rb:98:in `&lt;main&gt;' : undefined method `field_with' for nil:NilClass ( NoMethodError )</pre>
 <p>32 other scrapers you are watching finished successfully</p>
         EOF
         email.html_part.body.to_s.should include(expected)
