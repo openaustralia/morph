3bf1598d9f1d40116694658139ca004d9c27a556
Matthew Landauer <matthew@openaustralia.org>
2014-02-14 06:36:17 +1100

    When signing out return to the home page
    Doing this because if you are on an authenticated page and you log in and you
    stay on the same page (as was the previous behaviour) then it asks you to log
    in again which is kind of weird.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # Stays on the same page after signing out
-  def after_sign_out_path_for(resource_or_scope)
-    request.referrer
-  end
-
   # With this when logging in on an unauthenticated page it will redirect you to your own user page
   # When forced to log in because we were sent to an authenticated page it will redirect back to the
   # page after logging in
