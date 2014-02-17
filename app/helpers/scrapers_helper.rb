8492adfb242959f635f0e7d6a4c3ea0f3f369332
Matthew Landauer <matthew@openaustralia.org>
2014-02-18 09:50:01 +1100

    Extract helper

--- app/helpers/scrapers_helper.rb
+++ app/helpers/scrapers_helper.rb
@@ -5,4 +5,8 @@ module ScrapersHelper
     a += " (".html_safe + link_to("on GitHub", repo.rels[:html].href, target: "_blank") + ")".html_safe
     a
   end
+
+  def full_name_with_links(scraper)
+    link_to(scraper.owner.to_param, scraper.owner) + " / " + link_to(scraper.name, scraper)
+  end
 end
