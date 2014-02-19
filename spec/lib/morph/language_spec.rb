194dfcea17a3caca1f24db339703e57767c7f675
Matthew Landauer <matthew@openaustralia.org>
2014-02-19 15:29:17 +1100

    Add default scraper files for each language

--- spec/lib/morph/language_spec.rb
+++ spec/lib/morph/language_spec.rb
@@ -10,8 +10,8 @@ describe Morph::Language do
   describe ".default_scraper" do
     it do
       Morph::Language.default_scraper(:ruby).should == <<-EOF
-# This is a template for a Ruby scraper on Morph - https://morph.io
-# Including some code snippets below that you should find helpful
+# This is a template for a Ruby scraper on Morph (https://morph.io)
+# including some code snippets below that you should find helpful
 
 # require 'scraperwiki'
 # require 'mechanize'
@@ -40,7 +40,7 @@ describe Morph::Language do
     it do
       Morph::Language.default_scraper(:php).should == <<-EOF
 <?
-// This is a template for a PHP scraper on Morph - https://morph.io
+// This is a template for a PHP scraper on Morph (https://morph.io)
 // including some code snippets below that you should find helpful
 
 // require 'scraperwiki.php';
@@ -65,6 +65,34 @@ describe Morph::Language do
 // is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 // has at least a table called data.
 ?>
+      EOF
+    end
+
+    it do
+      Morph::Language.default_scraper(:python).should == <<-EOF
+# This is a template for a Python scraper on Morph (https://morph.io)
+# including some code snippets below that you should find helpful
+
+# import scraperwiki
+# import lxml.html
+# 
+# # Read in a page
+# html = scraperwiki.scrape("http://foo.com")
+# 
+# # Find something on the page using css selectors
+# root = lxml.html.fromstring(html)
+# root.cssselect("div[align='left']")
+# 
+# # Write out to the sqlite database using scraperwiki library
+# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
+# 
+# # An arbitrary query against the database
+# scraperwiki.sql.select("* from data where 'name'='peter'")
+
+# You don't have to do things with the ScraperWiki and lxml libraries. You can use whatever libraries are installed
+# on Morph for Python (https://github.com/openaustralia/morph-docker-python/blob/master/pip_requirements.txt) and all that matters
+# is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
+# has at least a table called data.
       EOF
     end
   end
