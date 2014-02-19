d1d6edc7204e531e74e5715ef73ac57db22ecf97
Matthew Landauer <matthew@openaustralia.org>
2014-02-20 08:38:18 +1100

    More explicitly split out scrapers into different groups

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,9 +2,18 @@ class OwnersController < ApplicationController
   def show
     @owner = Owner.find(params[:id])
     @scrapers = @owner.scrapers
-    @running_scrapers = @scrapers.select{|s| s.running?}
-    @erroring_scrapers = @scrapers.select{|s| s.requires_attention?}
-    @other_scrapers = @scrapers.select{|s| !s.requires_attention? && !s.running?}
+
+    # Split out scrapers into different groups
+    @running_scrapers, @erroring_scrapers, @other_scrapers = [], [], []
+    @scrapers.each do |scraper|
+      if scraper.running?
+        @running_scrapers << scraper
+      elsif scraper.requires_attention?
+        @erroring_scrapers << scraper
+      else
+        @other_scrapers << scraper
+      end
+    end
   end
 
   # Toggle whether we're watching this user / organization
