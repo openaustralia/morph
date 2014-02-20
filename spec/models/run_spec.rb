45d043612f84f83d7a350dc1243a64e3edec8ac3
Matthew Landauer <matthew@openaustralia.org>
2014-02-20 16:22:37 +1100

    Simple test for Run#wall_time

--- spec/models/run_spec.rb
+++ spec/models/run_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe Run do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "#wall_time" do
+    context "run with a start and end 1 minute apart" do
+      let(:run) { Run.new(id: 1, started_at: 5.minutes.ago, finished_at: 4.minutes.ago)}
+      it { run.wall_time.should be_within(0.1).of(60) }
+    end
+
+    context "run started but not finished" do
+      let(:run) { Run.new(id: 1, started_at: 5.minutes.ago)}
+      it { run.wall_time.should == 0 }
+    end
+
+    context "run queued but not started" do
+      let(:run) { Run.new(id: 1)}
+      it { run.wall_time.should == 0 }
+    end
+  end
 end
