7aeb347a7ce859dec2f59a29c02be2bb0acb4672
Matthew Landauer <matthew@openaustralia.org>
2014-03-03 12:35:15 +1100

    Extract helper for making a link look like a button
    Also turns it into a span if it's disabled

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,16 @@ module ApplicationHelper
   def duration_of_time_in_words(secs)
     distance_of_time_in_words(0, secs, include_seconds: true)
   end
+
+  def button_link_to(name = nil, options = {}, html_options = {}, &block)    
+    name, options, html_options = capture(&block), name, options if block_given?
+    html_options[:class] ||= ""
+    html_options[:class] += " btn btn-default"
+
+    if html_options[:disabled]
+      content_tag(:span, name, html_options)
+    else
+      link_to(name, options, html_options)
+    end
+  end
 end
