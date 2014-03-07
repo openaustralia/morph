a790f665514d79f82d8bd4b374796d5740dcc696
Matthew Landauer <matthew@openaustralia.org>
2014-03-07 21:27:36 +1100

    Whitespace change only

--- lib/morph/language.rb
+++ lib/morph/language.rb
@@ -65,10 +65,10 @@ module Morph
 
 # require 'scraperwiki'
 # require 'mechanize'
-# 
+#
 # agent = Mechanize.new
 #
-# # Read in a page 
+# # Read in a page
 # page = agent.get("http://foo.com")
 #
 # # Find somehing on the page using css selectors
@@ -93,21 +93,21 @@ module Morph
 
 // require 'scraperwiki.php';
 // require 'scraperwiki/simple_html_dom.php';
-// 
+//
 // // Read in a page
 // $html = scraperwiki::scrape("http://foo.com");
-// 
+//
 // // Find something on the page using css selectors
 // $dom = new simple_html_dom();
 // $dom->load($html);
 // print_r($dom->find("table.list"));
-// 
+//
 // // Write out to the sqlite database using scraperwiki library
 // scraperwiki::save_sqlite(array('name'), array('name' => 'susan', 'occupation' => 'software developer'));
-// 
+//
 // // An arbitrary query against the database
 // scraperwiki::select("* from data where 'name'='peter'")
- 
+
 // You don't have to do things with the ScraperWiki library. You can use whatever is installed
 // on Morph for PHP (See https://github.com/openaustralia/morph-docker-php) and all that matters
 // is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
@@ -121,17 +121,17 @@ module Morph
 
 # import scraperwiki
 # import lxml.html
-# 
+#
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
-# 
+#
 # # Find something on the page using css selectors
 # root = lxml.html.fromstring(html)
 # root.cssselect("div[align='left']")
-# 
+#
 # # Write out to the sqlite database using scraperwiki library
 # scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-# 
+#
 # # An arbitrary query against the database
 # scraperwiki.sql.select("* from data where 'name'='peter'")
 
