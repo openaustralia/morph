14dd0caabbf2241f7bf391cb4d37c62e3cdfb349
Henare Degan <henare.degan@gmail.com>
2014-03-21 13:05:07 +1100

    Install factory_girl

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -43,4 +43,15 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
+
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
 end
