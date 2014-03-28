e0cc20abc1755f5f08a779e1f4da306ff882f545
Henare Degan <henare.degan@gmail.com>
2014-03-28 14:47:25 +1100

    Remove test as it's expectation is deprecated

--- spec/lib/morph/database_spec.rb
+++ spec/lib/morph/database_spec.rb
@@ -2,12 +2,6 @@ require 'spec_helper'
 
 describe Morph::Database do
   describe '#clear' do
-    it "should not raise an error if there's no file" do
-      VCR.use_cassette('scraper_validations', allow_playback_repeats: true) do
-        expect { Morph::Database.new(create(:scraper)).clear }.not_to raise_error
-      end
-    end
-
     it "should attempt to remove the file if it's not there" do
       FileUtils.should_not_receive(:rm)
       VCR.use_cassette('scraper_validations', allow_playback_repeats: true) do
