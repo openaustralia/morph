fdceef7035b54f474e538f8c3c2cef55d7a6d69e
Matthew Landauer <matthew@openaustralia.org>
2014-04-02 14:58:13 +1100

    Reintroduce url for testing streaming

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Morph::Application.routes.draw do
   get "/pricing", to: "documentation#pricing"
   # Hmm not totally sure about this url.
   post "/run", to: "api#run_remote"
+  get "/test", to: "api#test"
   get '/settings', to: "users#settings", as: :user_settings
   post '/settings/reset_key', to: "users#reset_key", as: :user_reset_key
 
