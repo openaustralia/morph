985dad230367a4646d5ac777c4cd1a5be1af3375
Matthew Landauer <matthew@openaustralia.org>
2014-04-03 12:44:40 +1100

    Move secrets to .env

--- app/controllers/discourse_sso_controller.rb
+++ app/controllers/discourse_sso_controller.rb
@@ -2,7 +2,7 @@ class DiscourseSsoController < ApplicationController
   before_filter :authenticate_user!
 
   def sso
-    secret = "hellomatthew"
+    secret = ENV["DISCOURSE_SECRET"]
     sso = Discourse::SingleSignOn.parse(request.query_string, secret)
     sso.email = current_user.email
     sso.name = current_user.name
@@ -10,6 +10,6 @@ class DiscourseSsoController < ApplicationController
     sso.external_id = current_user.id # unique to your application
     sso.sso_secret = secret
 
-    redirect_to sso.to_url("http://discuss.dev.morph.io/session/sso_login")
+    redirect_to sso.to_url("#{ENV['DISCOURSE_URL']}/session/sso_login")
   end
 end
