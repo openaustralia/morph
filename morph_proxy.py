fd5674a1b36498e5d3a597203c180ded8c57d058
Matthew Landauer <matthew@oaf.org.au>
2014-04-22 08:11:59 +1000

    Add request and response size

--- morph_proxy.py
+++ morph_proxy.py
@@ -1,15 +1,5 @@
 # Run this with mitmdump -q -s morph_proxy.py
 
-def request(context, flow):
-  # print out all the basic information to determine what request is being made
-  # coming from which container
-  # print flow.request.method
-  # print flow.request.host
-  # print flow.request.path
-  # print flow.request.scheme
-  # print flow.request.client_conn.address[0]
-  # print "***"
-  #text = flow.request.method + " " + flow.request.scheme + "://" + flow.request.host + flow.request.path + " FROM " + flow.request.client_conn
-  text = flow.request.method + " " + flow.request.scheme + "://" + flow.request.host + flow.request.path + " FROM " + flow.request.client_conn.address[0]
+def response(context, flow):
+  text = flow.request.method + " " + flow.request.scheme + "://" + flow.request.host + flow.request.path + " FROM " + flow.request.client_conn.address[0] + " REQUEST SIZE " + str(len(flow.request.content)) + " RESPONSE SIZE " + str(len(flow.response.content))
   print text
-  # print "***"
