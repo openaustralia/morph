215724f6758f8f716b7d7ed4793e00b476b953f4
Matthew Landauer <matthew@oaf.org.au>
2014-04-22 12:14:58 +1000

    Remove debugging output

--- Procfile
+++ Procfile
@@ -1,3 +1,4 @@
 worker: bundle exec sidekiq --concurrency 5 -q default -q low
 web: bundle exec rails s
 faye: bundle exec dotenv rackup sync.ru -E production
+mitmproxy: dotenv mitmdump -q -s mitmproxy/log_to_morph.py
--- mitmproxy/log_to_morph.py
+++ mitmproxy/log_to_morph.py
@@ -6,8 +6,6 @@ from libmproxy.script import concurrent
 
 @concurrent
 def response(context, flow):
-  text = flow.request.method + " " + flow.request.scheme + "://" + flow.request.host + flow.request.path + " FROM " + flow.request.client_conn.address[0] + " REQUEST SIZE " + str(len(flow.request.content)) + " RESPONSE SIZE " + str(len(flow.response.content))
-  print text
   url = "http://localhost:3000/connection_logs"
   params = urllib.urlencode({
   	'ip_address': flow.request.client_conn.address[0],
