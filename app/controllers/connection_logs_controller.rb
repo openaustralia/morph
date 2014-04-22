32a0413978bb63f33cc3b2ec3a04e256ba6ca491
Matthew Landauer <matthew@oaf.org.au>
2014-04-22 11:55:23 +1000

    Check mitmproxy key on morph side

--- app/controllers/connection_logs_controller.rb
+++ app/controllers/connection_logs_controller.rb
@@ -2,15 +2,25 @@ class ConnectionLogsController < ApplicationController
   skip_before_filter :verify_authenticity_token
 
   def create
-    ConnectionLog.create(
-      ip_address: params[:ip_address],
-      method: params[:method],
-      scheme: params[:scheme],
-      host: params[:host],
-      path: params[:path],
-      request_size: params[:request_size],
-      response_size: params[:response_size]
-    )
-    render text: "Created"
+    if ConnectionLogsController.key == params[:key]
+      ConnectionLog.create(
+        ip_address: params[:ip_address],
+        method: params[:method],
+        scheme: params[:scheme],
+        host: params[:host],
+        path: params[:path],
+        request_size: params[:request_size],
+        response_size: params[:response_size]
+      )
+      render text: "Created"
+    else
+      render text: "Wrong API key", status: 401
+    end
+  end
+
+  private
+
+  def self.key
+    ENV["MITMPROXY_SECRET"]
   end
 end
--- spec/controllers/connection_logs_controller_spec.rb
+++ spec/controllers/connection_logs_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe ConnectionLogsController do
+  describe "#create" do
+    before(:each) { ConnectionLogsController.stub(key: "sjdf")}
 
+    it "should be successful if correct key is used" do
+      post :create, key: "sjdf"
+      response.should be_successful
+    end
+
+    it "should not be successful if wrong key is used" do
+      post :create, key: "foo"
+      response.should_not be_successful
+    end
+  end
 end
