e184963611f03e83785207d935247af4f24c7259
Matthew Landauer <matthew@oaf.org.au>
2014-04-22 11:35:11 +1000

    Turn on threading for proxy response handling

--- morph_proxy.py
+++ morph_proxy.py
@@ -1,7 +1,9 @@
 # Run this with mitmdump -q -s morph_proxy.py
 
 import urllib
+from libmproxy.script import concurrent
 
+@concurrent
 def response(context, flow):
   text = flow.request.method + " " + flow.request.scheme + "://" + flow.request.host + flow.request.path + " FROM " + flow.request.client_conn.address[0] + " REQUEST SIZE " + str(len(flow.request.content)) + " RESPONSE SIZE " + str(len(flow.response.content))
   print text
