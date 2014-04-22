c2cf0ca2d4e5ea5a91091264edca0a5be3efcc94
Matthew Landauer <matthew@oaf.org.au>
2014-04-22 10:29:04 +1000

    Add automated conversion of ip_address -> run_id

--- app/models/connection_log.rb
+++ app/models/connection_log.rb
@@ -1,2 +1,11 @@
 class ConnectionLog < ActiveRecord::Base
+  attr_accessor :ip_address
+
+  before_save :update_run_id_from_ip_address
+
+  def update_run_id_from_ip_address
+    if run_id.nil?
+      self.run_id = Run.where(ip_address: self.ip_address).order(started_at: :desc).first.id
+    end
+  end
 end
--- spec/models/connection_log_spec.rb
+++ spec/models/connection_log_spec.rb
@@ -1,5 +1,22 @@
 require 'spec_helper'
 
 describe ConnectionLog do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".create" do
+    before(:each) do
+      Run.create(id: 18, ip_address: "10.0.1.15", started_at: Date.new(2014,1,1))
+      # This is the most recent use of 10.0.1.15
+      Run.create(id: 20, ip_address: "10.0.1.15", started_at: Date.new(2014,2,1))
+      Run.create(id: 22, ip_address: "10.0.1.12", started_at: Date.new(2014,3,1))
+    end
+
+    it "should convert ip_address to run_id" do
+      a = ConnectionLog.create(ip_address: "10.0.1.15", host: "foo.com")
+      a.run_id.should == 20
+    end
+
+    it "should not convert ip_address if run_id is already set" do
+      a = ConnectionLog.create(ip_address: "10.0.1.15", run_id: 40, host: "foo.com")
+      a.run_id.should == 40
+    end
+  end
 end
