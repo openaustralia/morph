fcc73dd5fe7383e80761048cc6175f1fdc056df5
Matthew Landauer <matthew@oaf.org.au>
2014-04-26 17:35:51 +1000

    Make this work

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Morph::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  config.assets.precompile += %w( examples/style.css.scss )
+  config.assets.precompile += %w( examples/style.css )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
