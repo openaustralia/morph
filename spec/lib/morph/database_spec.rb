b4d339e6a61bf3835f288751e731c8fdb43706ef
Henare Degan <henare.degan@gmail.com>
2014-04-28 17:44:11 +1000

    Correct spec description

--- spec/lib/morph/database_spec.rb
+++ spec/lib/morph/database_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 describe Morph::Database do
   describe '#clear' do
-    it "should attempt to remove the file if it's not there" do
+    it "should not attempt to remove the file if it's not there" do
       FileUtils.should_not_receive(:rm)
       VCR.use_cassette('scraper_validations', allow_playback_repeats: true) do
         Morph::Database.new(create(:scraper).data_path).clear
