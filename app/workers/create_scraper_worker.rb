a19463c6c3d2b283389213dc32b9da1ed7ba56eb
Matthew Landauer <matthew@oaf.org.au>
2014-05-05 15:36:20 +1000

    Handle errors in background job

--- app/workers/create_scraper_worker.rb
+++ app/workers/create_scraper_worker.rb
@@ -3,27 +3,37 @@ class CreateScraperWorker
 
   def perform(scraper_id, current_user_id, scraper_url)
     # TODO Handle this failing half way through and rerunning
-    
+
     scraper = Scraper.find(scraper_id)
     current_user = User.find(current_user_id)
 
-    scraper.create_scraper_progress.update("Creating Github repository", 20)
-    repo = Morph::Github.create_repository(current_user, scraper.owner, scraper.name)
+    # Checking progress here as a crude way to see if this background process previously
+    # failed part of the way through.
+    # TODO Do this in a less hacky and more general way
+    if scraper.create_scraper_progress.progress <= 20
+      scraper.create_scraper_progress.update("Creating Github repository", 20)
+      repo = Morph::Github.create_repository(current_user, scraper.owner, scraper.name)
+    end
+
+    # This block should happily run several times (after failures)
+    scraper.create_scraper_progress.update("Add scraper template", 40)
     files = {
       Morph::Language.language_to_scraper_filename(scraper.original_language.to_sym) => Morph::Language.default_scraper(scraper.original_language.to_sym),
       ".gitignore" => "# Ignore output of scraper\n#{Morph::Database.sqlite_db_filename}\n",
+      # TODO Don't use hardcoded urls
+      "README.md" => "This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)"
     }
-    # TODO Don't use hardcoded urls
-    files["README.md"] = "This is a scraper that runs on [Morph](https://morph.io). To get started [see the documentation](https://morph.io/documentation)"
-    scraper.create_scraper_progress.update("Add scraper template", 40)
     scraper.add_commit_to_root_on_github(current_user, files, "Add template for Morph scraper")
 
+    # This block should happily run several times (after failures)
     scraper.create_scraper_progress.update("Get repository info", 60)
     scraper2 = Scraper.new_from_github(repo.full_name, current_user.octokit_client)
     # Copy the new data across
     scraper.update_attributes(description: scraper2.description, github_id: scraper2.github_id,
       owner_id: scraper2.owner_id, github_url: scraper2.github_url, git_url: scraper2.git_url)
     repo = current_user.octokit_client.edit_repository(scraper.full_name, homepage: scraper_url)
+
+    # This block should happily run several times (after failures)
     scraper.create_scraper_progress.update("Synching repository", 80)
     scraper.synchronise_repo
     scraper.create_scraper_progress.finished
