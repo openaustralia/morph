fc1d040541c204f5d075aaded3286ce5d8133eae
Matthew Landauer <matthew@oaf.org.au>
2014-05-17 16:36:07 +1000

    An environment variable needs to have a value

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,3 +1,4 @@
 class Variable < ActiveRecord::Base
   validates :name, format: {with: /\AMORPH_[A-Z0-9_]+\z/, message: "should look something like MORPH_SEAGULL"}
+  validates :value, presence: true
 end
