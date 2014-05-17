8093e2947bfe310dfd2dc61abbc645e49b575f3d
Matthew Landauer <matthew@oaf.org.au>
2014-05-17 15:55:09 +1000

    Allow numbers in environment variable names as well

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,3 +1,3 @@
 class Variable < ActiveRecord::Base
-  validates :name, format: {with: /\AMORPH_[A-Z_]+\z/, message: "should look something like MORPH_SEAGULL"}
+  validates :name, format: {with: /\AMORPH_[A-Z0-9_]+\z/, message: "should look something like MORPH_SEAGULL"}
 end
