4fe878d51e3b820aab7a3ee4193ef488fed388a8
Matthew Landauer <matthew@oaf.org.au>
2014-05-19 08:18:04 +1000

    Simplify display

--- app/helpers/scrapers_helper.rb
+++ app/helpers/scrapers_helper.rb
@@ -1,6 +1,6 @@
 module ScrapersHelper
   def radio_description(repo)
-    a = content_tag(:strong, repo.full_name)
+    a = content_tag(:strong, repo.name)
     a += " &mdash; #{repo.description}".html_safe unless repo.description.blank?
     a += " (".html_safe + link_to("on GitHub", repo.rels[:html].href, target: "_blank") + ")".html_safe
     a
