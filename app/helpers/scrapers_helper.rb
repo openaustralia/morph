81d977197f0879c58787f2bcc3ceac8215025ae0
Matthew Landauer <matthew@oaf.org.au>
2014-05-23 16:15:01 +1000

    In add from github disable repos we've already added

--- app/helpers/scrapers_helper.rb
+++ app/helpers/scrapers_helper.rb
@@ -1,9 +1,16 @@
 module ScrapersHelper
   def radio_description(repo)
-    a = content_tag(:strong, repo.name)
-    a += " &mdash; #{repo.description}".html_safe unless repo.description.blank?
-    a += " (".html_safe + link_to("on GitHub", repo.rels[:html].href, target: "_blank") + ")".html_safe
-    a
+    scraper = Scraper.where(full_name: repo.full_name).first
+    if scraper
+      a = content_tag(:strong, repo.name)
+      a += " &mdash; #{repo.description}".html_safe unless repo.description.blank?
+      content_tag(:p, a, class: "text-muted")
+    else
+      a = content_tag(:strong, repo.name)
+      a += " &mdash; #{repo.description}".html_safe unless repo.description.blank?
+      a += " (".html_safe + link_to("on GitHub", repo.rels[:html].href, target: "_blank") + ")".html_safe
+      a
+    end
   end
 
   def full_name_with_links(scraper)
