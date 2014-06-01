96f3b9babdbbf85a0285a19fdcb2bdb8e48e5ca0
Matthew Landauer <matthew@oaf.org.au>
2014-06-02 08:25:34 +1000

    Remove commented out routes

--- config/routes.rb
+++ config/routes.rb
@@ -72,8 +72,6 @@ Morph::Application.routes.draw do
 
   resources :connection_logs, only: :create
 
-  #resources "scraperwiki_forks", only: [:new, :create]
-
   get '/*id/data', to: "scrapers#data", as: :scraper_data
   post '/*id/watch', to: "scrapers#watch", as: :scraper_watch
   get '/*id/watchers', to: "scrapers#watchers", as: :scraper_watchers
@@ -84,52 +82,4 @@ Morph::Application.routes.draw do
   post "/*id/run", to: "scrapers#run", as: :run_scraper
   post "/*id/stop", to: "scrapers#stop", as: :stop_scraper
   post "/*id/clear", to: "scrapers#clear", as: :clear_scraper
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
