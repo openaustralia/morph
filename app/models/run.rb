a4c16441cf7539486bd51fabacb2612b0c6047cc
Matthew Landauer <matthew@oaf.org.au>
2014-06-03 18:44:10 +1000

    Get methods ready to be moved to a different class

--- app/models/run.rb
+++ app/models/run.rb
@@ -184,11 +184,11 @@ class Run < ActiveRecord::Base
 
     # Only try using buildpacks with ruby for the time being
     if owner.buildpacks && language == :ruby
-      go_with_logging_with_buildpacks do |s,c|
+      Run.go_with_logging_with_buildpacks(self) do |s,c|
         yield s,c
       end
     else
-      go_with_logging_original do |s,c|
+      Run.go_with_logging_original(self) do |s,c|
         yield s,c
       end
     end
@@ -220,30 +220,30 @@ class Run < ActiveRecord::Base
     end
   end
 
-  def go_with_logging_original
-    command = Metric.command(Morph::Language.scraper_command(language), Run.time_output_filename)
+  def self.go_with_logging_original(run)
+    command = Metric.command(Morph::Language.scraper_command(run.language), Run.time_output_filename)
     status_code = Morph::DockerRunner.run(
       command: command,
       user: "scraper",
-      image_name: docker_image,
-      container_name: docker_container_name,
-      repo_path: repo_path,
-      data_path: data_path,
-      env_variables: scraper.variables.map{|v| [v.name, v.value]}
+      image_name: run.docker_image,
+      container_name: run.docker_container_name,
+      repo_path: run.repo_path,
+      data_path: run.data_path,
+      env_variables: run.scraper.variables.map{|v| [v.name, v.value]}
     ) do |on|
         on.log { |s,c| yield s, c}
         on.ip_address do |ip|
           # Store the ip address of the container for this run
-          update_attributes(ip_address: ip)
+          run.update_attributes(ip_address: ip)
         end
     end
   end
 
-  def go_with_logging_with_buildpacks
+  def self.go_with_logging_with_buildpacks(run)
     # Compile the container
     i = Docker::Image.get('openaustralia/buildstep')
     # Insert the configuration part of the application code into the container
-    tar_path = tar_config_files
+    tar_path = run.tar_config_files
     hash = Digest::SHA2.hexdigest(File.read(tar_path))
 
     # Check if compiled image already exists
@@ -274,10 +274,10 @@ class Run < ActiveRecord::Base
 
     # Insert the actual code into the container
     i = Docker::Image.get("compiled_#{hash}")
-    tar_path = tar_run_files
+    tar_path = run.tar_run_files
     # TODO insert_local produces a left-over container. Fix this.
     i2 = i.insert_local('localPath' => tar_path, 'outputPath' => '/app')
-    i2.tag('repo' => "compiled2_#{id}")
+    i2.tag('repo' => "compiled2_#{run.id}")
     FileUtils.rm_f(tar_path)
 
     command = Metric.command("/start scraper", "/data/" + Run.time_output_filename)
@@ -285,19 +285,19 @@ class Run < ActiveRecord::Base
       command: command,
       # TODO Need to run this as the user scraper again
       user: "root",
-      image_name: "compiled2_#{id}",
-      container_name: docker_container_name,
-      data_path: data_path,
-      env_variables: scraper.variables.map{|v| [v.name, v.value]}
+      image_name: "compiled2_#{run.id}",
+      container_name: run.docker_container_name,
+      data_path: run.data_path,
+      env_variables: run.scraper.variables.map{|v| [v.name, v.value]}
     ) do |on|
         on.log { |s,c| yield s, c}
         on.ip_address do |ip|
           # Store the ip address of the container for this run
-          update_attributes(ip_address: ip)
+          run.update_attributes(ip_address: ip)
         end
     end
 
-    i = Docker::Image.get("compiled2_#{id}")
+    i = Docker::Image.get("compiled2_#{run.id}")
     i.delete
   end
 
