9e1a942f2474e4addbec41196f4212bb096e3e2f
Matthew Landauer <matthew@oaf.org.au>
2014-06-19 12:11:24 +1000

    Extract methods

--- app/models/site_setting.rb
+++ app/models/site_setting.rb
@@ -2,14 +2,28 @@ class SiteSetting < ActiveRecord::Base
   serialize :settings
 
   def self.read_only_mode
-    record.settings["read_only_mode"]
+    read_setting("read_only_mode")
   end
 
   def self.read_only_mode=(mode)
-    record.update_attributes(settings: record.settings.merge({"read_only_mode" => mode}))
+    write_setting("read_only_mode", mode)
   end
 
+  private
+  
   def self.record
-    SiteSetting.first || SiteSetting.create(settings: {"read_only_mode" => false})
+    SiteSetting.first || SiteSetting.create(settings: defaults)
+  end
+
+  def self.read_setting(key)
+    record.settings[key]
+  end
+
+  def self.write_setting(key, value)
+    record.update_attributes(settings: record.settings.merge({key => value}))
+  end
+
+  def self.defaults
+    {"read_only_mode" => false}
   end
 end
