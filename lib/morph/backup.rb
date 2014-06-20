63f08f8093b814d71bcc9e4436361f6b876aac7f
Matthew Landauer <matthew@oaf.org.au>
2014-06-20 13:00:30 +1000

    Gets db config from rails and removes any previous backup before starting new one

--- lib/morph/backup.rb
+++ lib/morph/backup.rb
@@ -4,9 +4,12 @@ module Morph
       unless SiteSetting.read_only_mode
         puts "WARNING: The site is NOT in read-only mode. So, things might get updated during the backup."
       end
+      puts "Removing any previous backup..."
+      FileUtils.rm_f("db/backups/mysql_backup.sql")
+      FileUtils.rm_f("db/backups/mysql_backup.sql.bz2")
       FileUtils.mkdir_p("db/backups")
       puts "Backing up MySQL..."
-      system("mysqldump -u scraping -pscraping scraping_development > db/backups/mysql_backup.sql")
+      system("mysqldump -u #{mysql_username} -p#{mysql_password} #{mysql_database} > db/backups/mysql_backup.sql")
       puts "Compressing MySQL backup..."
       system("bzip2 db/backups/mysql_backup.sql")
     end
@@ -15,8 +18,26 @@ module Morph
       puts "Uncompressing MySQL backup..."
       system("bunzip2 -k db/backups/mysql_backup.sql.bz2")
       puts "Restoring from MySQL backup..."
-      system("mysql -u scraping -pscraping scraping_development < db/backups/mysql_backup.sql")
+      system("mysql -u #{mysql_username} -p#{mysql_password} #{mysql_database} < db/backups/mysql_backup.sql")
       FileUtils.rm_f("db/backups/mysql_backup.sql")
+    end
+
+    private
+
+    def self.mysql_configuration
+      Rails.configuration.database_configuration[Rails.env]
+    end
+
+    def self.mysql_database
+      mysql_configuration["database"]
+    end
+
+    def self.mysql_username
+      mysql_configuration["username"]
+    end
+
+    def self.mysql_password
+      mysql_configuration["password"]
     end
   end
 end
