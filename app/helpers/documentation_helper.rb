2dee3b50eee92772698fdd013d92b6791270eaa6
Matthew Landauer <matthew@oaf.org.au>
2014-06-26 11:15:02 +1000

    Add improve buttons

--- app/helpers/documentation_helper.rb
+++ app/helpers/documentation_helper.rb
@@ -1,2 +1,15 @@
 module DocumentationHelper
+  def improve_button(text, file, options = {})
+    if options.has_key?(:spacer)
+      spacer = options[:spacer]
+    else
+      spacer = true
+    end
+    if spacer
+      c = "btn btn-default improve pull-right"
+    else
+      c = "btn btn-default pull-right"
+    end
+    link_to text, "https://github.com/openaustralia/morph/blob/master/app/views/documentation/#{file}", class: c
+  end
 end
