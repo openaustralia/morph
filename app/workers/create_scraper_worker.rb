b88db19be60c550e13b48b0de6a9aeffd04bb41d
Henare Degan <henare.degan@gmail.com>
2015-02-18 09:21:01 +1100

    On retries `repo` is not set so get `full_name` from `scraper`

--- app/workers/create_scraper_worker.rb
+++ app/workers/create_scraper_worker.rb
@@ -25,7 +25,7 @@ class CreateScraperWorker
 
     # This block should happily run several times (after failures)
     scraper.create_scraper_progress.update("Get repository info", 60)
-    scraper2 = Scraper.new_from_github(repo.full_name, current_user.octokit_client)
+    scraper2 = Scraper.new_from_github(scraper.full_name, current_user.octokit_client)
     # Copy the new data across
     scraper.update_attributes(description: scraper2.description, github_id: scraper2.github_id,
       owner_id: scraper2.owner_id, github_url: scraper2.github_url, git_url: scraper2.git_url)
