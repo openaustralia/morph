efc1bb6d9cae57f748791aed295f6e1582d7d81f
Matthew Landauer <matthew@oaf.org.au>
2015-03-20 09:52:29 +1100

    Try to see if we can set full_host differently on local vm

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,12 @@
 # HACK Temporary workaround until I figure out the root cause
 if Rails.env.production?
-  OmniAuth.config.full_host = "https://morph.io"
+  OmniAuth.config.full_host = Proc.new do |env|
+    # Horrible hack to deal with the difference between production and local vm
+    puts "HTTP_HOST: #{env['HTTP_HOST']}"
+    if env["HTTP_HOST"] == "dev.morph.io"
+      "https://dev.morph.io"
+    else
+      "https://morph.io"
+    end
+  end
 end
