9e03521f9889a802a1d33726119e03bad679c82f
Matthew Landauer <matthew@oaf.org.au>
2015-03-20 09:59:28 +1100

    Make hack a bit more general

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,12 +1,6 @@
 # HACK Temporary workaround until I figure out the root cause
 if Rails.env.production?
   OmniAuth.config.full_host = Proc.new do |env|
-    # Horrible hack to deal with the difference between production and local vm
-    puts "HTTP_HOST: #{env['HTTP_HOST']}"
-    if env["HTTP_HOST"] == "dev.morph.io"
-      "https://dev.morph.io"
-    else
-      "https://morph.io"
-    end
+    "https://" + env["HTTP_HOST"]
   end
 end
