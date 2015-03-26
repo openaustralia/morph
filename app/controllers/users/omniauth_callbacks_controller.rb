1db3211005af80b44875df81b0887104f82b6c68
Matthew Landauer <matthew@oaf.org.au>
2015-03-26 18:42:32 +1100

    Keep people signed in. Fixes #497

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,10 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
+  include Devise::Controllers::Rememberable
+
   def github
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
+    # Keep people signed in
+    remember_me(@user)
 
     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
     flash[:notice] = "Nice person you are. Welcome!"
