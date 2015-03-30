ad698ee5ba0a86050ef4318d2fdd9303c13baa8a
Matthew Landauer <matthew@oaf.org.au>
2015-03-31 09:23:02 +1100

    Extract legacy container compiler into its own class

---
+++ lib/morph/container_compiler_new/legacy.rb
@@ -0,0 +1,30 @@
+module Morph
+  module ContainerCompilerNew
+    class Legacy
+      def self.docker_container_name(run)
+        "#{run.owner.to_param}_#{run.name}_#{run.id}"
+      end
+
+      def self.compile_and_run(run)
+        wrapper = Multiblock.wrapper
+        yield(wrapper)
+
+        command = Metric.command(run.language.scraper_command, Run.time_output_filename)
+        env_variables = run.scraper ? run.scraper.variables.map{|v| [v.name, v.value]} : []
+        status_code = Morph::DockerRunner.run(
+          command: command,
+          user: "scraper",
+          image_name: run.language.docker_image,
+          container_name: docker_container_name(run),
+          repo_path: run.repo_path,
+          data_path: run.data_path,
+          env_variables: env_variables
+        ) do |on|
+            on.log {|s,c| wrapper.call(:log, s, c)}
+            on.ip_address {|ip| wrapper.call(:ip_address, ip)}
+        end
+        status_code
+      end
+    end
+  end
+end
