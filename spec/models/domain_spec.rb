92431f2992320b4b299b54b8c152425bc6f0d77e
Matthew Landauer <matthew@oaf.org.au>
2015-04-17 11:20:32 +1000

    Test extracted method

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -1,4 +1,26 @@
 require 'spec_helper'
 
 describe Domain do
+  describe ".lookup_meta" do
+    context "domain hasn't been seen before" do
+      it do
+        RestClient.should_receive(:get).with("http://morph.io").and_return("<html><head><meta name='Description' content='Get structured data out of the web. Code collaboration through GitHub. Run your scrapers in the cloud.'></head></html>")
+        Domain.lookup_meta("morph.io").should == "Get structured data out of the web. Code collaboration through GitHub. Run your scrapers in the cloud."
+        Domain.count.should == 1
+        domain = Domain.first
+        domain.name.should == "morph.io"
+        domain.meta.should == "Get structured data out of the web. Code collaboration through GitHub. Run your scrapers in the cloud."
+      end
+    end
+
+    context "domain has been seen before" do
+      before(:each) { Domain.create!(name: "morph.io", meta: "Foo")}
+
+      it do
+        RestClient.should_not_receive(:get)
+        Domain.lookup_meta("morph.io").should == "Foo"
+        Domain.count.should == 1
+      end
+    end
+  end
 end
--- spec/workers/new_domain_worker_spec.rb
+++ spec/workers/new_domain_worker_spec.rb
@@ -1,27 +1,4 @@
 require 'spec_helper'
 
 describe NewDomainWorker do
-  let(:worker) { NewDomainWorker.new }
-  describe "#perform" do
-    context "domain hasn't been seen before" do
-      it do
-        RestClient.should_receive(:get).with("http://morph.io").and_return("<html><head><meta name='Description' content='Get structured data out of the web. Code collaboration through GitHub. Run your scrapers in the cloud.'></head></html>")
-        worker.perform("morph.io")
-        Domain.count.should == 1
-        domain = Domain.first
-        domain.name.should == "morph.io"
-        domain.meta.should == "Get structured data out of the web. Code collaboration through GitHub. Run your scrapers in the cloud."
-      end
-    end
-
-    context "domain has been seen before" do
-      before(:each) { Domain.create!(name: "morph.io", meta: "Foo")}
-
-      it do
-        RestClient.should_not_receive(:get)
-        worker.perform("morph.io")
-        Domain.count.should == 1
-      end
-    end
-  end
 end
