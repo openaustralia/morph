a74754c3573f2624dceadc3a142861dd160dc4c7
Matthew Landauer <matthew@oaf.org.au>
2015-04-17 11:22:40 +1000

    Extract method

--- app/workers/new_domain_worker.rb
+++ app/workers/new_domain_worker.rb
@@ -3,12 +3,6 @@ class NewDomainWorker
 
   # Look up meta info for a domain
   def perform(domain_name)
-    # TODO If the last time the meta info was grabbed was a long time ago, refresh it
-    unless Domain.where(name: domain_name).exists?
-      doc = RestClient.get("http://#{domain_name}")
-      tag = Nokogiri::HTML(doc).at("html head meta[name='Description']")
-      meta = tag["content"] if tag
-      Domain.create!(name: domain_name, meta: meta)
-    end
+    Domain.lookup_meta(domain_name)
   end
 end
