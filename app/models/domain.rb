087ace67401ddfa140f386ed9c0cc68551d4ec95
Matthew Landauer <matthew@oaf.org.au>
2015-04-17 11:41:15 +1000

    Handle internal server error and require nokogiri for background job

--- app/models/domain.rb
+++ app/models/domain.rb
@@ -1,10 +1,18 @@
+# For the benefit of NewDomainWorker
+require "nokogiri"
+
 class Domain < ActiveRecord::Base
   # Lookup and cache meta information for a domain
   def self.lookup_meta(domain_name)
     # TODO If the last time the meta info was grabbed was a long time ago, refresh it
     domain = find_by(name: domain_name)
     if domain.nil?
-      doc = RestClient.get("http://#{domain_name}")
+      begin
+        doc = RestClient.get("http://#{domain_name}")
+      rescue RestClient::InternalServerError
+        doc = ""
+      end
+
       tag = Nokogiri::HTML(doc).at("html head meta[name='Description']")
       meta = tag["content"] if tag
       domain = create!(name: domain_name, meta: meta)
