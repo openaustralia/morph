33bc77ac63382b511a46a8f5c9354bc284762ef1
Matthew Landauer <matthew@oaf.org.au>
2015-04-20 12:06:59 +1000

    Add domain_id field to connection_logs

---
+++ db/migrate/20150420013101_add_domain_id_to_connection_logs.rb
@@ -0,0 +1,16 @@
+class AddDomainIdToConnectionLogs < ActiveRecord::Migration
+  class ConnectionLog < ActiveRecord::Base
+  end
+
+  def up
+    add_column :connection_logs, :domain_id, :integer
+    ConnectionLog.find_each do |c|
+      domain = Domain.find_by(name: c.host)
+      c.update_attribute(:domain_id, domain.id)
+    end
+  end
+
+  def down
+    remove_column :connection_logs, :domain_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150417052803) do
+ActiveRecord::Schema.define(version: 20150420013101) do
 
   create_table "active_admin_comments", force: true do |t|
     t.string   "namespace"
@@ -67,6 +67,7 @@ ActiveRecord::Schema.define(version: 20150417052803) do
     t.integer  "response_size"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "domain_id"
   end
 
   add_index "connection_logs", ["host"], name: "index_connection_logs_on_host", using: :btree
