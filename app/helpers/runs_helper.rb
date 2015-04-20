15d501988ed309db1601f50942df9ff729fabf8d
Matthew Landauer <matthew@oaf.org.au>
2015-04-20 13:48:18 +1000

    Inlined method

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -20,7 +20,7 @@ module RunsHelper
   end
 
   def scraped_domains_list(run)
-    d = run.scraped_domains.map{|d| link_to h(d), h("http://#{d}")}
+    d = run.scraped_domain_objects.map{|d| link_to h(d.name), h("http://#{d.name}")}
     # If there are more than 3 in the list then summarise
     if d.count > 3
       d = d[0..2] + [pluralize(d[3..-1].count, "other domain".html_safe)]
--- spec/helpers/runs_helper_spec.rb
+++ spec/helpers/runs_helper_spec.rb
@@ -3,33 +3,38 @@ require 'spec_helper'
 describe RunsHelper do
   describe "#scraped_domains_list" do
     let(:run) { mock_model(Run) }
+    let(:foo_domain) { mock_model(Domain, name: "foo.com")}
+    let(:bar_domain) { mock_model(Domain, name: "bar.com")}
+    let(:www_foo_domain) { mock_model(Domain, name: "www.foo.com")}
+    let(:www_bar_domain) { mock_model(Domain, name: "www.bar.com")}
+    let(:fiddle_domain) { mock_model(Domain, name: "fiddle.com")}
 
     it do
-      allow(run).to receive(:scraped_domains).and_return(["foo.com"])
+      allow(run).to receive(:scraped_domain_objects).and_return([foo_domain])
       expect(helper.scraped_domains_list(run)).to eq '<a href="http://foo.com">foo.com</a>'
       expect(helper.scraped_domains_list(run)).to be_html_safe
     end
 
     it do
-      allow(run).to receive(:scraped_domains).and_return(["foo.com", "bar.com"])
+      allow(run).to receive(:scraped_domain_objects).and_return([foo_domain, bar_domain])
       expect(helper.scraped_domains_list(run)).to eq '<a href="http://foo.com">foo.com</a> and <a href="http://bar.com">bar.com</a>'
       expect(helper.scraped_domains_list(run)).to be_html_safe
     end
 
     it do
-      allow(run).to receive(:scraped_domains).and_return(["foo.com", "bar.com", "www.foo.com"])
+      allow(run).to receive(:scraped_domain_objects).and_return([foo_domain, bar_domain, www_foo_domain])
       expect(helper.scraped_domains_list(run)).to eq '<a href="http://foo.com">foo.com</a>, <a href="http://bar.com">bar.com</a>, and <a href="http://www.foo.com">www.foo.com</a>'
       expect(helper.scraped_domains_list(run)).to be_html_safe
     end
 
     it do
-      allow(run).to receive(:scraped_domains).and_return(["foo.com", "bar.com", "www.foo.com", "www.bar.com"])
+      allow(run).to receive(:scraped_domain_objects).and_return([foo_domain, bar_domain, www_foo_domain, www_bar_domain])
       expect(helper.scraped_domains_list(run)).to eq '<a href="http://foo.com">foo.com</a>, <a href="http://bar.com">bar.com</a>, <a href="http://www.foo.com">www.foo.com</a>, and 1 other domain'
       expect(helper.scraped_domains_list(run)).to be_html_safe
     end
 
     it do
-      allow(run).to receive(:scraped_domains).and_return(["foo.com", "bar.com", "www.foo.com", "www.bar.com", "fiddle.com"])
+      allow(run).to receive(:scraped_domain_objects).and_return([foo_domain, bar_domain, www_foo_domain, www_bar_domain, fiddle_domain])
       expect(helper.scraped_domains_list(run)).to eq '<a href="http://foo.com">foo.com</a>, <a href="http://bar.com">bar.com</a>, <a href="http://www.foo.com">www.foo.com</a>, and 2 other domains'
       expect(helper.scraped_domains_list(run)).to be_html_safe
     end
