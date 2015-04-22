fe59f54bdf6d257972096a76fc2063dd8d8d6062
Matthew Landauer <matthew@oaf.org.au>
2015-04-22 11:10:15 +1000

    Move search to its own controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,2 +1,8 @@
 class SearchController < ApplicationController
+  def search
+    @q = params[:q]
+    @scrapers = Scraper.search(@q)
+    @owners = Owner.search(@q)
+    @type = params[:type]
+  end
 end
--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,11 +1,4 @@
 class StaticController < ApplicationController
   def index
   end
-
-  def search
-    @q = params[:q]
-    @scrapers = Scraper.search(@q)
-    @owners = Owner.search(@q)
-    @type = params[:type]
-  end
 end
