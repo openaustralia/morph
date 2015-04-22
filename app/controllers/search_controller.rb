5b6b9ece303f8277ef2ad8760a6ed2679da23775
Matthew Landauer <matthew@oaf.org.au>
2015-04-22 14:26:57 +1000

    Just highlight the name of a user in the search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def search
     @q = params[:q]
     @scrapers = Scraper.search(@q)
-    @owners = Owner.search(@q)
+    @owners = Owner.search(@q, highlight: {fields: [:name]})
     @type = params[:type]
   end
 end
