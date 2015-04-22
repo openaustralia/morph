0c2f84958dc4c13200964f77d86d28429944b209
Luke Bacon <luke@equivalentideas.com>
2015-04-22 14:21:30 +1000

    paginate search results, 15 results per page Follow the instructions at https://github.com/ankane/searchkick#pagination to add search to both scraper and owner results.
    Conflicts:
    	app/controllers/search_controller.rb

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,9 @@
 class SearchController < ApplicationController
   def search
     @q = params[:q]
-    @scrapers = Scraper.search(@q)
-    @owners = Owner.search(@q, highlight: {fields: [:nickname, :name, :company, :blog]})
+
+    @scrapers = Scraper.search @q, page: params[:page], per_page: 10
+    @owners = Owner.search @q, highlight: {fields: [:nickname, :name, :company, :blog]}, page: params[:page], per_page: 10
     @type = params[:type]
   end
 end
