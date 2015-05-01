a1128f60df4a41c618080faabe0074f412da047f
Matthew Landauer <matthew@oaf.org.au>
2015-05-01 17:41:24 +1000

    Admins can now set the new feature flag

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -29,7 +29,7 @@ class OwnersController < ApplicationController
 
   def update
     if @owner.user?
-      @owner.update_attributes(buildpacks: params[:user][:buildpacks])
+      @owner.update_attributes(buildpacks: params[:user][:buildpacks], see_downloads: params[:user][:see_downloads])
     elsif @owner.organization?
       @owner.update_attributes(buildpacks: params[:organization][:buildpacks])
     else
