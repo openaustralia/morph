9c266860f25396f898e63c3bd8fa36bbac2ee58a
Matthew Landauer <matthew@oaf.org.au>
2015-05-04 12:38:39 +1000

    Extract method

--- app/helpers/runs_helper.rb
+++ app/helpers/runs_helper.rb
@@ -29,7 +29,12 @@ module RunsHelper
   end
 
   def scraped_domains_list(run)
-    d = run.scraped_domains.map{|d| link_to h(d.name), h("http://#{d.name}")}
+    scraped_domains = run.scraped_domains
+    scraped_domains_list2(scraped_domains)
+  end
+
+  def scraped_domains_list2(scraped_domains)
+    d = scraped_domains.map{|d| link_to h(d.name), h("http://#{d.name}")}
     # If there are more than 3 in the list then summarise
     summary_of_array(d, "other domain".html_safe).to_sentence.html_safe
   end
