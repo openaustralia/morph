8365ba9404a1b365817a25787289045d57e271d7
Matthew Landauer <matthew@oaf.org.au>
2015-05-05 05:24:08 +1000

    For full_name of scraper allow searches to match middle of words. Fixes #613

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def search
     @q = params[:q]
-    @scrapers = Scraper.search @q, fields: [:full_name, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+    @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     @owners = Owner.search @q, highlight: {fields: [:nickname, :name, :company, :blog]}, page: params[:page], per_page: 10
     @type = params[:type]
   end
