4403cdc62009377f9e6096cb47a6baaf2e2018ea
Matthew Landauer <matthew@oaf.org.au>
2015-05-07 17:07:59 +1000

    Redirect to landing page on logging in. Fixes #705

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,6 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  # With this when logging in on an unauthenticated page it will redirect you to your own user page
-  # When forced to log in because we were sent to an authenticated page it will redirect back to the
-  # page after logging in
-  def user_root_path
-    user_path(current_user) if current_user
-  end
-
   # When trying to look at a page on active admin you're not allowed to
   def access_denied(exception)
     redirect_to current_user, :alert => exception.message
