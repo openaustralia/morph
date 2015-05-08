a87045ad5503c38fcc27c1d7ae0705618729ec14
Luke Bacon <luke@equivalentideas.com>
2015-05-08 15:36:39 +1000

    Inline temporary variable

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -4,22 +4,21 @@ class AlertMailer < ActionMailer::Base
   default from: "morph.io <contact@morph.io>"
 
   def alert_email(user, broken_scrapers, successful_count)
-    broken_runs = broken_scrapers.map {|s| s.last_run}
-    count = broken_runs.count
+    count = broken_scrapers.count
     @user, @successful_count = user, successful_count
     @analytics_params = {utm_medium: "email", utm_source: "alerts"}
     # The ones that are broken for the longest time come last
-    @broken_runs = broken_runs.sort do |a,b|
-      if b.scraper.latest_successful_run_time.nil? && a.scraper.latest_successful_run_time.nil?
+    @broken_runs = broken_scrapers.sort do |a,b|
+      if b.latest_successful_run_time.nil? && a.latest_successful_run_time.nil?
         0
-      elsif b.scraper.latest_successful_run_time.nil?
+      elsif b.latest_successful_run_time.nil?
         -1
-      elsif a.scraper.latest_successful_run_time.nil?
+      elsif a.latest_successful_run_time.nil?
         1
       else
-        b.scraper.latest_successful_run_time <=> a.scraper.latest_successful_run_time
+        b.latest_successful_run_time <=> a.latest_successful_run_time
       end
-    end
+    end.map {|s| s.last_run}
     @subject = "morph.io: #{pluralize(count, 'scraper')} you are watching #{count == 1 ? "is" : "are"} erroring"
 
     attachments.inline['logo_75x75.png'] = File.read(File.join(Rails.root, "app", "assets", path_to_image("logo_75x75.png")))
