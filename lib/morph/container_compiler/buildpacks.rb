cdd026ba2cd75d41e5a872cc628a3aaefe441eaa
Matthew Landauer <matthew@oaf.org.au>
2015-05-11 13:46:43 +1000

    Remove base class

--- lib/morph/container_compiler/buildpacks.rb
+++ lib/morph/container_compiler/buildpacks.rb
@@ -1,7 +1,27 @@
 module Morph
   module ContainerCompiler
-    class Buildpacks < Base
+    class Buildpacks
       ALL_CONFIG_FILENAMES = ["Gemfile", "Gemfile.lock", "Procfile", "requirements.txt", "runtime.txt", "composer.json", "composer.lock", "cpanfile"]
+
+      def self.docker_container_name(run)
+        "#{run.owner.to_param}_#{run.name}_#{run.id}"
+      end
+
+      # If image is present locally use that. If it isn't then pull it from the hub
+      # This makes initial setup easier
+      def self.get_or_pull_image(name)
+        wrapper = Multiblock.wrapper
+        yield(wrapper)
+
+        begin
+          Docker::Image.get(name)
+        rescue Docker::Error::NotFoundError
+          Docker::Image.create('fromImage' => name) do |chunk|
+            data = JSON.parse(chunk)
+            wrapper.call(:log, :internalout, "#{data['status']} #{data['id']} #{data['progress']}\n")
+          end
+        end
+      end
 
       def self.compile_and_run(run)
         wrapper = Multiblock.wrapper
