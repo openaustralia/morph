c5b26a511a6ed20d9b671d57672c83fa82480205
Luke Bacon <luke@equivalentideas.com>
2015-05-12 19:40:07 +1000

    Always show the 'all scrapers' count on scrapers tab

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,6 +8,7 @@ class SearchController < ApplicationController
     if @show == "all" && @type != "users"
       @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     else
+      @scrapers_total_count = (Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}]).count
       @scrapers = Scraper.search @q, where: {sqlite_db_size: {not: 0}}, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     end
   end
