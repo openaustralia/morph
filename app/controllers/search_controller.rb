99405f270a1019f6aad57a7c02118ba63c829b31
Luke Bacon <luke@equivalentideas.com>
2015-05-12 23:31:30 +1000

    Fix bug in search 'where' query by using indexed method
    The query was not returning correct results because the target
    data was not in the index.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,7 +9,7 @@ class SearchController < ApplicationController
       @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     else
       @scrapers_total_count = (Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}]).count
-      @scrapers = Scraper.search @q, where: {sqlite_db_size: {not: 0}}, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+      @scrapers = Scraper.search @q, where: {has_data?: true}, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     end
   end
 end
