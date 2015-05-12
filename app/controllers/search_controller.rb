b323284d520e46052fbf24ee42a7306115e5bc20
Luke Bacon <luke@equivalentideas.com>
2015-05-12 19:21:18 +1000

    By default, only show results for scrapers with data
    If show 'all' is set, search all and return results.
    
    relates to #712

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
   def search
     @q = params[:q]
-    @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     @owners = Owner.search @q, highlight: {fields: [:nickname, :name, :company, :blog]}, page: params[:page], per_page: 10
     @type = params[:type]
     @show = params[:show]
+
+    if @show == "all" && @type != "users"
+      @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+    else
+      @scrapers = Scraper.search @q, where: {sqlite_db_size: {not: 0}}, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+    end
   end
 end
