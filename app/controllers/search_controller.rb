59ba14b6191085426dbd36accb97e479738931ec
Luke Bacon <luke@equivalentideas.com>
2015-05-12 19:15:03 +1000

    Add a 'show' param for controlling results filters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,5 +4,6 @@ class SearchController < ApplicationController
     @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
     @owners = Owner.search @q, highlight: {fields: [:nickname, :name, :company, :blog]}, page: params[:page], per_page: 10
     @type = params[:type]
+    @show = params[:show]
   end
 end
