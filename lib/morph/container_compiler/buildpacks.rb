0f84b1b45140a1f473d615b3fbd7a75b220a717a
Matthew Landauer <matthew@oaf.org.au>
2015-05-14 14:27:37 +1000

    Get rid of use of thread-unsafe FileUtils.cd

--- lib/morph/container_compiler/buildpacks.rb
+++ lib/morph/container_compiler/buildpacks.rb
@@ -216,17 +216,16 @@ module Morph
       def self.create_tar(directory)
         tempfile = Tempfile.new('morph_tar')
 
-        in_directory(directory) do
-          # We used to use Archive::Tar::Minitar but that doesn't support
-          # symbolic links in the tar file. So, using tar from the command line
-          # instead.
-          `tar cf #{tempfile.path} .`
-        end
+        # We used to use Archive::Tar::Minitar but that doesn't support
+        # symbolic links in the tar file. So, using tar from the command line
+        # instead.
+        `tar cf #{tempfile.path} -C #{directory} .`
         content = File.read(tempfile.path)
         FileUtils.rm_f(tempfile.path)
         content
       end
 
+      # BEWARE: Not thread safe!
       def self.in_directory(directory)
         cwd = FileUtils.pwd
         FileUtils.cd(directory)
