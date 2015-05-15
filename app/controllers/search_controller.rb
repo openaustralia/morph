94b0d487c0a13054a48cf2b75598acc1d0e188de
Luke Bacon <luke@equivalentideas.com>
2015-05-15 15:07:09 +1000

    Group together default search params (whitespace only)

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,12 +1,13 @@
 class SearchController < ApplicationController
   def search
-    @q = params[:q]
     default_owner_search_params = {highlight: {fields: [:nickname, :name, :company, :blog]}, page: params[:page], per_page: 10}
+    default_scraper_search_params = {fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10}
+
+    @q = params[:q]
     @owners = Owner.search @q, default_owner_search_params
     @type = params[:type]
     @show = params[:show]
 
-    default_scraper_search_params = {fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10}
     all_scrapers = Scraper.search @q, default_scraper_search_params
     @scrapers_total_count = all_scrapers.total_count
 
