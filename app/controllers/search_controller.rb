c63236b94a63de88851a745632ec66da6a5b5eed
Luke Bacon <luke@equivalentideas.com>
2015-05-15 14:35:28 +1000

    Extract variable so that @scrapers_total_count set consistently
    The @scraper_total_count is being set in two places, in two
    different ways. Extract the all_scrapers query into a temporary
    variable and use that to set the @scrapers_total_count.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,12 +5,13 @@ class SearchController < ApplicationController
     @type = params[:type]
     @show = params[:show]
 
+    all_scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+    @scrapers_total_count = all_scrapers.total_count
+
     if @show == "all" && @type != "users"
       @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
-      @scrapers_total_count = @scrapers.total_count
     else
       @scrapers = Scraper.search @q, where: {has_data?: true}, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
-      @scrapers_total_count = (Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}]).count
     end
   end
 end
