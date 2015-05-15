db14c46253d9a10fe4440527d9dac1d4a7269825
Luke Bacon <luke@equivalentideas.com>
2015-05-15 14:57:39 +1000

    Extract variable default_scraper_search_params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,13 +5,14 @@ class SearchController < ApplicationController
     @type = params[:type]
     @show = params[:show]
 
-    all_scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+    default_scraper_search_params = {fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10}
+    all_scrapers = Scraper.search @q, default_scraper_search_params
     @scrapers_total_count = all_scrapers.total_count
 
     if @show == "all" && @type != "users"
-      @scrapers = Scraper.search @q, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+      @scrapers = all_scrapers
     else
-      @scrapers = Scraper.search @q, where: {has_data?: true}, fields: [{full_name: :word_middle}, :description, {scraped_domain_names: :word_end}], highlight: true, page: params[:page], per_page: 10
+      @scrapers = Scraper.search @q, default_scraper_search_params.merge(where: {has_data?: true})
     end
   end
 end
