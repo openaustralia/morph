c2e9f38ec5fdb0bb1ba1e00523f7899148f5b785
Matthew Landauer <matthew@oaf.org.au>
2015-05-20 14:57:33 +1000

    Extract helper for basic message for no search results

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,5 @@
 module SearchHelper
+  def no_search_results_message(things, search_term)
+    "Sorry, we couldn't find any ".html_safe + h(things) + " relevant to your search term " + content_tag(:strong, "“" + search_term + "”") + "."
+  end
 end
--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper'
 
-# Specs in this file have access to a helper object that includes
-# the SearchHelper. For example:
-#
-# describe SearchHelper do
-#   describe "string concat" do
-#     it "concats two strings with spaces" do
-#       expect(helper.concat_strings("this","that")).to eq("this that")
-#     end
-#   end
-# end
 describe SearchHelper do
+  describe ".no_search_results_message" do
+    it { expect(helper.no_search_results_message("scrapers", "bibble")).to eq "Sorry, we couldn't find any scrapers relevant to your search term <strong>“bibble”</strong>."}
+
+    it "should allow html in things" do
+      expect(helper.no_search_results_message("scrapers <em>with data</em>".html_safe, "bibble")).to eq "Sorry, we couldn't find any scrapers <em>with data</em> relevant to your search term <strong>“bibble”</strong>."
+    end
+
+    it "should allow escape html if things is not html safe" do
+      expect(helper.no_search_results_message("scrapers <em>with data</em>", "bibble")).to eq "Sorry, we couldn't find any scrapers &lt;em&gt;with data&lt;/em&gt; relevant to your search term <strong>“bibble”</strong>."
+    end
+
+    it "should escape the search term" do
+      expect(helper.no_search_results_message("scrapers <em>with data</em>", "<bibble>")).to eq "Sorry, we couldn't find any scrapers &lt;em&gt;with data&lt;/em&gt; relevant to your search term <strong>“&lt;bibble&gt;”</strong>."
+    end
+
+    it "should be html safe" do
+      expect(helper.no_search_results_message("<foo>", "<bar>")).to be_html_safe
+    end
+  end
 end
