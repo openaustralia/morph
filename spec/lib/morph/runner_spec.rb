4d1f6f981a27993df6784e4cd80ab93b3544f17e
Matthew Landauer <matthew@oaf.org.au>
2015-05-23 12:34:23 +1000

    Refactor tests

--- spec/lib/morph/runner_spec.rb
+++ spec/lib/morph/runner_spec.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe Morph::DockerRunner do
-  context "a set of files" do
-    before :each do
-      FileUtils.mkdir_p("test")
-      FileUtils.touch("test/Procfile")
-      FileUtils.touch("test/Gemfile")
-      FileUtils.touch("test/Gemfile.lock")
+  describe ".add_config_defaults_to_directory" do
+    before(:each) do
+      FileUtils.mkdir("test")
       FileUtils.touch("test/scraper.rb")
     end
+    after(:each) { FileUtils.rm_rf("test") }
 
-    after :each do
-      FileUtils.rm_rf("test")
-    end
+    context "a set of files" do
+      before :each do
+        FileUtils.touch("test/Procfile")
+        FileUtils.touch("test/Gemfile")
+        FileUtils.touch("test/Gemfile.lock")
+      end
 
-    describe ".add_config_defaults_to_directory" do
       it do
         Dir.mktmpdir do |dir|
           Morph::Runner.add_config_defaults_to_directory("test", dir)
@@ -22,25 +22,16 @@ describe Morph::DockerRunner do
           File.read(File.join(dir, "Gemfile")).should == ""
           File.read(File.join(dir, "Gemfile.lock")).should == ""
           File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
-          File.read(File.join(dir, "scraper.rb")).should == ""
         end
       end
     end
-  end
 
-  context "another set of files" do
-    before :each do
-      FileUtils.mkdir_p("test")
-      FileUtils.touch("test/Gemfile")
-      FileUtils.touch("test/Gemfile.lock")
-      FileUtils.touch("test/scraper.rb")
-    end
+    context "another set of files" do
+      before :each do
+        FileUtils.touch("test/Gemfile")
+        FileUtils.touch("test/Gemfile.lock")
+      end
 
-    after :each do
-      FileUtils.rm_rf("test")
-    end
-
-    describe ".add_config_defaults_to_directory" do
       it do
         Dir.mktmpdir do |dir|
           Morph::Runner.add_config_defaults_to_directory("test", dir)
@@ -48,24 +39,15 @@ describe Morph::DockerRunner do
           File.read(File.join(dir, "Gemfile")).should == ""
           File.read(File.join(dir, "Gemfile.lock")).should == ""
           File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
-          File.read(File.join(dir, "scraper.rb")).should == ""
         end
       end
     end
-  end
 
-  context "user tries to override Procfile" do
-    before :each do
-      FileUtils.mkdir_p("test")
-      File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
-      FileUtils.touch("test/scraper.rb")
-    end
+    context "user tries to override Procfile" do
+      before :each do
+        File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
+      end
 
-    after :each do
-      FileUtils.rm_rf("test")
-    end
-
-    describe ".add_config_defaults_to_directory" do
       it do
         Dir.mktmpdir do |dir|
           Morph::Runner.add_config_defaults_to_directory("test", dir)
@@ -74,7 +56,6 @@ describe Morph::DockerRunner do
           File.read(File.join(dir, "Gemfile")).should == File.read(ruby.default_config_file_path("Gemfile"))
           File.read(File.join(dir, "Gemfile.lock")).should == File.read(ruby.default_config_file_path("Gemfile.lock"))
           File.read(File.join(dir, "Procfile")).should == File.read(ruby.default_config_file_path("Procfile"))
-          File.read(File.join(dir, "scraper.rb")).should == ""
         end
       end
     end
