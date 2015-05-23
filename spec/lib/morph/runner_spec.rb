c0a9e8e4397231d9df7beb83ea729b760b99d220
Matthew Landauer <matthew@oaf.org.au>
2015-05-23 12:36:57 +1000

    Remove one of two similar tests

--- spec/lib/morph/runner_spec.rb
+++ spec/lib/morph/runner_spec.rb
@@ -8,9 +8,9 @@ describe Morph::DockerRunner do
     end
     after(:each) { FileUtils.rm_rf("test") }
 
-    context "a set of files" do
+    context "user tries to override Procfile" do
       before :each do
-        FileUtils.touch("test/Procfile")
+        File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
         FileUtils.touch("test/Gemfile")
         FileUtils.touch("test/Gemfile.lock")
       end
@@ -39,23 +39,6 @@ describe Morph::DockerRunner do
           File.read(File.join(dir, "Gemfile")).should == ""
           File.read(File.join(dir, "Gemfile.lock")).should == ""
           File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
-        end
-      end
-    end
-
-    context "user tries to override Procfile" do
-      before :each do
-        File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
-      end
-
-      it do
-        Dir.mktmpdir do |dir|
-          Morph::Runner.add_config_defaults_to_directory("test", dir)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
-          ruby = Morph::Language.new(:ruby)
-          File.read(File.join(dir, "Gemfile")).should == File.read(ruby.default_config_file_path("Gemfile"))
-          File.read(File.join(dir, "Gemfile.lock")).should == File.read(ruby.default_config_file_path("Gemfile.lock"))
-          File.read(File.join(dir, "Procfile")).should == File.read(ruby.default_config_file_path("Procfile"))
         end
       end
     end
