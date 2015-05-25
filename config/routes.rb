05bf717f9b426a0e4cb15bb7e487ef9313c96a50
Luke Bacon <luke@equivalentideas.com>
2015-05-25 17:38:19 +1000

    Route scrapers#running to scrapers/running

--- config/routes.rb
+++ config/routes.rb
@@ -88,6 +88,8 @@ Rails.application.routes.draw do
     end
   end
 
+  get 'scrapers/running', to: "scrapers#running"
+
   # These routes with path: "/" need to be at the end
   resources :owners, path: "/", only: [:show, :update]
   resources :users, path: "/", only: :show
