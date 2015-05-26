f64459f4f29eb47aea81e6dab17161fd499c3fa5
Matthew Landauer <matthew@oaf.org.au>
2015-05-26 16:26:59 +1000

    Test that perl defaults for config files is doing the right thing

--- spec/lib/morph/runner_spec.rb
+++ spec/lib/morph/runner_spec.rb
@@ -2,71 +2,87 @@ require 'spec_helper'
 
 describe Morph::Runner do
   describe ".add_config_defaults_to_directory" do
-    before(:each) do
-      FileUtils.mkdir("test")
-      FileUtils.touch("test/scraper.rb")
-    end
+    before(:each) { FileUtils.mkdir("test") }
     after(:each) { FileUtils.rm_rf("test") }
 
-    context "user tries to override Procfile" do
-      before :each do
-        File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
-        FileUtils.touch("test/Gemfile")
-        FileUtils.touch("test/Gemfile.lock")
-      end
+    context "a perl scraper" do
+      before(:each) { FileUtils.touch("test/scraper.pl") }
 
-      it "should always use the template Procfile" do
+      it do
         Dir.mktmpdir do |dir|
           Morph::Runner.add_config_defaults_to_directory("test", dir)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
-          File.read(File.join(dir, "Gemfile")).should == ""
-          File.read(File.join(dir, "Gemfile.lock")).should == ""
-          File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
+          Dir.entries(dir).sort.should == [".", "..", "Procfile", "app.psgi", "cpanfile", "scraper.pl"]
+          perl = Morph::Language.new(:perl)
+          File.read(File.join(dir, "Procfile")).should == File.read(perl.default_config_file_path("Procfile"))
+          File.read(File.join(dir, "app.psgi")).should == File.read(perl.default_config_file_path("app.psgi"))
+          File.read(File.join(dir, "cpanfile")).should == File.read(perl.default_config_file_path("cpanfile"))
         end
       end
     end
 
-    context "user supplies Gemfile and Gemfile.lock" do
-      before :each do
-        FileUtils.touch("test/Gemfile")
-        FileUtils.touch("test/Gemfile.lock")
-      end
+    context "a ruby scraper" do
+      before(:each) { FileUtils.touch("test/scraper.rb") }
 
-      it "should only provide a template Procfile" do
-        Dir.mktmpdir do |dir|
-          Morph::Runner.add_config_defaults_to_directory("test", dir)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
-          File.read(File.join(dir, "Gemfile")).should == ""
-          File.read(File.join(dir, "Gemfile.lock")).should == ""
-          File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
+      context "user tries to override Procfile" do
+        before :each do
+          File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
+          FileUtils.touch("test/Gemfile")
+          FileUtils.touch("test/Gemfile.lock")
+        end
+
+        it "should always use the template Procfile" do
+          Dir.mktmpdir do |dir|
+            Morph::Runner.add_config_defaults_to_directory("test", dir)
+            Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
+            File.read(File.join(dir, "Gemfile")).should == ""
+            File.read(File.join(dir, "Gemfile.lock")).should == ""
+            File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
+          end
         end
       end
-    end
 
-    context "user doesn't supply Gemfile or Gemfile.lock" do
-      it "should provide a template Gemfile, Gemfile.lock and Procfile" do
-        Dir.mktmpdir do |dir|
-          Morph::Runner.add_config_defaults_to_directory("test", dir)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
-          ruby = Morph::Language.new(:ruby)
-          File.read(File.join(dir, "Gemfile")).should == File.read(ruby.default_config_file_path("Gemfile"))
-          File.read(File.join(dir, "Gemfile.lock")).should == File.read(ruby.default_config_file_path("Gemfile.lock"))
-          File.read(File.join(dir, "Procfile")).should == File.read(ruby.default_config_file_path("Procfile"))
+      context "user supplies Gemfile and Gemfile.lock" do
+        before :each do
+          FileUtils.touch("test/Gemfile")
+          FileUtils.touch("test/Gemfile.lock")
+        end
+
+        it "should only provide a template Procfile" do
+          Dir.mktmpdir do |dir|
+            Morph::Runner.add_config_defaults_to_directory("test", dir)
+            Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
+            File.read(File.join(dir, "Gemfile")).should == ""
+            File.read(File.join(dir, "Gemfile.lock")).should == ""
+            File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
+          end
         end
       end
-    end
 
-    context "user supplies Gemfile but no Gemfile.lock" do
-      before :each do
-        FileUtils.touch("test/Gemfile")
+      context "user doesn't supply Gemfile or Gemfile.lock" do
+        it "should provide a template Gemfile, Gemfile.lock and Procfile" do
+          Dir.mktmpdir do |dir|
+            Morph::Runner.add_config_defaults_to_directory("test", dir)
+            Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile", "scraper.rb"]
+            ruby = Morph::Language.new(:ruby)
+            File.read(File.join(dir, "Gemfile")).should == File.read(ruby.default_config_file_path("Gemfile"))
+            File.read(File.join(dir, "Gemfile.lock")).should == File.read(ruby.default_config_file_path("Gemfile.lock"))
+            File.read(File.join(dir, "Procfile")).should == File.read(ruby.default_config_file_path("Procfile"))
+          end
+        end
       end
 
-      it "should not try to use the template Gemfile.lock" do
-        Dir.mktmpdir do |dir|
-          Morph::Runner.add_config_defaults_to_directory("test", dir)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Procfile", "scraper.rb"]
-          File.read(File.join(dir, "Gemfile")).should == ""
-          File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
+      context "user supplies Gemfile but no Gemfile.lock" do
+        before :each do
+          FileUtils.touch("test/Gemfile")
+        end
+
+        it "should not try to use the template Gemfile.lock" do
+          Dir.mktmpdir do |dir|
+            Morph::Runner.add_config_defaults_to_directory("test", dir)
+            Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Procfile", "scraper.rb"]
+            File.read(File.join(dir, "Gemfile")).should == ""
+            File.read(File.join(dir, "Procfile")).should == File.read(Morph::Language.new(:ruby).default_config_file_path("Procfile"))
+          end
         end
       end
     end
