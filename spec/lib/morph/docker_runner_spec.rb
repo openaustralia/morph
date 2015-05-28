cf72c2a622b646223382825f32b1e79142ec705c
Matthew Landauer <matthew@oaf.org.au>
2015-05-28 10:56:47 +1000

    Improve formatting

--- spec/lib/morph/docker_runner_spec.rb
+++ spec/lib/morph/docker_runner_spec.rb
@@ -1,118 +1,126 @@
 require 'spec_helper'
 
 describe Morph::DockerRunner do
-  context "a set of files" do
+  context 'a set of files' do
     before :each do
-      FileUtils.mkdir_p("test/foo")
-      FileUtils.mkdir_p("test/.bar")
-      FileUtils.touch("test/.a_dot_file.cfg")
-      FileUtils.touch("test/.bar/wibble.txt")
-      FileUtils.touch("test/one.txt")
-      FileUtils.touch("test/Procfile")
-      FileUtils.touch("test/two.txt")
-      FileUtils.touch("test/foo/three.txt")
-      FileUtils.touch("test/Gemfile")
-      FileUtils.touch("test/Gemfile.lock")
-      FileUtils.touch("test/scraper.rb")
-      FileUtils.ln_s("scraper.rb", "test/link.rb")
+      FileUtils.mkdir_p 'test/foo'
+      FileUtils.mkdir_p 'test/.bar'
+      FileUtils.touch 'test/.a_dot_file.cfg'
+      FileUtils.touch 'test/.bar/wibble.txt'
+      FileUtils.touch 'test/one.txt'
+      FileUtils.touch 'test/Procfile'
+      FileUtils.touch 'test/two.txt'
+      FileUtils.touch 'test/foo/three.txt'
+      FileUtils.touch 'test/Gemfile'
+      FileUtils.touch 'test/Gemfile.lock'
+      FileUtils.touch 'test/scraper.rb'
+      FileUtils.ln_s 'scraper.rb', 'test/link.rb'
     end
 
     after :each do
-      FileUtils.rm_rf("test")
+      FileUtils.rm_rf 'test'
     end
 
-    describe ".copy_config_to_directory" do
+    describe '.copy_config_to_directory' do
       it do
         Dir.mktmpdir do |dir|
-          Morph::DockerRunner.copy_config_to_directory("test", dir, true)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock", "Procfile"]
-          File.read(File.join(dir, "Gemfile")).should == ""
-          File.read(File.join(dir, "Gemfile.lock")).should == ""
-          File.read(File.join(dir, "Procfile")).should == ""
+          Morph::DockerRunner.copy_config_to_directory('test', dir, true)
+          expect(Dir.entries(dir).sort).to eq [
+            '.', '..', 'Gemfile', 'Gemfile.lock', 'Procfile']
+          expect(File.read(File.join(dir, 'Gemfile'))).to eq ''
+          expect(File.read(File.join(dir, 'Gemfile.lock'))).to eq ''
+          expect(File.read(File.join(dir, 'Procfile'))).to eq ''
         end
       end
 
       it do
         Dir.mktmpdir do |dir|
-          Morph::DockerRunner.copy_config_to_directory("test", dir, false)
-          Dir.entries(dir).sort.should == [".", "..", ".a_dot_file.cfg", ".bar", "foo", "link.rb", "one.txt", "scraper.rb", "two.txt"]
-          Dir.entries(File.join(dir, ".bar")).sort.should == [".", "..", "wibble.txt"]
-          Dir.entries(File.join(dir, "foo")).sort.should == [".", "..", "three.txt"]
-          File.read(File.join(dir, ".a_dot_file.cfg")).should == ""
-          File.read(File.join(dir, ".bar", "wibble.txt")).should == ""
-          File.read(File.join(dir, "foo/three.txt")).should == ""
-          File.read(File.join(dir, "one.txt")).should == ""
-          File.read(File.join(dir, "scraper.rb")).should == ""
-          File.read(File.join(dir, "two.txt")).should == ""
-          File.readlink(File.join(dir, "link.rb")).should == "scraper.rb"
+          Morph::DockerRunner.copy_config_to_directory('test', dir, false)
+          expect(Dir.entries(dir).sort).to eq [
+            '.', '..', '.a_dot_file.cfg', '.bar', 'foo', 'link.rb', 'one.txt',
+            'scraper.rb', 'two.txt']
+          expect(Dir.entries(File.join(dir, '.bar')).sort).to eq [
+            '.', '..', 'wibble.txt']
+          expect(Dir.entries(File.join(dir, 'foo')).sort).to eq [
+            '.', '..', 'three.txt']
+          expect(File.read(File.join(dir, '.a_dot_file.cfg'))).to eq ''
+          expect(File.read(File.join(dir, '.bar', 'wibble.txt'))).to eq ''
+          expect(File.read(File.join(dir, 'foo/three.txt'))).to eq ''
+          expect(File.read(File.join(dir, 'one.txt'))).to eq ''
+          expect(File.read(File.join(dir, 'scraper.rb'))).to eq ''
+          expect(File.read(File.join(dir, 'two.txt'))).to eq ''
+          expect(File.readlink(File.join(dir, 'link.rb'))).to eq 'scraper.rb'
         end
       end
     end
   end
 
-  context "another set of files" do
+  context 'another set of files' do
     before :each do
-      FileUtils.mkdir_p("test/foo")
-      FileUtils.touch("test/one.txt")
-      FileUtils.touch("test/foo/three.txt")
-      FileUtils.touch("test/Gemfile")
-      FileUtils.touch("test/Gemfile.lock")
-      FileUtils.touch("test/scraper.rb")
+      FileUtils.mkdir_p('test/foo')
+      FileUtils.touch('test/one.txt')
+      FileUtils.touch('test/foo/three.txt')
+      FileUtils.touch('test/Gemfile')
+      FileUtils.touch('test/Gemfile.lock')
+      FileUtils.touch('test/scraper.rb')
     end
 
     after :each do
-      FileUtils.rm_rf("test")
+      FileUtils.rm_rf('test')
     end
 
-    describe ".copy_config_to_directory" do
+    describe '.copy_config_to_directory' do
       it do
         Dir.mktmpdir do |dir|
-          Morph::DockerRunner.copy_config_to_directory("test", dir, true)
-          Dir.entries(dir).sort.should == [".", "..", "Gemfile", "Gemfile.lock"]
-          File.read(File.join(dir, "Gemfile")).should == ""
-          File.read(File.join(dir, "Gemfile.lock")).should == ""
+          Morph::DockerRunner.copy_config_to_directory('test', dir, true)
+          expect(Dir.entries(dir).sort).to eq [
+            '.', '..', 'Gemfile', 'Gemfile.lock']
+          expect(File.read(File.join(dir, 'Gemfile'))).to eq ''
+          expect(File.read(File.join(dir, 'Gemfile.lock'))).to eq ''
         end
       end
 
       it do
         Dir.mktmpdir do |dir|
-          Morph::DockerRunner.copy_config_to_directory("test", dir, false)
-          Dir.entries(dir).sort.should == [".", "..", "foo", "one.txt", "scraper.rb"]
-          Dir.entries(File.join(dir, "foo")).sort.should == [".", "..", "three.txt"]
-          File.read(File.join(dir, "foo/three.txt")).should == ""
-          File.read(File.join(dir, "one.txt")).should == ""
-          File.read(File.join(dir, "scraper.rb")).should == ""
+          Morph::DockerRunner.copy_config_to_directory('test', dir, false)
+          expect(Dir.entries(dir).sort).to eq [
+            '.', '..', 'foo', 'one.txt', 'scraper.rb']
+          expect(Dir.entries(File.join(dir, 'foo')).sort).to eq [
+            '.', '..', 'three.txt']
+          expect(File.read(File.join(dir, 'foo/three.txt'))).to eq ''
+          expect(File.read(File.join(dir, 'one.txt'))).to eq ''
+          expect(File.read(File.join(dir, 'scraper.rb'))).to eq ''
         end
       end
     end
   end
 
-  context "user tries to override Procfile" do
+  context 'user tries to override Procfile' do
     before :each do
-      FileUtils.mkdir_p("test")
-      File.open("test/Procfile", "w") {|f| f << "scraper: some override"}
-      FileUtils.touch("test/scraper.rb")
+      FileUtils.mkdir_p('test')
+      File.open('test/Procfile', 'w') { |f| f << 'scraper: some override' }
+      FileUtils.touch('test/scraper.rb')
     end
 
     after :each do
-      FileUtils.rm_rf("test")
+      FileUtils.rm_rf('test')
     end
 
-    describe ".copy_config_to_directory" do
+    describe '.copy_config_to_directory' do
       it do
         Dir.mktmpdir do |dir|
-          Morph::DockerRunner.copy_config_to_directory("test", dir, true)
-          Dir.entries(dir).sort.should == [".", "..", "Procfile"]
-          ruby = Morph::Language.new(:ruby)
-          File.read(File.join(dir, "Procfile")).should == "scraper: some override"
+          Morph::DockerRunner.copy_config_to_directory('test', dir, true)
+          expect(Dir.entries(dir).sort).to eq ['.', '..', 'Procfile']
+          expect(File.read(File.join(dir, 'Procfile')))
+            .to eq 'scraper: some override'
         end
       end
 
       it do
         Dir.mktmpdir do |dir|
-          Morph::DockerRunner.copy_config_to_directory("test", dir, false)
-          Dir.entries(dir).sort.should == [".", "..", "scraper.rb"]
-          File.read(File.join(dir, "scraper.rb")).should == ""
+          Morph::DockerRunner.copy_config_to_directory('test', dir, false)
+          expect(Dir.entries(dir).sort).to eq ['.', '..', 'scraper.rb']
+          expect(File.read(File.join(dir, 'scraper.rb'))).to eq ''
         end
       end
     end
