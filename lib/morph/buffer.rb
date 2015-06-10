e8d66f6e142054f33764f80697be1dd1f9eb147c
Matthew Landauer <matthew@oaf.org.au>
2015-06-10 17:33:08 +1000

    Add simple line oriented buffer class

---
+++ lib/morph/buffer.rb
@@ -0,0 +1,29 @@
+module Morph
+  class Buffer
+    def initialize
+      @buffer = ''
+    end
+
+    def extract
+      r = []
+      while i = @buffer.index("\n")
+        line = @buffer[0..i]
+        yield line if block_given?
+        r << line
+        @buffer = @buffer[i + 1..-1]
+      end
+      r
+    end
+
+    def <<(text)
+      @buffer << text
+    end
+
+    def finish
+      fail if @buffer.include?("\n")
+      r = @buffer
+      @buffer = ''
+      r
+    end
+  end
+end
---
+++ spec/lib/morph/buffer_spec.rb
@@ -0,0 +1,65 @@
+require 'spec_helper'
+
+describe Morph::Buffer do
+  let(:buffer) { Morph::Buffer.new }
+
+  context 'nothing buffered' do
+    describe '#extract' do
+      it do
+        lines = []
+        buffer.extract { |line| lines << line }
+        expect(lines).to eq []
+      end
+
+      it { expect(buffer.extract).to eq [] }
+    end
+  end
+
+  context 'less than one line' do
+    before(:each) { buffer << 'hello' }
+
+    describe '#extract' do
+      it do
+        lines = []
+        buffer.extract { |line| lines << line }
+        expect(lines).to eq []
+      end
+
+      it { expect(buffer.extract).to eq [] }
+    end
+
+    describe '#finish' do
+      it { expect(buffer.finish).to eq 'hello' }
+
+      it 'should empty the buffer' do
+        buffer.finish
+        expect(buffer.finish).to eq ''
+      end
+    end
+  end
+
+  context 'one and a bit lines' do
+    before(:each) { buffer << "hello\nfoo" }
+
+    describe '#extract' do
+      it do
+        lines = []
+        buffer.extract { |line| lines << line }
+        expect(lines).to eq ["hello\n"]
+      end
+
+      it { expect(buffer.extract).to eq ["hello\n"] }
+    end
+
+    describe '#finish' do
+      it do
+        buffer.extract
+        expect(buffer.finish).to eq 'foo'
+      end
+
+      it do
+        expect { buffer.finish }.to raise_error
+      end
+    end
+  end
+end
