f559a575d9dfc77e0c4aa3bf478fbe77a360e3ce
Matthew Landauer <matthew@oaf.org.au>
2015-06-15 11:42:21 +1000

    Extract filter

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -6,26 +6,19 @@ class ApiController < ApplicationController
   # token authentication
   skip_before_filter :verify_authenticity_token, only: [:run_remote]
   before_filter :authenticate_api_key
+  before_filter :can_run
 
   # Receive code from a remote client, run it and return the result.
   # This will be a long running request
   # TODO: Document this API
   def run_remote
-    if !current_user.ability.can? :create, Run
-      render json: {
-        stream: 'internalerr',
-        text: "You currently can't start a scraper run." \
-              ' See https://morph.io for more details'
-      }
-    else
-      response.headers['Content-Type'] = 'text/event-stream'
-      run = Run.create(queued_at: Time.now, auto: false, owner: current_user)
-      # TODO: Shouldn't need to untar here because it just gets retarred
-      Archive::Tar::Minitar.unpack(params[:code].tempfile, run.repo_path)
+    response.headers['Content-Type'] = 'text/event-stream'
+    run = Run.create(queued_at: Time.now, auto: false, owner: current_user)
+    # TODO: Shouldn't need to untar here because it just gets retarred
+    Archive::Tar::Minitar.unpack(params[:code].tempfile, run.repo_path)
 
-      Morph::Runner.new(run).go { |s, text| stream_message(s, text) }
-      response.stream.close
-    end
+    Morph::Runner.new(run).go { |s, text| stream_message(s, text) }
+    response.stream.close
   ensure
     # Cleanup run
     FileUtils.rm_rf(run.data_path)
@@ -33,6 +26,16 @@ class ApiController < ApplicationController
   end
 
   private
+
+  def can_run
+    return if current_user.ability.can? :create, Run
+
+    render json: {
+      stream: 'internalerr',
+      text: "You currently can't start a scraper run." \
+            ' See https://morph.io for more details'
+    }
+  end
 
   def stream_message(stream, text)
     response.stream.write({ stream: stream, text: text }.to_json + "\n")
