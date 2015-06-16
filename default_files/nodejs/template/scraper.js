d70d285d0d564558d9ba76a69bf2d72385ad3064
Matthew Landauer <matthew@oaf.org.au>
2015-06-16 17:16:06 +1000

    Add templates for nodejs

---
+++ default_files/nodejs/template/scraper.js
@@ -0,0 +1,7 @@
+// This is a template for a Node.js scraper on morph.io (https://morph.io)
+// including some code snippets below that you should find helpful
+
+// Node.js support is EXTREMELY new so we don't have decent template yet
+// Sorry.
+
+console.log("Hello world!");
