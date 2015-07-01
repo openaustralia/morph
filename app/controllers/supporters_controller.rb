e98630d191915190868fd80a7741bf7a85d99e6b
Henare Degan <henare.degan@gmail.com>
2015-07-01 10:15:25 +1000

    Start of supporter create action

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -3,4 +3,27 @@ class SupportersController < ApplicationController
 
   def new
   end
+
+  def create
+    @price = params[:price]
+
+    # TODO: Save customer to user
+    customer = Stripe::Customer.create(
+      email:       params[:stripeEmail],
+      card:        params[:stripeToken],
+      description: "Customer for @#{current_user.nickname}"
+    )
+
+    # TODO: Use subscriptions
+    charge = Stripe::Charge.create(
+      customer:    customer.id,
+      amount:      @price,
+      description: "morph.io basic supporter",
+      currency:    "aud"
+    )
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to supporters_path
+  end
 end
