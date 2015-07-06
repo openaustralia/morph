4238803a79697798349efd4c22a72541ae51596d
Matthew Landauer <matthew@oaf.org.au>
2015-07-06 15:13:31 +1000

    Show the actual supporter level of the user

--- app/helpers/supporters_helper.rb
+++ app/helpers/supporters_helper.rb
@@ -2,4 +2,15 @@ module SupportersHelper
   def number_in_cents_to_currency(number)
     number_to_currency(number.to_f / 100)
   end
+
+  def human_plan_name(stripe_plan_id)
+    case stripe_plan_id
+    when 'basic'
+      'Basic Supporter'
+    when 'standard'
+      'Standard Supporter'
+    when 'advanced'
+      'Advanced Supporter'
+    end
+  end
 end
