bb711ae4f8649e8500b0bdbe01f0daececcf34b6
Matthew Landauer <matthew@oaf.org.au>
2015-07-06 11:04:38 +1000

    Use single quotes rather than double quotes where possible

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,19 +1,19 @@
-require "codeclimate-test-reporter"
+require 'codeclimate-test-reporter'
 CodeClimate::TestReporter.start
 
 require 'simplecov'
-SimpleCov.start "rails"
+SimpleCov.start 'rails'
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
-ENV["RAILS_ENV"] ||= 'test'
-require File.expand_path("../../config/environment", __FILE__)
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
 # Commented out for the benefit of zeus
-#require 'rspec/autorun'
+# require 'rspec/autorun'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
@@ -56,7 +56,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  config.order = 'random'
 
   config.infer_spec_type_from_file_location!
 
@@ -83,6 +83,6 @@ RSpec.configure do |config|
     end
   end
 
-  config.filter_run_excluding docker: true if ENV["DONT_RUN_DOCKER_TESTS"]
-  config.filter_run_excluding slow: true unless ENV["RUN_SLOW_TESTS"]
+  config.filter_run_excluding docker: true if ENV['DONT_RUN_DOCKER_TESTS']
+  config.filter_run_excluding slow: true unless ENV['RUN_SLOW_TESTS']
 end
