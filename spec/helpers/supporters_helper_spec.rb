472c35953c7dea970b21e3142fa6106f34cdea07
Matthew Landauer <matthew@oaf.org.au>
2015-07-07 16:21:05 +1000

    Small test refactor

--- spec/helpers/supporters_helper_spec.rb
+++ spec/helpers/supporters_helper_spec.rb
@@ -2,22 +2,26 @@ require 'spec_helper'
 
 describe SupportersHelper do
   describe "#plan_change_word" do
+    let(:basic) { Plan.new("morph_basic") }
+    let(:standard) { Plan.new("morph_standard") }
+    let(:advanced) { Plan.new("morph_advanced") }
+
     context "no current plan" do
-      it { expect(helper.plan_change_word(Plan.new(nil), Plan.new("morph_basic"))).to eql "Signup" }
-      it { expect(helper.plan_change_word(Plan.new(""), Plan.new("morph_basic"))).to eql "Signup" }
-      it { expect(helper.plan_change_word(Plan.new(" "), Plan.new("morph_basic"))).to eql "Signup" }
+      it { expect(helper.plan_change_word(Plan.new(nil), basic)).to eql "Signup" }
+      it { expect(helper.plan_change_word(Plan.new(""), basic)).to eql "Signup" }
+      it { expect(helper.plan_change_word(Plan.new(" "), basic)).to eql "Signup" }
     end
 
     context "plan upgrade" do
-      it { expect(helper.plan_change_word(Plan.new("morph_basic"), Plan.new("morph_standard"))).to eql "Upgrade" }
-      it { expect(helper.plan_change_word(Plan.new("morph_basic"), Plan.new("morph_advanced"))).to eql "Upgrade" }
-      it { expect(helper.plan_change_word(Plan.new("morph_standard"), Plan.new("morph_advanced"))).to eql "Upgrade" }
+      it { expect(helper.plan_change_word(basic, standard)).to eql "Upgrade" }
+      it { expect(helper.plan_change_word(basic, advanced)).to eql "Upgrade" }
+      it { expect(helper.plan_change_word(standard, advanced)).to eql "Upgrade" }
     end
 
     context "plan downgrade" do
-      it { expect(helper.plan_change_word(Plan.new("morph_standard"), Plan.new("morph_basic"))).to eql "Downgrade" }
-      it { expect(helper.plan_change_word(Plan.new("morph_advanced"), Plan.new("morph_basic"))).to eql "Downgrade" }
-      it { expect(helper.plan_change_word(Plan.new("morph_advanced"), Plan.new("morph_standard"))).to eql "Downgrade" }
+      it { expect(helper.plan_change_word(standard, basic)).to eql "Downgrade" }
+      it { expect(helper.plan_change_word(advanced, basic)).to eql "Downgrade" }
+      it { expect(helper.plan_change_word(advanced, standard)).to eql "Downgrade" }
     end
   end
 end
