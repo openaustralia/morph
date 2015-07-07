db9d4fb6d3081c24064289398b5a75558d3ab521
Henare Degan <henare.degan@gmail.com>
2015-07-07 10:36:11 +1000

    Always recreate the Stripe customer from the given token
    We should never hit this page when a user is already signed up but if
    we do it's best to create a new customer in Stripe for the token
    they've just created (i.e. the card they just entered).

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -5,18 +5,12 @@ class SupportersController < ApplicationController
   end
 
   def create
-    # Create or retrieve the Stripe customer
-    if current_user.stripe_customer_id
-      # TODO: Handle missing or deleted customer
-      customer = Stripe::Customer.retrieve current_user.stripe_customer_id
-    else
-      customer = Stripe::Customer.create(
-        email:       params[:stripeEmail],
-        card:        params[:stripeToken],
-        description: "Customer for @#{current_user.nickname}"
-      )
-      current_user.update! stripe_customer_id: customer.id
-    end
+    customer = Stripe::Customer.create(
+      email:       params[:stripeEmail],
+      card:        params[:stripeToken],
+      description: "Customer for @#{current_user.nickname}"
+    )
+    current_user.update! stripe_customer_id: customer.id
 
     # TODO: Handle missing or incorrect plan
     subscription = customer.subscriptions.create plan: params[:plan_id]
