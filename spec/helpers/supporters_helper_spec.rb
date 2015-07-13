4518ebe29c337b79f30d04395eda6143c0a208ff
Henare Degan <henare.degan@gmail.com>
2015-07-13 15:40:05 +1000

    Fix tests so they don't fall over when other model changes

--- spec/helpers/supporters_helper_spec.rb
+++ spec/helpers/supporters_helper_spec.rb
@@ -7,22 +7,23 @@ describe SupportersHelper do
     let(:advanced) { Plan.new("morph_advanced") }
 
     context "no current plan" do
+      # TODO: Refactor or remove?
       context "morph_basic" do
-        it { expect(helper.plan_change_word(Plan.new(nil), basic)).to eql "Become a Supporter" }
-        it { expect(helper.plan_change_word(Plan.new(""), basic)).to eql "Become a Supporter" }
-        it { expect(helper.plan_change_word(Plan.new(" "), basic)).to eql "Become a Supporter" }
+        it { expect(helper.plan_change_word(Plan.new(nil), basic)).to eql "Become a #{basic.name}" }
+        it { expect(helper.plan_change_word(Plan.new(""), basic)).to eql "Become a #{basic.name}" }
+        it { expect(helper.plan_change_word(Plan.new(" "), basic)).to eql "Become a #{basic.name}" }
       end
 
       context "morph_standard" do
-        it { expect(helper.plan_change_word(Plan.new(nil), standard)).to eql "Become a Hero" }
-        it { expect(helper.plan_change_word(Plan.new(""), standard)).to eql "Become a Hero" }
-        it { expect(helper.plan_change_word(Plan.new(" "), standard)).to eql "Become a Hero" }
+        it { expect(helper.plan_change_word(Plan.new(nil), standard)).to eql "Become a #{standard.name}" }
+        it { expect(helper.plan_change_word(Plan.new(""), standard)).to eql "Become a #{standard.name}" }
+        it { expect(helper.plan_change_word(Plan.new(" "), standard)).to eql "Become a #{standard.name}" }
       end
 
       context "morph_advanced" do
-        it { expect(helper.plan_change_word(Plan.new(nil), advanced)).to eql "Become a Champion" }
-        it { expect(helper.plan_change_word(Plan.new(""), advanced)).to eql "Become a Champion" }
-        it { expect(helper.plan_change_word(Plan.new(" "), advanced)).to eql "Become a Champion" }
+        it { expect(helper.plan_change_word(Plan.new(nil), advanced)).to eql "Become a #{advanced.name}" }
+        it { expect(helper.plan_change_word(Plan.new(""), advanced)).to eql "Become a #{advanced.name}" }
+        it { expect(helper.plan_change_word(Plan.new(" "), advanced)).to eql "Become a #{advanced.name}" }
       end
     end
 
