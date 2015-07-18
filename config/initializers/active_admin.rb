595fc346cdb09516e3dd81815ef47be31baddb89
Matthew Landauer <matthew@oaf.org.au>
2015-07-18 11:02:22 +1000

    Add d3 library to active admin

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -170,6 +170,7 @@ ActiveAdmin.setup do |config|
   # To load a javascript file:
   #   config.register_javascript 'my_javascript.js'
 
+  config.register_javascript 'd3.js'
 
   # == CSV options
   #
