de8c19ce0ce9f12a1ff8cb098646d9bc7b297d1a
Matthew Landauer <matthew@oaf.org.au>
2015-07-20 18:02:13 +1000

    Make tooltip for owners show the full name as well if it's available

--- app/helpers/owners_helper.rb
+++ app/helpers/owners_helper.rb
@@ -8,10 +8,23 @@ module OwnersHelper
       options[:class] += " has-tooltip"
       # Trying container: 'body' as a workaround for a bug where gravatars move when
       # tooltips are activated
-      tooltip_text = owner.nickname if tooltip_text.nil?
-      options[:data] = {placement: "bottom", title: tooltip_text, container: 'body'}
+      if tooltip_text.nil?
+        tooltip_text = owner_tooltip_content(owner)
+        html = true
+      else
+        html = false
+      end
+      options[:data] = {placement: "bottom", title: tooltip_text, html: html, container: 'body'}
     end
     options[:alt] = owner.nickname
     image_tag owner.gravatar_url(size), options
+  end
+
+  def owner_tooltip_content(owner)
+    if owner.name
+      "<h4>#{h(owner.name)}</h4><h5>#{h(owner.nickname)}</h5>"
+    else
+      "<h4>#{h(owner.nickname)}</h4>"
+    end
   end
 end
