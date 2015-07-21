89204a8a28db9eb1a580967686e9af745acb56fb
Henare Degan <henare.degan@gmail.com>
2015-07-21 17:34:20 +1000

    Extract view partial from controller's flash

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,18 +2,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Devise::Controllers::Rememberable
 
   def github
-    sign_up = User.find_by(nickname: request.env["omniauth.auth"].info.nickname).blank?
+    @sign_up = User.find_by(nickname: request.env["omniauth.auth"].info.nickname).blank?
 
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
     # Keep people signed in
     remember_me(@user)
 
-    if sign_up
-      @user.watch_all_owners
-      flash[:notice] = "Welcome to morph.io! You're now watching any scrapers you have access to."
-    else
-      flash[:notice] = "Nice person you are. Welcome!"
-    end
+    @user.watch_all_owners if @sign_up
+
+    flash[:notice] = render_to_string(partial: "users/sign_in_message")
 
     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
   end
