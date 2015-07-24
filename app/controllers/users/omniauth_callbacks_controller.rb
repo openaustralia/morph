a7876252cfa794864c29412f937da72285819025
Henare Degan <henare.degan@gmail.com>
2015-07-24 13:27:53 +1000

    Don't show a different message on sign up
    As per feedback from @mlandauer, instead the very first time they
    receive an email alert there should be an introductory paragraph that
    explains why they are receiving that email and what they can do to
    change the settings.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,16 +2,15 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   include Devise::Controllers::Rememberable
 
   def github
-    @sign_up = User.find_by(nickname: request.env["omniauth.auth"].info.nickname).blank?
+    new_sign_up = User.find_by(nickname: request.env["omniauth.auth"].info.nickname).blank?
 
     @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
     # Keep people signed in
     remember_me(@user)
 
-    @user.watch_all_owners if @sign_up
+    @user.watch_all_owners if new_sign_up
 
     flash[:notice] = render_to_string(partial: "users/sign_in_message")
-
     sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
   end
 end
