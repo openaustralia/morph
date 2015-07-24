8b47e47f8e67dd381adba6c14fc89625dc495770
Henare Degan <henare.degan@gmail.com>
2015-07-24 14:15:05 +1000

    When someone is sent an alert record when it happens

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -11,6 +11,9 @@ class AlertMailer < ActionMailer::Base
     @subject = "#{pluralize(broken_scrapers.count, 'scraper')} you are watching #{broken_scrapers.count == 1 ? "is" : "are"} erroring"
 
     attachments.inline['logo_75x75.png'] = File.read(File.join(Rails.root, "app", "assets", path_to_image("logo_75x75.png")))
-    mail(to: "#{user.name} <#{user.email}>", subject: @subject) if user.email
+    if user.email
+      mail(to: "#{user.name} <#{user.email}>", subject: @subject)
+      user.update(alerted_at: Time.current)
+    end
   end
 end
