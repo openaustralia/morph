8da1039ddde8519cff0cbd15974c99511b749a60
Matthew Landauer <matthew@oaf.org.au>
2015-07-28 09:03:23 +1000

    Make wording in email alert more specific about when scrapers appear in the list

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,8 @@
 module UsersHelper
   def alert_scrapers_summary_sentence(success_count, broken_count)
     result = pluralize(success_count, "scraper") + " you are watching "
-    result += success_count == 1 ? "is" : "are"
-    result += " working. "
+    result += success_count == 1 ? "has" : "have"
+    result += " run successfully in the last 48 hours. "
     result += broken_count == 1 ? "This" : "These"
     result += " " + broken_count.to_s + " "
     result += broken_count == 1 ? "has" : "have"
