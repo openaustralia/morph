6da68b1a3465bdb6bf55a4f5f14ac06642aeab9c
Henare Degan <henare.degan@gmail.com>
2016-01-08 14:57:36 +1100

    Split method into smaller methods

--- lib/morph/docker_maintenance.rb
+++ lib/morph/docker_maintenance.rb
@@ -1,21 +1,29 @@
 module Morph
   class DockerMaintenance
     def self.delete_container_and_remove_image(container)
+      delete_container(container)
+      remove_image(container_image(container))
+    end
+
+    private
+
+    def self.delete_container(container)
       Rails.logger.info "Deleting container #{container}..."
       container.delete
+    end
 
-      begin
-        # Get container image ID and strip tag
-        image_id = container.info["Image"].split(":").first
-        i = Docker::Image.get(image_id)
+    def self.container_image(container)
+      image_id = container.info["Image"].split(":").first
+      Docker::Image.get(image_id)
+    end
 
-        Rails.logger.info "Removing image #{i.id}..."
-        i.remove
-      rescue Docker::Error::ConfictError
-        Rails.logger.warn "Conflict removing image, skipping..."
-      rescue Docker::Error::NotFoundError
-        Rails.logger.warn "Couldn't find container image, skipping..."
-      end
+    def self.remove_image
+      Rails.logger.info "Removing image #{i.id}..."
+      i.remove
+    rescue Docker::Error::ConfictError
+      Rails.logger.warn "Conflict removing image, skipping..."
+    rescue Docker::Error::NotFoundError
+      Rails.logger.warn "Couldn't find container image, skipping..."
     end
   end
 end
