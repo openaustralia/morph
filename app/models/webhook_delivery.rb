dfbb13acf2a1d74cd9b6de531919ec490ad74754
Chris Mytton <chrismytton@gmail.com>
2016-01-25 23:35:33 +0000

    Specs for webhooks and webhook deliveries

--- app/models/webhook_delivery.rb
+++ app/models/webhook_delivery.rb
@@ -1,4 +1,10 @@
 class WebhookDelivery < ActiveRecord::Base
+  SUCCESSFUL_STATUSES = 200..299
+
   belongs_to :webhook
   belongs_to :run
+
+  def success?
+    SUCCESSFUL_STATUSES.include?(response_code)
+  end
 end
--- spec/models/webhook_delivery_spec.rb
+++ spec/models/webhook_delivery_spec.rb
@@ -1,5 +1,10 @@
-require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe WebhookDelivery, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '#success?' do
+    it 'returns true if the status code is 2xx' do
+      webhook_delivery = WebhookDelivery.new(response_code: 200)
+      expect(webhook_delivery.success?).to be true
+    end
+  end
 end
