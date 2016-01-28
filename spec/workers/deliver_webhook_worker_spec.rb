1ea3f66f47a0e006eb8250fc181436d6e77dbbed
Chris Mytton <chrismytton@gmail.com>
2016-01-28 22:51:16 +0000

    Add spec for DeliverWebhookWorker

---
+++ spec/workers/deliver_webhook_worker_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe DeliverWebhookWorker, :vcr do
+  it 'works' do
+    VCR.use_cassette('webhook_delivery') do
+      webhook = Webhook.create!(url: 'http://requestb.in/x3pcr8x3')
+      webhook_delivery = webhook.deliveries.create!
+      DeliverWebhookWorker.new.perform(webhook_delivery.id)
+      webhook_delivery.reload
+      expect(webhook_delivery.response_code).to be(200)
+      expect(webhook_delivery.sent_at).to be_within(1.minute).of(DateTime.now)
+    end
+  end
+end
