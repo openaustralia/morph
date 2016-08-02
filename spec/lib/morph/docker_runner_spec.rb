88ff2d59b47dcc9a98ce6d645ebe1a6d4c737b85
Matthew Landauer <mlandauer@gmail.com>
2016-08-02 15:56:49 +1000

    Test that a failing python compile will stop

--- spec/lib/morph/docker_runner_spec.rb
+++ spec/lib/morph/docker_runner_spec.rb
@@ -30,6 +30,13 @@ describe Morph::DockerRunner do
         .to eq @container_count
     end
 
+    it "should stop if a python compile fails" do
+      copy_test_scraper('failing_compile_python')
+      c, i3 = Morph::DockerRunner.compile_and_start_run(@dir, {}, {}) {}
+      expect(c).to be_nil
+      expect(i3).to be_nil
+    end
+
     it 'should be able to run hello world' do
       copy_test_scraper('hello_world_js')
 
