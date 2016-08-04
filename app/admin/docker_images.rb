38097f41c490df8c52970f3c382a8f393f7c413e
Matthew Landauer <mlandauer@gmail.com>
2016-08-04 13:25:26 +1000

    Fix up last used by run column in docker images admin page

--- app/admin/docker_images.rb
+++ app/admin/docker_images.rb
@@ -17,7 +17,10 @@ ActiveAdmin.register_page 'Docker Images' do
               time_ago_in_words(Time.at(image.info['Created'])) + ' ago'
             end
             td do
-              time = Run.where(docker_image: image.id[0..11]).maximum(:created_at)
+              # We're getting an image id which is in a different form than
+              # the one that's stored in the database
+              image_id = image.id.split(':')[1][0..11]
+              time = Run.where(docker_image: image_id).maximum(:created_at)
               time_ago_in_words(time) + ' ago' if time
             end
           end
