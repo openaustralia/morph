47727072985f185a9eedab58cb58fb45b8037715
Matthew Landauer <mlandauer@gmail.com>
2016-08-17 16:35:19 +1000

    Update initializer to only do something if the settings table exists

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,5 @@
 # At Rails startup ensure that the sidekiq limit is in synch with the site
 # setting
-SiteSetting.update_sidekiq_maximum_concurrent_scrapers!
+if SiteSetting.table_exists?
+  SiteSetting.update_sidekiq_maximum_concurrent_scrapers!
+end
