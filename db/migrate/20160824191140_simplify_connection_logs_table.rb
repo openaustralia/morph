4a8d9a469a2555be7816e3be51f9fad101ce8a1b
Matthew Landauer <mlandauer@gmail.com>
2016-08-25 15:07:39 +1000

    Revert "Revert migration so that it can be applied later"
    This reverts commit f5f9af9e70eebae9f38e452a0ff286c36febd67d.

---
+++ db/migrate/20160824191140_simplify_connection_logs_table.rb
@@ -0,0 +1,27 @@
+class SimplifyConnectionLogsTable < ActiveRecord::Migration
+  def change
+    # Removing a bunch of data that we were wanting to collect but weren't
+    # actually using. Doing this because this table was growing very large
+    # in production >30GB.
+    reversible do |dir|
+      dir.up do
+        change_table :connection_logs do |t|
+          t.remove :response_code, :response_size, :request_size, :path,
+                   :scheme, :method, :updated_at
+        end
+      end
+
+      dir.down do
+        change_table :connection_logs do |t|
+          t.integer :response_code, limit: 4
+          t.integer :response_size, limit: 4
+          t.integer :request_size,  limit: 4
+          t.text :path, limit: 65535
+          t.string :scheme, limit: 255
+          t.string :method, limit: 255
+          t.datetime :updated_at
+        end
+      end
+    end
+  end
+end
