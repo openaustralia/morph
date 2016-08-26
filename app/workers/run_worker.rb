a6a3e7c4c099366b5d290f345ac14a9c50586c68
Matthew Landauer <mlandauer@gmail.com>
2016-08-26 15:11:39 +1000

    Remove sidekiq unique jobs. Fixes #1060

--- app/workers/run_worker.rb
+++ app/workers/run_worker.rb
@@ -4,7 +4,7 @@ class RunWorker
 
   include Sidekiq::Worker
   # TODO: Make backtrace: true to be a default option for all workers
-  sidekiq_options queue: :scraper, backtrace: true, unique: true
+  sidekiq_options queue: :scraper, backtrace: true
 
   def perform(run_id)
     run = Run.find_by(id: run_id)
