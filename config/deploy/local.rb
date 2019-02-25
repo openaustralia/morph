5fd836ed4db65b58ffc2eb9a003c31e3b363789d
James Polley <jp@jamezpolley.com>
2019-02-25 17:55:03 +1100

    When deploying locally, use the local git repo

--- config/deploy/local.rb
+++ config/deploy/local.rb
@@ -1,6 +1,20 @@
 set :stage, :local
 set :rails_env, :production
 
+before :deploy, :deploy_from_local_repo
+
+task :deploy_from_local_repo do
+  set :repo_url,  "file:///tmp/.git"
+  set :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
+  run_locally do
+    execute "tar -zcvf /tmp/repo.tgz .git"
+  end
+  on roles(:all) do
+    upload! '/tmp/repo.tgz', '/tmp/repo.tgz'
+    execute 'tar -zxvf /tmp/repo.tgz -C /tmp'
+  end
+end
+
 role :app, %w{deploy@localhost}
 role :web, %w{deploy@localhost}
 role :db,  %w{deploy@localhost}
