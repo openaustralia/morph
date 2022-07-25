05ddb977646977873e278ef4f4aaf2d65247b22b
Matthew Landauer <mlandauer@gmail.com>
2022-07-25 16:30:20 +1000

    Remove empty spec files to keep newer rubocop checks happy

--- spec/controllers/application_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-describe ApplicationController do
-end
--- spec/controllers/supporters_controller_spec.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-require "spec_helper"
-
-describe SupportersController do
-end
