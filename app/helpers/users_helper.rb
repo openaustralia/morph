2700ef8c84026a981341ccd2d3b67124f270e31c
Matthew Landauer <mlandauer@gmail.com>
2022-07-25 16:15:26 +1000

    Fix for rubocop change where I broke it

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,13 +5,13 @@ module UsersHelper
     result = []
     result << pluralize(success_count, "scraper")
     result << " you are watching "
-    result << success_count == 1 ? "has" : "have"
+    result << (success_count == 1 ? "has" : "have")
     result << " run successfully in the last 48 hours. "
-    result << broken_count == 1 ? "This" : "These"
+    result << (broken_count == 1 ? "This" : "These")
     result << " "
     result << broken_count.to_s
     result << " "
-    result << broken_count == 1 ? "has" : "have"
+    result << (broken_count == 1 ? "has" : "have")
     result << " a problem:"
     safe_join(result)
   end
