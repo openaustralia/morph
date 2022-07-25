249d47056bab5a94bd794f1e0eb9c9bdbaf15bcd
Matthew Landauer <mlandauer@gmail.com>
2022-07-25 16:08:07 +1000

    Keep rubocop happy

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,13 +2,17 @@
 
 module UsersHelper
   def alert_scrapers_summary_sentence(success_count, broken_count)
-    result = pluralize(success_count, "scraper") + " you are watching "
-    result += success_count == 1 ? "has" : "have"
-    result += " run successfully in the last 48 hours. "
-    result += broken_count == 1 ? "This" : "These"
-    result += " " + broken_count.to_s + " "
-    result += broken_count == 1 ? "has" : "have"
-    result += " a problem:"
-    result
+    result = []
+    result << pluralize(success_count, "scraper")
+    result << " you are watching "
+    result << success_count == 1 ? "has" : "have"
+    result << " run successfully in the last 48 hours. "
+    result << broken_count == 1 ? "This" : "These"
+    result << " "
+    result << broken_count.to_s
+    result << " "
+    result << broken_count == 1 ? "has" : "have"
+    result << " a problem:"
+    safe_join(result)
   end
 end
