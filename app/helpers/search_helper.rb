85c3f148b881b5ee4bcd0a9e1da6b56ab7a0f476
Matthew Landauer <mlandauer@gmail.com>
2022-07-25 15:53:09 +1000

    Keep rubocop happy

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -2,6 +2,6 @@
 
 module SearchHelper
   def no_search_results_message(things, search_term)
-    "Sorry, we couldn't find any ".html_safe + h(things) + " relevant to your search term " + content_tag(:strong, "“" + search_term + "”") + "."
+    safe_join(["Sorry, we couldn't find any ", things, " relevant to your search term ", content_tag(:strong, "“#{search_term}”"), "."])
   end
 end
--- spec/helpers/search_helper_spec.rb
+++ spec/helpers/search_helper_spec.rb
@@ -4,18 +4,18 @@ require "spec_helper"
 
 describe SearchHelper do
   describe ".no_search_results_message" do
-    it { expect(helper.no_search_results_message("scrapers", "bibble")).to eq "Sorry, we couldn't find any scrapers relevant to your search term <strong>“bibble”</strong>." }
+    it { expect(helper.no_search_results_message("scrapers", "bibble")).to eq "Sorry, we couldn&#39;t find any scrapers relevant to your search term <strong>“bibble”</strong>." }
 
     it "should allow html in things" do
-      expect(helper.no_search_results_message("scrapers <em>with data</em>".html_safe, "bibble")).to eq "Sorry, we couldn't find any scrapers <em>with data</em> relevant to your search term <strong>“bibble”</strong>."
+      expect(helper.no_search_results_message("scrapers <em>with data</em>".html_safe, "bibble")).to eq "Sorry, we couldn&#39;t find any scrapers <em>with data</em> relevant to your search term <strong>“bibble”</strong>."
     end
 
     it "should allow escape html if things is not html safe" do
-      expect(helper.no_search_results_message("scrapers <em>with data</em>", "bibble")).to eq "Sorry, we couldn't find any scrapers &lt;em&gt;with data&lt;/em&gt; relevant to your search term <strong>“bibble”</strong>."
+      expect(helper.no_search_results_message("scrapers <em>with data</em>", "bibble")).to eq "Sorry, we couldn&#39;t find any scrapers &lt;em&gt;with data&lt;/em&gt; relevant to your search term <strong>“bibble”</strong>."
     end
 
     it "should escape the search term" do
-      expect(helper.no_search_results_message("scrapers <em>with data</em>", "<bibble>")).to eq "Sorry, we couldn't find any scrapers &lt;em&gt;with data&lt;/em&gt; relevant to your search term <strong>“&lt;bibble&gt;”</strong>."
+      expect(helper.no_search_results_message("scrapers <em>with data</em>", "<bibble>")).to eq "Sorry, we couldn&#39;t find any scrapers &lt;em&gt;with data&lt;/em&gt; relevant to your search term <strong>“&lt;bibble&gt;”</strong>."
     end
 
     it "should be html safe" do
