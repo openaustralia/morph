8254bfdb522b7355dcb1766b86c9d29094e91154
Matthew Landauer <mlandauer@gmail.com>
2022-07-25 16:19:05 +1000

    Keep rubocop happy

--- app/controllers/admin/site_settings_controller.rb
+++ app/controllers/admin/site_settings_controller.rb
@@ -5,7 +5,7 @@ module Admin
     def toggle_read_only_mode
       authorize! :toggle_read_only_mode, SiteSetting
       SiteSetting.toggle_read_only_mode!
-      flash[:notice] = "Read-only mode is now " + (SiteSetting.read_only_mode ? "on" : "off")
+      flash[:notice] = "Read-only mode is now #{SiteSetting.read_only_mode ? 'on' : 'off'}"
       redirect_to admin_dashboard_url
     end
 
