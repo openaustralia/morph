54147208c8a1fbb7d776c5503a11ed1b40944ba9
Matthew Landauer <mlandauer@gmail.com>
2022-07-29 14:45:16 +1000

    Small rubocop fix

--- app/workers/deliver_webhook_worker.rb
+++ app/workers/deliver_webhook_worker.rb
@@ -9,7 +9,7 @@ class DeliverWebhookWorker
     # TODO: As soon as we've upgraded to a recent version of Ubuntu switch the SSL
     # verification back on. It's not crazy-super important that we verify SSL with
     # webhooks but it's sure a good idea.
-    connection = Faraday.new(webhook_delivery.webhook.url, ssl: {verify: false})
+    connection = Faraday.new(webhook_delivery.webhook.url, ssl: { verify: false })
     response = connection.post
     webhook_delivery.update_attributes(
       response_code: response.status,
