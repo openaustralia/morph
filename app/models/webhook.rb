1d5b0316b22959e49798fb066fe6fbaf73a78559
Matthew Landauer <mlandauer@gmail.com>
2022-08-01 13:00:35 +1000

    Get rid of new deprecation warning

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -2,7 +2,7 @@
 
 class Webhook < ApplicationRecord
   belongs_to :scraper
-  has_many :deliveries, class_name: WebhookDelivery, dependent: :delete_all
+  has_many :deliveries, class_name: "WebhookDelivery", dependent: :delete_all
   validates :url, presence: true, url: true, uniqueness: { scope: :scraper, message: "already another webhook with this URL" }
 
   def last_delivery
