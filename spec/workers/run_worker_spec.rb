83d359d817785983cd3bc556650925e052645e91
Matthew Landauer <mlandauer@gmail.com>
2022-08-02 12:16:32 +1000

    Fix a few small rubocop rspec things

--- spec/workers/run_worker_spec.rb
+++ spec/workers/run_worker_spec.rb
@@ -7,18 +7,19 @@ describe RunWorker do
 
   it "calls the runner" do
     run = Run.create!(owner: user)
-    runner = double
-    expect(Morph::Runner).to receive(:new).with(run).and_return(runner)
-    expect(runner).to receive(:synch_and_go!)
+    runner = instance_double(Morph::Runner, synch_and_go!: nil)
+    allow(Morph::Runner).to receive(:new).with(run).and_return(runner)
 
     described_class.new.perform(run.id)
+
+    expect(runner).to have_received(:synch_and_go!)
   end
 
   it "does nothing if the run does not exist anymore" do
     described_class.new.perform(123456)
   end
 
-  context do
+  context "with no container for run 123456" do
     before do
       Morph::DockerUtils.find_all_containers_with_label_and_value(Morph::Runner.run_label_key, "123456").each(&:delete)
     end
@@ -29,13 +30,13 @@ describe RunWorker do
 
     it "raises an exception if we already have the maximum number of running containers" do
       run = Run.create!(owner: user, id: 123456)
-      expect(Morph::Runner).to receive(:available_slots).and_return(0)
+      allow(Morph::Runner).to receive(:available_slots).and_return(0)
       expect { described_class.new.perform(run.id) }.to raise_error RunWorker::NoRemainingSlotsError
     end
 
     it "does not raise an exception if we are finishing off an already running container", docker: true do
       run = Run.create!(owner: user, id: 123456)
-      expect(Morph::Runner).to receive(:available_slots).and_return(0)
+      allow(Morph::Runner).to receive(:available_slots).and_return(0)
       Docker::Container.create(
         "Cmd" => ["ls"],
         "Image" => "openaustralia/buildstep",
