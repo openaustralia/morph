cc8eac54d4264ac3dcebb4e386a1c08b39c9cf9e
Matthew Landauer <mlandauer@gmail.com>
2022-08-09 09:27:02 +1000

    More typechecking

--- app/controllers/supporters_controller.rb
+++ app/controllers/supporters_controller.rb
@@ -1,4 +1,4 @@
-# typed: false
+# typed: true
 # frozen_string_literal: true
 
 class SupportersController < ApplicationController
@@ -12,58 +12,65 @@ class SupportersController < ApplicationController
   end
 
   def create
+    authenticated_user = T.must(current_user)
+
     customer = Stripe::Customer.create(
-      email: current_user.email,
+      email: authenticated_user.email,
       card: params[:stripeToken],
-      description: "morph.io user @#{current_user.nickname}"
+      description: "morph.io user @#{authenticated_user.nickname}"
     )
-    current_user.update! stripe_customer_id: customer.id
+    authenticated_user.update! stripe_customer_id: customer.id
 
     # TODO: Handle missing or incorrect plan
     subscription = customer.subscriptions.create plan: params[:plan_id]
-    current_user.update! stripe_plan_id: subscription[:plan][:id], stripe_subscription_id: subscription[:id]
+    authenticated_user.update! stripe_plan_id: subscription[:plan][:id], stripe_subscription_id: subscription[:id]
 
     session[:new_supporter] = true
-    redirect_to user_path(current_user), notice: render_to_string(partial: "create_flash")
+    redirect_to user_path(authenticated_user), notice: render_to_string(partial: "create_flash")
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to supporters_path
   end
 
   def create_one_time
+    authenticated_user = T.must(current_user)
+    params_amount = T.cast(params[:amount], T.any(String, Numeric))
+
     customer = Stripe::Customer.create(
-      email: current_user.email,
+      email: authenticated_user.email,
       card: params[:stripeTokenOneTime],
-      description: "morph.io user @#{current_user.nickname}"
+      description: "morph.io user @#{authenticated_user.nickname}"
     )
-    current_user.update! stripe_customer_id: customer.id
+    authenticated_user.update! stripe_customer_id: customer.id
 
     Stripe::Charge.create(
       customer: customer.id,
-      amount: (params[:amount].to_f * 100).round,
+      amount: (params_amount.to_f * 100).round,
       description: "morph.io contribution",
       currency: "USD"
     )
 
     session[:new_supporter] = true
-    redirect_to user_path(current_user), notice: render_to_string(partial: "one_time_contribution_thanks")
+    redirect_to user_path(authenticated_user), notice: render_to_string(partial: "one_time_contribution_thanks")
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to supporters_path
   end
 
   def update
-    @from_plan = current_user.plan
+    authenticated_user = T.must(current_user)
+
+    @from_plan = authenticated_user.plan
     @to_plan = Plan.new(params[:plan_id])
 
-    customer = Stripe::Customer.retrieve current_user.stripe_customer_id
-    subscription = customer.subscriptions.retrieve current_user.stripe_subscription_id
+    customer = Stripe::Customer.retrieve T.must(authenticated_user.stripe_customer_id)
+    subscription = customer.subscriptions.retrieve authenticated_user.stripe_subscription_id
     subscription.plan = params[:plan_id]
     subscription.save
-    current_user.update! stripe_plan_id: subscription[:plan][:id]
+    authenticated_user.update! stripe_plan_id: subscription[:plan][:id]
 
     session[:new_supporter] = true
-    redirect_to user_path(current_user), notice: render_to_string(partial: "update_flash")
+    redirect_to user_path(authenticated_user), notice: render_to_string(partial: "update_flash")
   end
 
   private
