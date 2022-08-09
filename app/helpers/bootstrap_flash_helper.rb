9da9ef57c3e6b67636e9aea64b2929bab41cbabc
Matthew Landauer <mlandauer@gmail.com>
2022-08-09 13:50:52 +1000

    I think the includes were breaking things when running the web app

--- app/helpers/bootstrap_flash_helper.rb
+++ app/helpers/bootstrap_flash_helper.rb
@@ -1,13 +1,13 @@
-# typed: true
+# typed: false
 # frozen_string_literal: true
 
 # Copied from twitter-bootstrap-rails gem https://github.com/seyhunak/twitter-bootstrap-rails/blob/b701e23e91bd2726af5bc669b87f7f34efc96ab1/app/helpers/bootstrap_flash_helper.rb
 
 module BootstrapFlashHelper
   # For sorbet
-  include ActionView::Helpers::TagHelper
-  include Kernel
-  include ActionDispatch::Flash::RequestMethods
+  # include ActionView::Helpers::TagHelper
+  # include Kernel
+  # include ActionDispatch::Flash::RequestMethods
 
   ALERT_TYPES = %i[success info warning danger].freeze unless const_defined?(:ALERT_TYPES)
 
