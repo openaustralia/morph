a55fe3e78cd8070fb0edda1d6fee45af89cab1c9
Matthew Landauer <mlandauer@gmail.com>
2022-08-11 16:44:56 -0600

    Improve typechecking

--- app/lib/morph/docker_maintenance.rb
+++ app/lib/morph/docker_maintenance.rb
@@ -1,8 +1,11 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Morph
   class DockerMaintenance
+    extend T::Sig
+
+    sig { params(container: Docker::Container).void }
     def self.delete_container(container)
       Rails.logger.info "Deleting container #{container.id}..."
       container.delete
@@ -10,6 +13,7 @@ module Morph
       Rails.logger.warn "Exception while removing container #{container.id}: #{e.inspect}"
     end
 
+    sig { params(image_id: String).void }
     def self.remove_image(image_id)
       Rails.logger.info "Removing image #{image_id}..."
       Docker::Image.get(image_id).remove
