c1e19adaaaba5110ef0494b6bc66b44888e330f0
Matthew Landauer <mlandauer@gmail.com>
2022-08-13 09:01:52 -0600

    More typechecking

--- app/lib/morph/vanity_stats.rb
+++ app/lib/morph/vanity_stats.rb
@@ -1,32 +1,26 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module Morph
   # Calculate vanity stats that we show on the landing page
   class VanityStats
+    extend T::Sig
+
     # Include rows that have been changed and created
+    sig { returns(Integer) }
     def self.total_database_rows_updated_in_last_week
       Run.where("finished_at > ?", 7.days.ago).sum(:records_added) +
         Run.where("finished_at > ?", 7.days.ago).sum(:records_changed)
     end
 
+    sig { returns(Integer) }
     def self.total_pages_scraped_in_last_week
       ConnectionLog.where("created_at > ?", 7.days.ago).count
     end
 
+    sig { returns(Integer) }
     def self.total_api_queries_in_last_week
       ApiQuery.where("created_at > ?", 7.days.ago).count
-    end
-
-    # TODO: Speed this up by storing a cached version of this information
-    # in the database
-    def self.total_database_rows
-      Scraper.all.to_a.sum(&:sqlite_total_rows)
-    end
-
-    # Round down to the nearest million
-    def self.rounded_total_database_rows_in_millions
-      (total_database_rows / 1_000_000.0).floor
     end
   end
 end
