1d2be048529511f3e1186e8aec687481045891f2
Matthew Landauer <mlandauer@gmail.com>
2022-08-14 12:11:05 -0600

    More typechecking

--- app/models/connection_log.rb
+++ app/models/connection_log.rb
@@ -1,9 +1,17 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 # A record of an http/https request from a scraper to the outside world
 class ConnectionLog < ApplicationRecord
-  attr_accessor :ip_address
+  extend T::Sig
+
+  sig { returns(T.nilable(String)) }
+  attr_reader :ip_address
+
+  sig { params(ip_address: String).void }
+  def ip_address=(ip_address)
+    @ip_address = T.let(ip_address, T.nilable(String))
+  end
 
   belongs_to :domain
   # It's not actually optional. It hopefully gets set by the callback below
@@ -11,6 +19,7 @@ class ConnectionLog < ApplicationRecord
   belongs_to :run, optional: true
   before_save :update_run_id_from_ip_address
 
+  sig { void }
   def update_run_id_from_ip_address
     return if run_id
 
