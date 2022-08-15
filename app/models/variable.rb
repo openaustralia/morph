95cf8d9cef3c5d01610fc6a001955b3d3be8fbb5
Matthew Landauer <mlandauer@gmail.com>
2022-08-15 02:45:10 -0400

    More typechecking

--- app/models/variable.rb
+++ app/models/variable.rb
@@ -1,8 +1,10 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 # A secret environment variable and its value that can be passed to a scraper
 class Variable < ApplicationRecord
+  extend T::Sig
+
   belongs_to :scraper
   validates :name, format: {
     with: /\AMORPH_[A-Z0-9_]+\z/,
@@ -11,6 +13,7 @@ class Variable < ApplicationRecord
   validates :value, presence: true
 
   # Given an array of Variable objects returns a hash of names and values
+  sig { params(variables: T::Array[Variable]).returns(T::Hash[String, String]) }
   def self.to_hash(variables)
     variables.to_h { |v| [v.name, v.value] }
   end
