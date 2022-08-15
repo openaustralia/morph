6e8c1fa792b9dbdd05177c45cc0436d97bb05a79
Matthew Landauer <mlandauer@gmail.com>
2022-08-15 02:35:35 -0400

    Improve typechecking

--- app/models/webhook.rb
+++ app/models/webhook.rb
@@ -1,11 +1,14 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class Webhook < ApplicationRecord
+  extend T::Sig
+
   belongs_to :scraper
   has_many :deliveries, class_name: "WebhookDelivery", dependent: :delete_all
   validates :url, presence: true, url: true, uniqueness: { scope: :scraper, message: "already another webhook with this URL" }
 
+  sig { returns(WebhookDelivery) }
   def last_delivery
     deliveries.order(sent_at: :desc).first
   end
--- app/models/webhook_delivery.rb
+++ app/models/webhook_delivery.rb
@@ -1,12 +1,14 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class WebhookDelivery < ApplicationRecord
-  SUCCESSFUL_STATUSES = (200..299).freeze
+  extend T::Sig
+  SUCCESSFUL_STATUSES = T.let((200..299).freeze, T::Range[Integer])
 
   belongs_to :webhook
   belongs_to :run
 
+  sig { returns(T::Boolean) }
   def success?
     SUCCESSFUL_STATUSES.include?(response_code)
   end
