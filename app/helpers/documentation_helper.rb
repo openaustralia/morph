685ae6a8e480471de441a542f8b7f78043ca7c59
Matthew Landauer <mlandauer@gmail.com>
2022-08-15 20:37:06 -0400

    Remove unused code

--- app/helpers/documentation_helper.rb
+++ app/helpers/documentation_helper.rb
@@ -5,17 +5,7 @@ module DocumentationHelper
   # For sorbet
   include ActionView::Helpers::UrlHelper
 
-  def improve_button(text, file, options = {})
-    spacer = if options.key?(:spacer)
-               options[:spacer]
-             else
-               true
-             end
-    c = if spacer
-          "btn btn-default improve pull-right"
-        else
-          "btn btn-default pull-right"
-        end
-    link_to text, "https://github.com/openaustralia/morph/blob/master/app/views/documentation/#{file}", class: c
+  def improve_button(text, file)
+    link_to text, "https://github.com/openaustralia/morph/blob/master/app/views/documentation/#{file}", class: "btn btn-default improve pull-right"
   end
 end
