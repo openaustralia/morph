718a6e0a0190af0140a3ad58e4ac9fb178885998
Matthew Landauer <mlandauer@gmail.com>
2022-08-16 07:27:27 -0400

    Improve typechecking

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 module ApplicationHelper
+  extend T::Sig
+
   # For sorbet
   include ActionView::Helpers::UrlHelper
   include ActionView::Helpers::OutputSafetyHelper
@@ -9,30 +11,29 @@ module ApplicationHelper
   include ActionView::Helpers::DateHelper
   include Kernel
 
+  sig { params(secs: Integer).returns(String) }
   def duration_of_time_in_words(secs)
     distance_of_time_in_words(0, secs, include_seconds: true)
   end
 
-  def button_link_to(name = nil, options = {}, html_options = {}, &block)
-    if block_given?
-      html_options = options
-      options = name
-      name = capture(&block)
-    end
+  sig { params(name: String, link: String, html_options: T::Hash[Symbol, String]).returns(String) }
+  def button_link_to(name, link, html_options = {})
     html_options[:class] ||= ""
-    html_options[:class] += " btn btn-default"
+    html_options[:class] = "#{T.must(html_options[:class])} btn btn-default"
 
     if html_options[:disabled]
       content_tag(:span, name, html_options)
     else
-      link_to(name, options, html_options)
+      link_to(name, link, html_options)
     end
   end
 
+  sig { params(text: String, url: String).returns(String) }
   def bs_nav_link(text, url)
     content_tag(:li, link_to(text, url), class: ("active" if current_page?(url)))
   end
 
+  sig { params(key: Symbol, options: T::Hash[T.untyped, T.untyped]).returns(String) }
   def language_name_with_icon(key, options = {})
     l = Morph::Language.new(key)
     safe_join([image_tag(l.image_path, options), " ", l.human])
