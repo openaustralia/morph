36ad4f452ce63b94006a6789a41bd63d87ba6438
Matthew Landauer <mlandauer@gmail.com>
2022-08-18 12:23:12 -0400

    More typechecking

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
-# typed: true
+# typed: strict
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+  extend T::Sig
+
   before_action :authenticate_user!, except: :index
   before_action :load_resource, except: %i[index stats]
   load_and_authorize_resource
 
+  sig { void }
   def index
-    @users = @users.order(created_at: :desc)
+    @users = T.let(@users.order(created_at: :desc), T.untyped)
     respond_to do |format|
       format.html do
         @users = @users.page(params[:page])
@@ -16,12 +19,16 @@ class UsersController < ApplicationController
     end
   end
 
+  sig { void }
   def watching; end
+
+  sig { void }
   def stats; end
 
   private
 
+  sig { void }
   def load_resource
-    @user = User.friendly.find(params[:id])
+    @user = T.let(User.friendly.find(params[:id]), T.nilable(User))
   end
 end
