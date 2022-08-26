2c5e77ad49b66bc083f77401dd52180cd2cca358
Matthew Landauer <mlandauer@gmail.com>
2022-08-26 08:23:34 -0400

    Swallow particular delivery error on webhook

--- app/workers/deliver_webhook_worker.rb
+++ app/workers/deliver_webhook_worker.rb
@@ -14,10 +14,16 @@ class DeliverWebhookWorker
     # verification back on. It's not crazy-super important that we verify SSL with
     # webhooks but it's sure a good idea.
     connection = Faraday.new(webhook_delivery.webhook.url, ssl: { verify: false })
-    response = connection.post
-    webhook_delivery.update(
-      response_code: response.status,
-      sent_at: DateTime.now
-    )
+    begin
+      response = connection.post
+      webhook_delivery.update(
+        response_code: response.status,
+        sent_at: DateTime.now
+      )
+    rescue Faraday::ConnectionFailed => e
+      # Swallow connection failures to a webhook so that they don't show up alongside other much
+      # more important errors. Also, we don't want these just constantly retrying after failures
+      Rails.logger.error("Webhook delivery failure on scraper #{webhook_delivery.run.scraper.full_name}: #{e}")
+    end
   end
 end
