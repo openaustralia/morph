3235b9f535afe1f005542868760d420433fd58fc
Matthew Landauer <mlandauer@gmail.com>
2022-09-14 10:51:59 +1000

    Remove old (and probably out of date) comments

--- app/lib/ability.rb
+++ app/lib/ability.rb
@@ -65,31 +65,5 @@ class Ability
     can :update_sidekiq_maximum_concurrent_scrapers, SiteSetting if user.admin?
 
     can :create, Run unless SiteSetting.read_only_mode
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/ryanb/cancan/wiki/Defining-Abilities
   end
 end
