b31004b20e98a7ab10ef5fd6dce7d2d24429fc8f
Matthew Landauer <mlandauer@gmail.com>
2022-09-19 09:30:25 +1000

    No need to duplication admin authorization inside admin panel
    By moving the controller actions to within active_admin we take advantage of the admin authorization that's already setup there so we don't need to duplicate this which means we can get rid of a whole SiteSettingAbility class

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,6 +4,20 @@
 ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
+  page_action :toggle_read_only_mode, method: :post do
+    SiteSetting.toggle_read_only_mode!
+    flash[:notice] = "Read-only mode is now #{SiteSetting.read_only_mode ? 'on' : 'off'}"
+    redirect_to admin_dashboard_url
+  end
+
+  page_action :update_maximum_concurrent_scrapers, method: :post do
+    params_maximum_concurrent_scrapers = T.cast(params[:maximum_concurrent_scrapers], T.any(String, Numeric))
+
+    SiteSetting.maximum_concurrent_scrapers = params_maximum_concurrent_scrapers.to_i
+    flash[:notice] = "Updated maximum concurrent scrapers to #{SiteSetting.maximum_concurrent_scrapers}"
+    redirect_to admin_dashboard_url
+  end
+
   content title: proc { I18n.t("active_admin.dashboard") } do
     columns do
       column do
@@ -24,9 +38,9 @@ ActiveAdmin.register_page "Dashboard" do
     div class: "blank_slate_container", id: "dashboard_default_message" do
       para do
         if SiteSetting.read_only_mode
-          button_to "Switch off site-wide read-only mode", toggle_read_only_mode_admin_site_settings_path
+          button_to "Switch off site-wide read-only mode", admin_dashboard_toggle_read_only_mode_path
         else
-          button_to "Go into site-wide read-only mode", toggle_read_only_mode_admin_site_settings_path
+          button_to "Go into site-wide read-only mode", admin_dashboard_toggle_read_only_mode_path
         end
       end
       para do
