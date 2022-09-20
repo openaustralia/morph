86113fd9f3ebf33d2549d5af8ce36dab56d4371a
Matthew Landauer <mlandauer@gmail.com>
2022-09-20 11:30:30 +1000

    Filter search results for scrapers by those we are allowed to see

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -17,9 +17,22 @@ class SearchController < ApplicationController
     @show = T.let(show, T.nilable(String))
 
     @owners = T.let(Owner.search(@q, default_owner_search_params), T.nilable(Searchkick::Relation))
-    @all_scrapers = T.let(Scraper.search(@q, default_scraper_search_params), T.nilable(Searchkick::Relation))
-    @filtered_scrapers = T.let(Scraper.search(@q, default_scraper_search_params.merge(where: { data?: true })), T.nilable(Searchkick::Relation))
+    # We're sending the id of every scraper we are allowed to view to searchkick. This is obviously not ideal as it will grow as the number of scrapers grow.
+    # TODO: Would it be more sensible to only send the ids that we *can't* view?
+    # Or is there some other entirely more sensible approach to this problem? We just don't want to duplicate the authorisation logic in ScraperAbility here.
+    ids = Scraper.accessible_by(current_ability).ids
+    @all_scrapers = T.let(Scraper.search(@q, default_scraper_search_params.merge(where: { id: ids })), T.nilable(Searchkick::Relation))
+    @filtered_scrapers = T.let(Scraper.search(@q, default_scraper_search_params.merge(where: { data?: true, id: ids })), T.nilable(Searchkick::Relation))
 
     @scrapers = T.let(@show == "all" ? @all_scrapers : @filtered_scrapers, T.nilable(Searchkick::Relation))
+  end
+
+  private
+
+  # Overriding the default ability class name used because we've split them out. See
+  # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/split_ability.md
+  sig { returns(ScraperAbility) }
+  def current_ability
+    @current_ability ||= T.let(ScraperAbility.new(current_user), T.nilable(ScraperAbility))
   end
 end
