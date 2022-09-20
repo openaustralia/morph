3fc39c4d50f8ec03c9d39ab457dace065be8b8e6
Matthew Landauer <mlandauer@gmail.com>
2022-09-20 12:47:24 +1000

    Use cancancan baked-in aliases to simplify further

--- app/abilities/scraper_ability.rb
+++ app/abilities/scraper_ability.rb
@@ -9,24 +9,24 @@ class ScraperAbility
   sig { params(owner: T.nilable(Owner)).void }
   def initialize(owner)
     # Everyone can list all (non private) scrapers
-    can %i[index show], Scraper, private: false
+    can :read, Scraper, private: false
 
     return unless owner
 
     can :data, Scraper, private: false
-    can :new, Scraper unless SiteSetting.read_only_mode
+    can :create, Scraper unless SiteSetting.read_only_mode
     can :watch, Scraper, private: false unless SiteSetting.read_only_mode
 
     # user can view scrapers owned by them (even if private) and settings of scrapers they own
-    can %i[index show data], Scraper, owner_id: owner.id
-    can %i[destroy edit update create watch], Scraper, owner_id: owner.id unless SiteSetting.read_only_mode
+    can %i[read data], Scraper, owner_id: owner.id
+    can %i[destroy update watch], Scraper, owner_id: owner.id unless SiteSetting.read_only_mode
 
     # user can view scrapers and settings of scrapers belonging to an org they are a
     # member of
     if owner.is_a?(User)
       owner.organizations.each do |org|
-        can %i[index show data], Scraper, owner_id: org.id
-        can %i[destroy edit update create watch], Scraper, owner_id: org.id unless SiteSetting.read_only_mode
+        can %i[read data], Scraper, owner_id: org.id
+        can %i[destroy update create watch], Scraper, owner_id: org.id unless SiteSetting.read_only_mode
       end
     end
 
