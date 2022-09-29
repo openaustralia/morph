9229bda897bfb5c7e9bdeec2ccbbb072badf2f91
Matthew Landauer <mlandauer@gmail.com>
2022-09-29 16:21:54 +1000

    Intermittant issue is closed. So let's remove the message to the user

--- app/lib/morph/runner.rb
+++ app/lib/morph/runner.rb
@@ -207,9 +207,6 @@ module Morph
           this is to be expected.
 
           To fix this make your scraper write to an SQLite database at data.sqlite.
-
-          However, this could also be related to an intermittent problem which we're
-          working hard to resolve: https://github.com/openaustralia/morph/issues/1064
         ERROR
         block.call Time.zone.now, :internalerr, m if block_given?
         status_code = 998
