1a1112af85212bcdf8793ee5ead870363bd1b46b
Matthew Landauer <mlandauer@gmail.com>
2022-10-15 10:54:40 +1100

    Group some tests

--- spec/abilities/scraper_ability_spec.rb
+++ spec/abilities/scraper_ability_spec.rb
@@ -31,20 +31,22 @@ describe "ScraperAbility" do
   context "when a regular authenticated user" do
     let(:user) { create(:user) }
 
-    it { is_expected.to be_able_to(:index, Scraper) }
-    it { is_expected.to be_able_to(:show, scraper) }
-    it { is_expected.not_to be_able_to(:show, private_scraper) }
-    it { is_expected.to be_able_to(:new, Scraper) }
-    it { is_expected.to be_able_to(:create, Scraper) }
-    it { is_expected.not_to be_able_to(:create_private, Scraper) }
-    it { is_expected.to be_able_to(:watch, scraper) }
-    it { is_expected.not_to be_able_to(:watch, private_scraper) }
-    it { is_expected.to be_able_to(:data, scraper) }
-    it { is_expected.not_to be_able_to(:data, private_scraper) }
-    it { is_expected.not_to be_able_to(:memory_setting, Scraper) }
-    it { is_expected.not_to be_able_to(:edit, scraper) }
-    it { is_expected.not_to be_able_to(:destroy, scraper) }
-    it { is_expected.not_to be_able_to(:update, scraper) }
+    context "when scraper is now owned by the user" do
+      it { is_expected.to be_able_to(:index, Scraper) }
+      it { is_expected.to be_able_to(:show, scraper) }
+      it { is_expected.not_to be_able_to(:show, private_scraper) }
+      it { is_expected.to be_able_to(:new, Scraper) }
+      it { is_expected.to be_able_to(:create, Scraper) }
+      it { is_expected.not_to be_able_to(:create_private, Scraper) }
+      it { is_expected.to be_able_to(:watch, scraper) }
+      it { is_expected.not_to be_able_to(:watch, private_scraper) }
+      it { is_expected.to be_able_to(:data, scraper) }
+      it { is_expected.not_to be_able_to(:data, private_scraper) }
+      it { is_expected.not_to be_able_to(:memory_setting, Scraper) }
+      it { is_expected.not_to be_able_to(:edit, scraper) }
+      it { is_expected.not_to be_able_to(:destroy, scraper) }
+      it { is_expected.not_to be_able_to(:update, scraper) }
+    end
 
     context "when scraper is owned by the user" do
       before do
@@ -100,20 +102,24 @@ describe "ScraperAbility" do
   context "when an admin" do
     let(:user) { create(:user, admin: true) }
 
-    # Just checking for extra permissions an admin is expected to have
-    it { is_expected.to be_able_to(:memory_setting, Scraper) }
-    it { is_expected.to be_able_to(:create_private, Scraper) }
+    context "when the site is not in read-only mode" do
+      # Just checking for extra permissions an admin is expected to have
+      it { is_expected.to be_able_to(:memory_setting, Scraper) }
+      it { is_expected.to be_able_to(:create_private, Scraper) }
+    end
 
     context "when the site is in read-only mode" do
       before do
         SiteSetting.read_only_mode = true
       end
 
-      it { is_expected.not_to be_able_to(:new, Scraper) }
-      it { is_expected.not_to be_able_to(:create, Scraper) }
-      it { is_expected.not_to be_able_to(:watch, scraper) }
-      it { is_expected.not_to be_able_to(:memory_setting, Scraper) }
-      it { is_expected.not_to be_able_to(:create_private, Scraper) }
+      context "when scraper is not owned by the user" do
+        it { is_expected.not_to be_able_to(:new, Scraper) }
+        it { is_expected.not_to be_able_to(:create, Scraper) }
+        it { is_expected.not_to be_able_to(:watch, scraper) }
+        it { is_expected.not_to be_able_to(:memory_setting, Scraper) }
+        it { is_expected.not_to be_able_to(:create_private, Scraper) }
+      end
 
       context "when scraper is owned by the user" do
         before do
