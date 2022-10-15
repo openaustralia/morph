c6f403060eb3063f4027d6e7aaeacc11e336fce0
Matthew Landauer <mlandauer@gmail.com>
2022-10-15 12:24:28 +1100

    Continuing to try (and failing) to make these tests a bit more readable

--- spec/abilities/scraper_ability_spec.rb
+++ spec/abilities/scraper_ability_spec.rb
@@ -8,9 +8,8 @@ describe "ScraperAbility" do
   subject(:ability) { ScraperAbility.new(user) }
 
   context "when an unauthenticated user" do
-    let(:user) { nil }
-
     context "with a public scraper" do
+      let(:user) { nil }
       let(:scraper) { create(:scraper) }
 
       it { is_expected.to be_able_to(:index, Scraper) }
@@ -27,6 +26,7 @@ describe "ScraperAbility" do
     end
 
     context "with a private scraper" do
+      let(:user) { nil }
       let(:scraper) { create(:scraper, private: true) }
 
       it { is_expected.not_to be_able_to(:show, scraper) }
@@ -39,12 +39,11 @@ describe "ScraperAbility" do
   end
 
   context "when a regular authenticated user" do
-    let(:user) { create(:user) }
-
     context "with a public scraper" do
-      let(:scraper) { create(:scraper) }
-
       context "when scraper is owned by a different user" do
+        let(:user) { create(:user) }
+        let(:scraper) { create(:scraper) }
+
         it { is_expected.to be_able_to(:index, Scraper) }
         it { is_expected.to be_able_to(:show, scraper) }
         it { is_expected.to be_able_to(:new, Scraper) }
@@ -59,9 +58,8 @@ describe "ScraperAbility" do
       end
 
       context "when scraper is owned by the user" do
-        before do
-          scraper.update(owner: user)
-        end
+        let(:user) { create(:user) }
+        let(:scraper) { create(:scraper, owner: user) }
 
         it { is_expected.to be_able_to(:edit, scraper) }
         it { is_expected.to be_able_to(:destroy, scraper) }
@@ -70,11 +68,9 @@ describe "ScraperAbility" do
       end
 
       context "when a scraper is owned by an organization" do
+        let(:user) { create(:user) }
         let(:organization) { create(:organization) }
-
-        before do
-          scraper.update(owner: organization)
-        end
+        let(:scraper) { create(:scraper, owner: organization) }
 
         context "when the user is not a member of the organization" do
           it { is_expected.not_to be_able_to(:edit, scraper) }
@@ -97,9 +93,10 @@ describe "ScraperAbility" do
     end
 
     context "with a private scraper" do
-      let(:scraper) { create(:scraper, private: true) }
-
       context "when scraper is not owned by the user" do
+        let(:user) { create(:user) }
+        let(:scraper) { create(:scraper, private: true) }
+
         it { is_expected.not_to be_able_to(:show, scraper) }
         it { is_expected.not_to be_able_to(:watch, scraper) }
         it { is_expected.not_to be_able_to(:data, scraper) }
@@ -109,9 +106,8 @@ describe "ScraperAbility" do
       end
 
       context "when scraper is owned by the user" do
-        before do
-          scraper.update(owner: user)
-        end
+        let(:user) { create(:user) }
+        let(:scraper) { create(:scraper, private: true, owner: user) }
 
         it { is_expected.to be_able_to(:show, scraper) }
         it { is_expected.to be_able_to(:edit, scraper) }
@@ -122,10 +118,11 @@ describe "ScraperAbility" do
       end
 
       context "when scraper is owned by an organization the user is a member of" do
+        let(:user) { create(:user) }
         let(:organization) { create(:organization) }
+        let(:scraper) { create(:scraper, private: true, owner: organization) }
 
         before do
-          scraper.update(owner: organization)
           create(:organizations_user, organization: organization, user: user)
         end
 
@@ -140,9 +137,9 @@ describe "ScraperAbility" do
   end
 
   context "when an admin" do
-    let(:user) { create(:user, admin: true) }
-
     context "when the site is not in read-only mode" do
+      let(:user) { create(:user, admin: true) }
+
       # Just checking for extra permissions an admin is expected to have
       it { is_expected.to be_able_to(:memory_setting, Scraper) }
       it { is_expected.to be_able_to(:create_private, Scraper) }
@@ -154,9 +151,10 @@ describe "ScraperAbility" do
       end
 
       context "with a public scraper" do
-        let(:scraper) { create(:scraper) }
-
         context "when scraper is not owned by the user" do
+          let(:user) { create(:user, admin: true) }
+          let(:scraper) { create(:scraper) }
+
           it { is_expected.not_to be_able_to(:new, Scraper) }
           it { is_expected.not_to be_able_to(:create, Scraper) }
           it { is_expected.not_to be_able_to(:watch, scraper) }
@@ -165,9 +163,8 @@ describe "ScraperAbility" do
         end
 
         context "when scraper is owned by the user" do
-          before do
-            scraper.update(owner: user)
-          end
+          let(:user) { create(:user, admin: true) }
+          let(:scraper) { create(:scraper, owner: user) }
 
           it { is_expected.not_to be_able_to(:destroy, scraper) }
           it { is_expected.not_to be_able_to(:update, scraper) }
@@ -175,10 +172,11 @@ describe "ScraperAbility" do
         end
 
         context "when scraper is owned by an organization the user is a member of" do
+          let(:user) { create(:user, admin: true) }
           let(:organization) { create(:organization) }
+          let(:scraper) { create(:scraper, owner: organization) }
 
           before do
-            scraper.update(owner: organization)
             create(:organizations_user, organization: organization, user: user)
           end
 
