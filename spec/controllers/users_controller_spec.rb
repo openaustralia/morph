793d3b2e495b8f126d97d0ed3661231571b9fdbb
Brenda Wallace <brenda@wallace.net.nz>
2025-07-09 17:23:16 +1200

    lint/rubocop

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,66 +4,66 @@
 require "spec_helper"
 
 RSpec.describe UsersController, type: :controller do
-    let(:user) { create(:user) }
+  let(:user) { create(:user) }
 
-    describe "GET #index" do
-        context "as an authenticated user" do
-            before { sign_in user }
+  describe "GET #index" do
+    context "as an authenticated user" do
+      before { sign_in user }
 
-            it "returns http success for html" do
-                get :index
-                expect(response).to have_http_status(:success)
-                expect(response.content_type).to include("text/html")
-            end
+      it "returns http success for html" do
+        get :index
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to include("text/html")
+      end
 
-            it "returns http success for json" do
-                get :index, format: :json
-                expect(response).to have_http_status(:success)
-                expect(response.content_type).to include("application/json")
-            end
+      it "returns http success for json" do
+        get :index, format: :json
+        expect(response).to have_http_status(:success)
+        expect(response.content_type).to include("application/json")
+      end
 
-            it "paginates users for html" do
-                create_list(:user, 3)
-                get :index
-                expect(assigns(:users)).to respond_to(:current_page)
-            end
-        end
-
-        context "as a guest" do
-            it "returns http success" do
-                get :index
-                expect(response).to have_http_status(:success)
-            end
-        end
+      it "paginates users for html" do
+        create_list(:user, 3)
+        get :index
+        expect(assigns(:users)).to respond_to(:current_page)
+      end
     end
 
-    describe "GET #watching" do
-        before { sign_in user }
+    context "as a guest" do
+      it "returns http success" do
+        get :index
+        expect(response).to have_http_status(:success)
+      end
+    end
+  end
 
-        it "returns http success" do
-            get :watching, params: { id: user.id }
-            expect(response).to have_http_status(:success)
-        end
+  describe "GET #watching" do
+    before { sign_in user }
+
+    it "returns http success" do
+      get :watching, params: { id: user.id }
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #stats" do
+    before { sign_in user }
+
+    it "returns http success" do
+      get :stats
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "authorization" do
+    it "redirects to sign in for watching if not signed in" do
+      get :watching, params: { id: user.id }
+      expect(response).to redirect_to(new_user_session_path)
     end
 
-    describe "GET #stats" do
-        before { sign_in user }
-
-        it "returns http success" do
-            get :stats
-            expect(response).to have_http_status(:success)
-        end
+    it "redirects to sign in for stats if not signed in" do
+      get :stats
+      expect(response).to redirect_to(new_user_session_path)
     end
-
-    describe "authorization" do
-        it "redirects to sign in for watching if not signed in" do
-            get :watching, params: { id: user.id }
-            expect(response).to redirect_to(new_user_session_path)
-        end
-
-        it "redirects to sign in for stats if not signed in" do
-            get :stats
-            expect(response).to redirect_to(new_user_session_path)
-        end
-    end
+  end
 end
