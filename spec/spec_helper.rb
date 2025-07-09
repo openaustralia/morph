483fdb10546df6952e5d4e265ff0bacacedffdc8
Brenda Wallace <brenda@wallace.net.nz>
2025-07-09 12:21:27 +1200

    Add minimum coverage level

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require "simplecov"
 SimpleCov.start 'rails' do
   formatter SimpleCov::Formatter::JSONFormatter  if ENV['CI']
   track_files "**/*.rb"
+  SimpleCov.minimum_coverage 50
 end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
