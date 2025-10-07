96dee8d52d6185423abbd445aaf7e1e5ff0d9509
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-10-07 19:44:54 +1100

    Fix broken examples, using heroku-18 and actually save a record except for python since ceder-4 and heroku-18 can't load any python versions

--- default_files/perl/template/cpanfile
+++ default_files/perl/template/cpanfile
@@ -3,5 +3,6 @@
 # Find out more: https://morph.io/documentation/perl
 
 requires "LWP", "6.06";
+requires "LWP::Protocol::https", "6.06";
 requires "HTML::Tree", "5.03";
 requires "Database::DumpTruck", "1.2";
--- default_files/ruby/template/scraper.rb
+++ default_files/ruby/template/scraper.rb
@@ -1,22 +1,37 @@
-# This is a template for a Ruby scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+#!/usr/bin/env ruby
+# frozen_string_literal: true
 
-# require 'scraperwiki'
-# require 'mechanize'
-#
-# agent = Mechanize.new
-#
-# # Read in a page
-# page = agent.get("http://foo.com")
-#
-# # Find something on the page using css selectors
-# p page.at('div.content')
-#
-# # Write out to the sqlite database using scraperwiki library
-# ScraperWiki.save_sqlite(["name"], {"name" => "susan", "occupation" => "software developer"})
-#
-# # An arbitrary query against the database
-# ScraperWiki.select("* from data where 'name'='peter'")
+# This is a template for a Ruby scraper on morph.io (https://morph.io)
+
+Bundler.require
+
+require "scraperwiki"
+require "mechanize"
+
+class Scraper
+  def self.run
+    agent = Mechanize.new
+
+    # Read in a page
+    page = agent.get("https://example.com")
+
+    # Find something on the page using css selectors
+    page.search("h1").each do |h1|
+      value = h1.text.strip
+      # Write out to the sqlite database using scraperwiki library
+      ScraperWiki.save_sqlite(["name"], { "name" => value })
+    end
+
+    # An arbitrary query against the database
+    rows = ScraperWiki.select("rowid AS id, name FROM data ORDER BY rowid desc LIMIT 3")
+    rows.each do |row|
+      puts "#{row['id']}: #{row['name']}"
+    end
+  end
+end
+
+# Run the scraper whilst allowing this file to be required in tests without auto-execution
+Scraper.run if __FILE__ == $PROGRAM_NAME
 
 # You don't have to do things with the Mechanize or ScraperWiki libraries.
 # You can use whatever gems you want: https://morph.io/documentation/ruby
