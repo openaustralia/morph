d5fa6138c000b28e90a3334401055fd194e5adbe
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-10-11 10:29:07 +1100

    Move require 'csv' to controller that needs it

--- config/application.rb
+++ config/application.rb
@@ -2,8 +2,6 @@ require_relative 'boot'
 
 require 'rails/all'
 
-require 'csv'
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
