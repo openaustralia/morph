da7a2b9f1a167a39e70e9b1eadca9b0b80a9fbb8
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-10-11 21:34:11 +1100

    fix DEPRECATION WARNING: Initialization autoloaded the constants ActionText::ContentHelper and ActionText::TagHelper by excluding unessisary parts of rails. Also reduces memory required

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,23 @@
 require_relative 'boot'
 
-require 'rails/all'
+# require 'rails/all' # clean-up deprecation warnings by excluding unused modules with autoload in initialization issues
+
+require 'rails'
+
+# Loaded in the same order as rails/all
+require "active_record/railtie"  # ActiveRecord Used (18 files)
+# require "active_storage/engine"  # ActiveStorage Not used nor in schema.rb
+require "action_controller/railtie"  # ActionController Used (6 files)
+require "action_view/railtie"  # ActionView Used (13 files)
+require "action_mailer/railtie"  # ActionMailer Used (4 files)
+require "active_job/railtie"  # ActiveJob used by sidekiq
+# require "action_cable/engine"  # ActionCable Not used (faye/render_sync use their own)
+require "action_mailbox/engine"  # ActionMailbox Not directly referenced BUT required for tests to work
+# require "action_text/engine"  # ActionText Not used - excluding 
+# require "rails/test_unit/railtie"  # TestUnit Not used (we use spec instead) ####
+require "sprockets/railtie"  # Sprockets Used (1 files)
+
+# require "active_model/railtie"  # ActiveModel used for validations
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
