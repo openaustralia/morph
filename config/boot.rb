4f0cc2730d12ac8dea23b5efc4c1313157273c67
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-10-15 21:34:28 +1100

    WIP

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,4 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
-require 'bundler/setup' # Set up gems listed in the Gemfile.
-
-# TODO: Remove the following when Rails is fixed (LoggerThreadSafeLevel removed)
-require 'logger'
-
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+require "bundler/setup" # Set up gems listed in the Gemfile.
+require "bootsnap/setup" # Speed up boot time by caching expensive operations.
