8ed9022d4ddd6cbf106701240ea6cd263eed53e9
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-11-02 02:31:50 +1100

    Reverted removal of an inflection for the Hero plan name and added spec

---
+++ spec/initializers/inflections_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe "Custom inflections" do
+  describe "hero/heroes pluralization required for Plan name" do
+    it "pluralizes 'hero' as 'heroes'" do
+      expect("hero".pluralize).to eq("heroes")
+    end
+
+    it "pluralizes 'Hero' as 'Heroes'" do
+      expect("Hero".pluralize).to eq("Heroes")
+    end
+
+    it "singularizes 'heroes' as 'hero'" do
+      expect("heroes".singularize).to eq("hero")
+    end
+
+    it "singularizes 'Heroes' as 'Hero'" do
+      expect("Heroes".singularize).to eq("Hero")
+    end
+  end
+end
