ad3ffb7f8c019473163ffd6aa83554da987f999e
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-11-02 03:01:44 +1100

    Removed rails upgrade migration as it does nothing apart from block rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2025_10_10_074453) do
+ActiveRecord::Schema.define(version: 2022_10_13_051602) do
 
   create_table "active_admin_comments", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci", force: :cascade do |t|
     t.string "namespace"
