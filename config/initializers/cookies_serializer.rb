0aa590b046b3a9ac2090b5bc5f676344475fa866
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-11-02 02:08:37 +1100

    Upgraded cookies from hybrid to json

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -2,5 +2,4 @@
 
 # Specify a serializer for the signed and encrypted cookie jars.
 # Valid options are :json, :marshal, and :hybrid.
-# FIXME: Change to :json 1 hour after after next deploy (for seamless upgrade of user session cookies)
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+Rails.application.config.action_dispatch.cookies_serializer = :json
