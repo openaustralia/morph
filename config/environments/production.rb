7c599233f59a348c4319f3e6e53de9db5c36c91a
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-11-13 02:24:28 +1100

    Set protocol for default_url_options and refactor old hash rocket constant

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -124,7 +124,7 @@ Rails.application.configure do
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
   host = ENV.fetch("SERVER_NAME", 'morph.io')
-  config.action_mailer.default_url_options = { :host => host, protocol: "https" }
+  config.action_mailer.default_url_options = {  protocol: "https", host: host}
 end
 
 # So that the same host setting is available outside the mailer
