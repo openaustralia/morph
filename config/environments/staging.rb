c38e53cfed4b93ea78360412188965edb938f895
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-03 02:16:38 +1100

    Use a mailpit server for staging by default

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -4,6 +4,8 @@ require_relative "production"
 # Staging-specific overrides
 Rails.application.configure do
   config.action_mailer.default_url_options = { :host => ENV.fetch("STAGING_HOSTNAME"), protocol: "https" }
+  # FIXME: Change to an oaf test server when we have one
+  config.action_mailer.smtp_settings[:address] = ENV["CUTTLEFISH_SERVER"] || "plannies-mate.thesite.info"
 end
 
 # So that the same host setting is available outside the mailer
