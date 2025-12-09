a1d43bf037094e04775d03c0b03a1d2ebd22c40d
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-10 05:57:54 +1100

    Updated by rails app:update command
    # Conflicts:
    #	bin/rails
    #	bin/rake
    #	config/environments/production.rb
    #	config/initializers/inflections.rb
    #	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_051602) do
+ActiveRecord::Schema.define(version: 2025_10_11_130403) do
 
   create_table "active_admin_comments", id: :integer, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
     t.string "namespace"
