28046b632237f996ee8431f496b0a445746820f0
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-10 00:00:57 +1100

    Rails 6.0 needs explicit import of variables and mixins
    # Conflicts:
    #	app/assets/stylesheets/_landing_page.scss

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -4,8 +4,9 @@ ActiveAdmin.setup do |config|
   #
   # Set the title that is displayed on the main layout
   # for each of the active admin pages.
-  #
-  config.site_title = "morph.io"
+
+  config.site_title = Rails.application.routes.default_url_options[:host] ||
+                      "morph.io"
 
   # Set the link url for the title. For example, to take
   # users to your main site. Defaults to no link.
