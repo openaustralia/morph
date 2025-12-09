7f396af21db08fef22d522b24fe1feb518867d89
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-09 20:54:41 +1100

    Add GITHUB_APP_INSTALLED_BY env var for testing github integration

---
+++ spec/lib/morph/github_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rspec'
+
+RSpec.describe 'Morph::Github' do
+  before do
+    # Do nothing
+  end
+
+  after do
+    # Do nothing
+  end
+
+  context 'when condition' do
+    it 'succeeds' do
+      pending 'Not implemented'
+    end
+  end
+end
