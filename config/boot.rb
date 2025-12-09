35d454d05699ebf3146557c2d3cf7f790791aad3
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-10 04:35:58 +1100

    Updated by rails app:update command (regenerate db/schema.rb later)
    # Conflicts:
    #	config/environments/production.rb
    #	config/initializers/inflections.rb
    #	db/schema.rb

--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 
-# TODO: Remove the following rails 6.0 fix when upgrading to Rails 6.1+ (LoggerThreadSafeLevel removed)
+# TODO: Remove the following rails 6.0,6.1 fix when upgrading to Rails 7.0+ (LoggerThreadSafeLevel removed)
 require 'logger'
 
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
