68602799aaf9db59dcfd5e148eb1cc36115440cb
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-19 03:42:01 +1100

    Add spec of StaticController

---
+++ spec/controllers/static_controller_spec.rb
@@ -0,0 +1,18 @@
+# typed: false
+# frozen_string_literal: true
+
+require "spec_helper"
+
+RSpec.describe StaticController, type: :controller do
+  describe "GET #index" do
+    it "returns http success" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+  end
+end
