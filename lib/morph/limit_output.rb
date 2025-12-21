ccad76d407e1a2be10957d864323f8216955453e
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2025-12-21 16:59:38 +1100

    Fix some bugs and move limit_output into a class suitable for in process testing
    * Nil would have been passed to exit if the command had been killed by a signal
    * Added test for extra arguments (previously silently ignored)
    * Made default exit status 1 rather than nil in case popen3 doesn't execute block

--- lib/morph/limit_output.rb
+++ lib/morph/limit_output.rb
@@ -1,81 +1,89 @@
 #!/usr/bin/env ruby
 
-# This wrapper script runs a command and lets standard out and error flow
-# through. However, it does limit the number of lines of output. This is
-# used by morph as a wrapper around running scrapers to ensure that they
-# can't fill up the docker container log file (and hence the server disk).
-
 require "optparse"
 require "open3"
 
-max_lines = nil
-command = nil
-exit_status = nil
+module Morph
 
-OptionParser.new do |opts|
-  opts.banner = "Usage: ./limit_output.rb [max lines] [command to run]"
+  # This wrapper script runs a command and lets standard out and error flow
+  # through. However, it does limit the number of lines of output. This is
+  # used by morph as a wrapper around running scrapers to ensure that they
+  # can't fill up the docker container log file (and hence the server disk)
+  class LimitOutput
+    USAGE = "Usage: ./limit_output.rb max_lines command_to_run"
 
-  max_lines = ARGV[0].to_i
-  if ARGV[0].nil?
-    STDERR.puts "Please give me the maximum number of lines of output to show"
-    puts opts
-    exit 1
-  end
+    def self.run(args, stdout_io: $stdout, stderr_io: $stderr)
+      args = OptionParser.new do |opts|
+        opts.banner = USAGE
+        if args.length.zero?
+          stderr_io.puts "Please give me the maximum number of lines"
+        elsif args.length == 1
+          stderr_io.puts "Please give me a command to run"
+        elsif args.length > 2
+          stderr_io.puts "Please give me the command to run as one argument"
+        end
+        if args.length != 2
+          stderr_io.puts opts
+          return 1
+        end
+      end.parse(args)
 
-  command = ARGV[1]
-  if command.nil?
-    STDERR.puts "Please give me a command to run"
-    puts opts
-    exit(1)
-  end
-end.parse!
+      max_lines = args[0].to_i
+      command = args[1]
 
-# Disable output buffering
-STDOUT.sync = true
-STDERR.sync = true
+      # Disable output buffering
+      stdout_io.sync = true
+      stderr_io.sync = true
 
-stdout_buffer = ""
-stderr_buffer = ""
+      stdout_buffer = ""
+      stderr_buffer = ""
 
-line_count = 0
+      line_count = 0
 
-Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
-  streams = [stdout, stderr]
-  until streams.empty?
-    IO.select(streams).flatten.compact.each do |io|
-      if io.eof?
-        streams.delete io
-        next
+      exit_status = 1
+      Open3.popen3(command) do |_stdin, stdout, stderr, wait_thr|
+        streams = [stdout, stderr]
+        until streams.empty?
+          IO.select(streams).flatten.compact.each do |io|
+            if io.eof?
+              streams.delete io
+              next
+            end
+
+            on_stdout_stream = io.fileno == stdout.fileno
+            # Just send this stuff straight through
+            buffer = on_stdout_stream ? stdout_buffer : stderr_buffer
+            s = io.readpartial(1)
+            buffer << s
+            next unless s == "\n"
+
+            if line_count < max_lines || max_lines.zero?
+              (on_stdout_stream ? stdout_io : stderr_io) << buffer
+            elsif line_count == max_lines
+              stderr_io.puts "limit_output.rb: Too many lines of output!"
+            end
+            if on_stdout_stream
+              stdout_buffer = ""
+            else
+              stderr_buffer = ""
+            end
+            line_count += 1
+          end
+        end
+
+        # Output whatever is left in the buffers
+        if line_count < max_lines || max_lines.zero?
+          stdout_io << stdout_buffer
+          stderr_io << stderr_buffer
+        end
+
+        process_status = wait_thr.value
+        exit_status = process_status.exited? ? process_status.exitstatus : process_status.termsig + 128
       end
 
-      on_stdout_stream = io.fileno == stdout.fileno
-      # Just send this stuff straight through
-      buffer = on_stdout_stream ? stdout_buffer : stderr_buffer
-      s = io.readpartial(1)
-      buffer << s
-      next unless s == "\n"
-
-      if line_count < max_lines || max_lines.zero?
-        (on_stdout_stream ? STDOUT : STDERR) << buffer
-      elsif line_count == max_lines
-        STDERR.puts "limit_output.rb: Too many lines of output!"
-      end
-      if on_stdout_stream
-        stdout_buffer = ""
-      else
-        stderr_buffer = ""
-      end
-      line_count += 1
+      exit_status
     end
   end
-
-  # Output whatever is left in the buffers
-  if line_count < max_lines || max_lines.zero?
-    STDOUT << stdout_buffer
-    STDERR << stderr_buffer
-  end
-
-  exit_status = wait_thr.value.exitstatus
 end
 
-exit(exit_status)
+exit Morph::LimitOutput.run(ARGV) if __FILE__ == $0
