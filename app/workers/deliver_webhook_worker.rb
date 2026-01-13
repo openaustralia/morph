22be7d23ca41823b990f3f02286564b0c3cdd5bb
Ian Heggie (OAF) <ian+oaf@heggie.biz>
2026-01-13 14:57:55 +1100

    Add url substitution when delivering webhook

--- app/workers/deliver_webhook_worker.rb
+++ app/workers/deliver_webhook_worker.rb
@@ -13,7 +13,7 @@ class DeliverWebhookWorker
     # TODO: As soon as we've upgraded to a recent version of Ubuntu switch the SSL
     # verification back on. It's not crazy-super important that we verify SSL with
     # webhooks but it's sure a good idea.
-    connection = Faraday.new(webhook_delivery.webhook.url, ssl: { verify: false })
+    connection = Faraday.new(substituted_url(webhook_delivery), ssl: { verify: false })
     begin
       response = connection.post
       webhook_delivery.update(
@@ -25,5 +25,25 @@ class DeliverWebhookWorker
       # more important errors. Also, we don't want these just constantly retrying after failures
       Rails.logger.error("Webhook delivery failure on scraper #{webhook_delivery.run.scraper.full_name}: #{e}")
     end
+  end
+
+  private
+
+  def substituted_url(webhook_delivery)
+    url = webhook_delivery.webhook.url
+    run = webhook_delivery.run
+    {
+      "ADDED" => run.records_added,
+      "AUTO" => run.auto?,
+      "COMMIT" => run.git_revision,
+      "OUTCOME" => run.finished_successfully? ? "success" : "failed",
+      "REMOVED" => run.records_removed,
+      "REVISION" => run.git_revision[0..7],
+      "RUN_TIME" => run.wall_time.to_i,
+      "STATUS_CODE" => run.status_code
+    }.each do |name, value|
+      url = url.gsub(name, value.to_s)
+    end
+    url
   end
 end
